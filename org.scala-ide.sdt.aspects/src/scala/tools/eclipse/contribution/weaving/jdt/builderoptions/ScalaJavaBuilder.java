9ec73f28ef2067a98bb0817e54d683e7215393ed
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-02-05 10:53:33 +0000

    Initial commit.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin-aspects/trunk@17032 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ org.scala-ide.sdt.aspects/src/scala/tools/eclipse/contribution/weaving/jdt/builderoptions/ScalaJavaBuilder.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2008 Miles Sabin
+ * All rights reserved
+ * ------------------------
+ * http://www.milessabin.com
+ * mailto:miles@milessabin.com
+ */
+
+package scala.tools.eclipse.contribution.weaving.jdt.builderoptions;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.internal.core.builder.JavaBuilder;
+
+import scala.tools.eclipse.contribution.weaving.jdt.util.ReflectionUtils;
+
+@SuppressWarnings("restriction")
+public class ScalaJavaBuilder extends JavaBuilder {
+
+  private static final Method setProjectMethod;
+  static {
+    try{
+      Class<?> ibClazz = Class.forName("org.eclipse.core.internal.events.InternalBuilder");
+      setProjectMethod = ReflectionUtils.getMethod(ibClazz, "setProject", IProject.class);
+    }
+    catch(ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  public void clean(IProgressMonitor monitor) throws CoreException {
+    super.clean(monitor);
+  }
+  
+  @SuppressWarnings("unchecked")
+  public IProject[] build(int kind, Map ignored, IProgressMonitor monitor) throws CoreException {
+    return super.build(kind, ignored, monitor);
+  }
+  
+  public void setProject0(IProject project) {
+    try
+    {
+      setProjectMethod.invoke(this, project);
+    }
+    catch(IllegalArgumentException e)
+    {
+      throw new RuntimeException(e);
+    }
+    catch(IllegalAccessException e)
+    {
+      throw new RuntimeException(e);
+    }
+    catch(InvocationTargetException e)
+    {
+      throw new RuntimeException(e);
+    }
+  }
+}
