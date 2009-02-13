929238163213335cf298c75b1a1a2a14e2cfb7a4
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-02-13 14:57:01 +0000

    Use more JDT Java editor functionality (eg. breadcrumb now fully functional, fixing #1192). Some simplifications due to aspects.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin-aspects/trunk@17108 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ org.scala-ide.sdt.aspects/src/scala/tools/eclipse/contribution/weaving/jdt/ui/javaeditor/ScalaEditor.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2005-2009 LAMP/EPFL
+ */
+// $Id$
+
+package scala.tools.eclipse.contribution.weaving.jdt.ui.javaeditor;
+
+import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
+
+@SuppressWarnings("restriction")
+public abstract class ScalaEditor extends CompilationUnitEditor {
+}
