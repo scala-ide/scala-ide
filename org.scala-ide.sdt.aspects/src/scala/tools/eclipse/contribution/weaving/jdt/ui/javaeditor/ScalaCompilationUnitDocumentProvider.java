a65fda760e8b18fd5461f4e4631760ec482da26a
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-02-16 17:19:40 +0000

    Fixed #1410, #1534, #1627 and #1716. Also fixed an unreported issue with [17107] where the Scala Editors title icon would be reset to the standard (for the underlying Java editor) Java source icon.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin-aspects/trunk@17122 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ org.scala-ide.sdt.aspects/src/scala/tools/eclipse/contribution/weaving/jdt/ui/javaeditor/ScalaCompilationUnitDocumentProvider.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2005-2009 LAMP/EPFL
+ */
+// $Id$
+
+package scala.tools.eclipse.contribution.weaving.jdt.ui.javaeditor;
+
+import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitDocumentProvider;
+
+@SuppressWarnings("restriction")
+public class ScalaCompilationUnitDocumentProvider extends CompilationUnitDocumentProvider {
+}
