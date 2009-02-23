5d52436c25c905ff0e2970f4d0187a9e8fccd81c
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-02-23 22:39:27 +0000

    More untangling.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin/trunk@17185 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/scala/tools/editor/ParseNodes.scala
+++ src/scala/tools/editor/ParseNodes.scala
@@ -5,32 +5,26 @@
 // $Id$
 
 package scala.tools.editor 
+
 import scala.tools.nsc.ast.parser.Tokens._
 import scala.tools.nsc.util
-trait ParseNodes extends Tokenizers with lampion.compiler.Parsers {
+
+trait ParseNodes extends Tokenizers {
   import compiler.Tree
   override type ParseTree = Tree
 
-  type ParseNode <: Node with ParseNodeImpl
-  trait ParseNodeImpl extends super[Tokenizers].ParseNodeImpl with super[Parsers].ParseNodeImpl with util.Position {selfX : ParseNode =>
-    def self : ParseNode
-    def asParseTree : compiler.StubTree
-    private[ParseNodes] def prefix0 : String
-    protected override def destroy0 = super.destroy0
-    override def dbgString = "XXX"
-    override def source = Some(file.unit.source)
-    override def offset = Some(absolute)
-  }
   type File <: FileImpl
-  trait FileImplA extends super[Tokenizers].FileImpl {selfX : File => }
-  trait FileImplB extends super[Parsers].FileImpl {selfX : File => }
-  trait FileImpl extends FileImplA with FileImplB {selfX : File =>
+  trait FileImpl extends super.FileImpl {selfX : File =>
     def self : File
 
     type ParseNode <: ParseNodes.this.ParseNode with ParseNodeImpl
-    trait ParseNodeImpl extends super[FileImplA].ParseNodeImpl with super[FileImplB].ParseNodeImpl with ParseNodes.this.ParseNodeImpl {selfX : ParseNode =>
+    trait ParseNodeImpl extends super.ParseNodeImpl with util.Position { selfX : ParseNode =>
       def self : ParseNode
-      private[ParseNodes] def prefix0 = prefix
+      def asParseTree : compiler.StubTree
+      protected override def destroy0 = super.destroy0
+      override def dbgString = "XXX"
+      override def source = Some(file.unit.source)
+      override def offset = Some(absolute)
       def offsetFor(deflated : Int) = absolute + inflate(deflated)
     }
   }
