d33141daa7a83deb186bca11065dfeb5e9245cd5
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-02-24 19:32:10 +0000

    More flattening.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin/trunk@17199 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/lampion/core/Positions.scala
+++ src/lampion/core/Positions.scala
@@ -8,10 +8,55 @@ package lampion.core
 
 import scala.collection.jcl._
 
-trait Positions extends Files {
+trait Positions {
   type File <: FileImpl
-  trait FileImpl extends super.FileImpl {
+  trait FileImpl {
     def self : File
+    
+    def file = self
+    
+    def content : RandomAccessSeq[Char]
+    
+    def willBuild = {}
+    
+    def repair(offset : Int, added : Int, removed : Int) : Unit = {}
+    
+    def startWith(idx : Int, str : String) = {
+      var idx0 = idx
+      val str0 : RandomAccessSeq[Char] = (str)
+      while (idx0 < str.length && idx0 < content.length && str0(idx0) == content(idx0))
+        idx0 = idx0 + 1
+      idx0 == str.length
+    }
+    
+    private var editing0 = false
+    
+    final def editing = editing0
+    
+    final def editing_=(value : Boolean) = if (!editing0 && value) {
+      editing0 = value
+      prepareForEditing
+    } else if(editing0 && !value) editing0 = value
+    
+    def doUnload : Unit = {
+      editing0 = false
+    }
+    
+    def loaded = {
+      editing0 = false
+      assert(!editing0)
+    }
+    
+    def unloaded = {
+      editing0 = false
+    }
+
+    def prepareForEditing = {}
+    
+    def isLoaded : Boolean
+    def doLoad : Unit = {}
+    def clear = {}
+    
     trait PositionBank {
       protected def pushBack = true
       def destroy : Unit = { while (head.next != null) destroy(head.next) }
