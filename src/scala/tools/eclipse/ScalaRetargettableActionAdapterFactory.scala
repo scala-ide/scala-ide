fd808f7f2f2eb7a7e01cf62ef7af560bb70a1096
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-07-06 14:53:07 +0000

    Debugging works again.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin/trunk@18217 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ src/scala/tools/eclipse/ScalaRetargettableActionAdapterFactory.scala
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2005-2009 LAMP/EPFL
+ */
+// $Id$
+
+package scala.tools.eclipse
+
+import org.eclipse.core.runtime.IAdapterFactory
+import org.eclipse.debug.ui.actions.{ IRunToLineTarget, IToggleBreakpointsTarget }
+import org.eclipse.jdt.internal.debug.ui.actions.RunToLineAdapter
+
+class ScalaRetargettableActionAdapterFactory extends IAdapterFactory {
+  override def getAdapter(adaptableObject : AnyRef, adapterType : Class[_]) =
+    if (adapterType == classOf[IRunToLineTarget])
+      new RunToLineAdapter
+    else if (adapterType == classOf[IToggleBreakpointsTarget])
+      new ScalaToggleBreakpointAdapter
+    else
+      null
+
+  override def getAdapterList : Array[Class[_]] = 
+    Array(classOf[IRunToLineTarget], classOf[IToggleBreakpointsTarget])
+}
---
+++ src/scala/tools/eclipse/ScalaToggleBreakpointAdapter.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2005-2009 LAMP/EPFL
+ */
+// $Id$
+
+package scala.tools.eclipse
+
+import java.util.HashMap
+
+import org.eclipse.core.runtime.{ CoreException, IProgressMonitor, IStatus, Status }
+import org.eclipse.core.runtime.jobs.Job
+import org.eclipse.debug.core.DebugPlugin
+import org.eclipse.jdt.core.{ IJavaElement, IMember, IType }
+import org.eclipse.jdt.debug.core.JDIDebugModel
+import org.eclipse.jdt.internal.debug.ui.{ BreakpointUtils, JDIDebugUIPlugin }
+import org.eclipse.jdt.internal.debug.ui.actions.{ ActionMessages, ToggleBreakpointAdapter }
+import org.eclipse.jface.viewers.IStructuredSelection
+import org.eclipse.jface.text.{ BadLocationException, ITextSelection }
+import org.eclipse.jface.viewers.ISelection
+import org.eclipse.ui.IWorkbenchPart
+
+import scala.tools.eclipse.util.ReflectionUtils
+
+class ScalaToggleBreakpointAdapter extends ToggleBreakpointAdapter { self =>
+  import ScalaToggleBreakpointAdapterUtils._
+  
+  override def toggleLineBreakpoints(part : IWorkbenchPart, selection : ISelection, bestMatch : Boolean) {
+    val job = new Job("Toggle Line Breakpoint") {
+      override def run(monitor : IProgressMonitor) : IStatus = {
+        val editor = self.getTextEditor(part)
+        if (editor != null && selection.isInstanceOf[ITextSelection]) {
+          if (monitor.isCanceled)
+            return Status.CANCEL_STATUS;
+          try {
+            report(null, part)
+            val sel = 
+              if(!selection.isInstanceOf[IStructuredSelection])
+                translateToMembers(part, selection)
+              else
+                selection
+            
+              if(sel.isInstanceOf[IStructuredSelection]) {
+                val member = sel.asInstanceOf[IStructuredSelection].getFirstElement.asInstanceOf[IMember]
+                val tpe =
+                  if(member.getElementType == IJavaElement.TYPE)
+                    member.asInstanceOf[IType]
+                  else
+                    member.getDeclaringType
+                val tname = createQualifiedTypeName(self, tpe)
+                val resource = BreakpointUtils.getBreakpointResource(tpe)
+                val lnumber = selection.asInstanceOf[ITextSelection].getStartLine+1
+                val existingBreakpoint = JDIDebugModel.lineBreakpointExists(resource, tname, lnumber)
+                if (existingBreakpoint != null) {
+                  DebugPlugin.getDefault().getBreakpointManager.removeBreakpoint(existingBreakpoint, true)
+                  return Status.OK_STATUS
+                }
+                val attributes = new HashMap[AnyRef, AnyRef](10)
+                val documentProvider = editor.getDocumentProvider
+                if (documentProvider == null)
+                  return Status.CANCEL_STATUS
+                val document = documentProvider.getDocument(editor.getEditorInput)
+                try {
+                  val line = document.getLineInformation(lnumber-1)
+                  val start = line.getOffset
+                  val end = start+line.getLength-1
+                  BreakpointUtils.addJavaBreakpointAttributesWithMemberDetails(attributes, tpe, start, end)
+                } catch {
+                  case ble : BadLocationException => JDIDebugUIPlugin.log(ble)
+                }
+                JDIDebugModel.createLineBreakpoint(resource, tname, lnumber, -1, -1, 0, true, attributes)
+              } else {
+                report(ActionMessages.ToggleBreakpointAdapter_3, part)
+                return Status.OK_STATUS
+              }
+            } catch {
+              case ce : CoreException => return ce.getStatus
+            }
+          }
+          return Status.OK_STATUS
+        }
+      }
+    
+    job.setSystem(true)
+    job.schedule
+  }
+
+  override def toggleBreakpoints(part : IWorkbenchPart, selection : ISelection) {
+    val sel = translateToMembers(part, selection)
+    if(sel.isInstanceOf[IStructuredSelection]) {
+      val member = sel.asInstanceOf[IStructuredSelection].getFirstElement.asInstanceOf[IMember]
+      val mtype = member.getElementType
+      if(mtype == IJavaElement.FIELD || mtype == IJavaElement.METHOD) {
+        if (selection.isInstanceOf[ITextSelection]) {
+          val ts = selection.asInstanceOf[ITextSelection]
+          toggleLineBreakpoints(part, ts)
+        } 
+      }
+      else if(member.getElementType == IJavaElement.TYPE)
+        toggleClassBreakpoints(part, sel)
+      else
+        toggleLineBreakpoints(part, selection, true)
+    }
+  }
+
+  override def report(message : String, part : IWorkbenchPart) = super.report(message, part)
+  override def getTextEditor(part : IWorkbenchPart) = super.getTextEditor(part)
+  override def translateToMembers(part : IWorkbenchPart, selection : ISelection) = super.translateToMembers(part, selection)
+}
+
+object ScalaToggleBreakpointAdapterUtils extends ReflectionUtils {
+  val toggleBreakpointAdapterClazz = classOf[ToggleBreakpointAdapter]
+  val createQualifiedTypeNameMethod = getDeclaredMethod(toggleBreakpointAdapterClazz, "createQualifiedTypeName", classOf[IType])
+  
+  def createQualifiedTypeName(tba : ToggleBreakpointAdapter, tpe : IType) = createQualifiedTypeNameMethod.invoke(tba, tpe).asInstanceOf[String]
+}
