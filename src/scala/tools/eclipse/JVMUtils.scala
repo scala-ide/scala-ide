f9161ad5b784058e8e97ad0eeb1cf2ccc7e934f8
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-11-04 21:13:18 +0000

    Use the functionality from scalac rather than duplicating it here.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin/trunk@19400 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/scala/tools/eclipse/JVMUtils.scala
+++ src/scala/tools/eclipse/JVMUtils.scala
@@ -5,150 +5,21 @@
 
 package scala.tools.eclipse
 
-import scala.collection.immutable.HashMap
-
 import scala.tools.nsc.Global 
-import scala.tools.nsc.symtab.Flags
-import ch.epfl.lamp.fjbg.{ JAccessFlags, JArrayType, JMethodType, JObjectType, JType }
+import ch.epfl.lamp.fjbg.JType
 
 trait JVMUtils { self : Global =>
-  def javaName(sym: Symbol): String = {
-    val suffix = if (sym.hasFlag(Flags.MODULE) && !sym.isMethod &&
-                      !sym.isImplClass && 
-                      !sym.hasFlag(Flags.JAVA)) "$" else "";
 
-    if (sym == definitions.NothingClass)
-      return javaName(definitions.RuntimeNothingClass)
-    else if (sym == definitions.NullClass)
-      return javaName(definitions.RuntimeNullClass)
+  private lazy val codeGenerator =
+    currentRun.phaseNamed(genJVM.phaseName).asInstanceOf[genJVM.JvmPhase].codeGenerator
 
-    (if (sym.isClass || (sym.isModule && !sym.isMethod))
-      sym.fullNameString('/')
-    else
-      sym.simpleName.toString.trim()) + suffix
-  }
-
-  def javaNames(syms: List[Symbol]): Array[String] = {
-    val res = new Array[String](syms.length)
-    var i = 0
-    syms foreach (s => { res(i) = javaName(s); i += 1 })
-    res
-  }
-
-  def javaFlags(sym: Symbol): Int = {
-    import JAccessFlags._
-
-    var jf: Int = 0
-    val f = sym.flags
-    jf = jf | (if (sym hasFlag Flags.SYNTHETIC) ACC_SYNTHETIC else 0)
-/*      jf = jf | (if (sym hasFlag Flags.PRIVATE) ACC_PRIVATE else 
-                if (sym hasFlag Flags.PROTECTED) ACC_PROTECTED else ACC_PUBLIC)
-*/
-    jf = jf | (if (sym hasFlag Flags.PRIVATE) ACC_PRIVATE else  ACC_PUBLIC)
-    jf = jf | (if ((sym hasFlag Flags.ABSTRACT) ||
-                   (sym hasFlag Flags.DEFERRED)) ACC_ABSTRACT else 0)
-    jf = jf | (if (sym hasFlag Flags.INTERFACE) ACC_INTERFACE else 0)
-    jf = jf | (if ((sym hasFlag Flags.FINAL)
-                     && !sym.enclClass.hasFlag(Flags.INTERFACE) 
-                     && !sym.isClassConstructor) ACC_FINAL else 0)
-    jf = jf | (if (sym.isStaticMember) ACC_STATIC else 0)
-    jf = jf | (if (sym hasFlag Flags.BRIDGE) ACC_BRIDGE | ACC_SYNTHETIC else 0)
-
-    if (sym.isClass && !sym.hasFlag(Flags.INTERFACE))
-      jf = jf | ACC_SUPER
-
-    // constructors of module classes should be private
-    if (sym.isPrimaryConstructor && isTopLevelModule(sym.owner)) {
-      jf |= ACC_PRIVATE
-      jf &= ~ACC_PUBLIC
-    }
-    jf
-  }
-
-  def javaType(t: Type): JType = t match {
-    case ThisType(sym) => 
-      if (sym == definitions.ArrayClass)
-        JObjectType.JAVA_LANG_OBJECT
-      else
-        new JObjectType(javaName(sym))
-
-    case SingleType(pre, sym) => 
-      primitiveTypeMap get sym.name.toString match {
-        case Some(k) => k
-        case None    => new JObjectType(javaName(sym))
-      }
-
-    case ConstantType(value) =>
-      javaType(t.underlying)
-
-    case TypeRef(_, sym, args) =>
-      primitiveTypeMap get sym.name.toString match {
-        case Some(k) => k
-        case None    => arrayOrClassType(sym, args)
-      }
-
-    case ClassInfoType(_, _, sym) =>
-      primitiveTypeMap get sym.name.toString match {
-        case Some(k) => k
-        case None    =>
-          if (sym == definitions.ArrayClass)
-            abort("ClassInfoType to ArrayClass!")
-          else
-            new JObjectType(javaName(sym))
-      }
-
-    case ExistentialType(tparams, t) =>
-      javaType(t)
-      
-    case m : MethodType =>
-      val t = m.finalResultType
-      new JMethodType(javaType(t), m.paramss.flatMap(_.map(javaType)).toArray)
-      
-    case p : PolyType =>
-      val t = p.finalResultType
-      javaType(t)
-    
-    case r : RefinedType =>
-      JObjectType.JAVA_LANG_OBJECT
-      //javaType(r.typeSymbol.tpe)
-    
-    case _ =>
-      abort(
-        "Unknown type: "+t+", "+t.normalize+"["+t.getClass+", "+t.normalize.getClass+"]"+ 
-        " TypeRef? "+t.isInstanceOf[TypeRef]+", "+t.normalize.isInstanceOf[TypeRef]) 
-  }
+  def javaName(sym : Symbol) : String = codeGenerator.javaName(sym)
   
-  private def arrayOrClassType(sym: Symbol, targs: List[Type]): JType = {
-    if (sym == definitions.ArrayClass)
-      new JArrayType(javaType(targs.head))
-    else if (sym.isClass)
-      new JObjectType(javaName(sym))
-    else {
-      assert(sym.isType, sym) // it must be compiling Array[a]
-      JObjectType.JAVA_LANG_OBJECT
-    }
-  }
+  def javaNames(syms : List[Symbol]) : Array[String] = codeGenerator.javaNames(syms)
   
-  private val primitiveTypeMap = HashMap(
-    (nme.Unit.toString -> JType.VOID),
-    (nme.Boolean.toString -> JType.BOOLEAN),
-    (nme.Byte.toString -> JType.BYTE),
-    (nme.Short.toString -> JType.SHORT),
-    (nme.Char.toString -> JType.CHAR),
-    (nme.Int.toString -> JType.INT),
-    (nme.Long.toString -> JType.LONG),
-    (nme.Float.toString -> JType.FLOAT),
-    (nme.Double.toString -> JType.DOUBLE),
-    (nme.Any.toString -> JObjectType.JAVA_LANG_OBJECT),
-    (nme.AnyRef.toString -> JObjectType.JAVA_LANG_OBJECT),
-    (nme.AnyVal.toString -> JObjectType.JAVA_LANG_OBJECT)
-  )
+  def javaFlags(sym : Symbol) : Int = codeGenerator.javaFlags(sym)
   
-  def javaType(s: Symbol): JType =
-    javaType(s.tpe)
-
-  def isTopLevelModule(sym: Symbol): Boolean =
-    atPhase (currentRun.picklerPhase.next) {
-      sym.isModuleClass && !sym.isImplClass && !sym.isNestedClass
-    }
+  def javaType(t : Type) : JType = codeGenerator.javaType(t)
+  
+  def javaType(sym : Symbol) : JType = codeGenerator.javaType(sym)
 }
