921319aff947d6a3584e5037b0a60ec1e73265a1
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-11-29 23:56:40 +0000

    Fixed #2727; tidied whitespace.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin/trunk@19930 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/scala/tools/eclipse/interpreter/InterpreterLaunchConfigurationDelegate.scala
+++ src/scala/tools/eclipse/interpreter/InterpreterLaunchConfigurationDelegate.scala
@@ -5,11 +5,16 @@
 
 package scala.tools.eclipse.interpreter
 
+import java.io.File
+
 import org.eclipse.jdt.launching._
 import org.eclipse.debug.core._
 import org.eclipse.core.runtime._
 
+import org.eclipse.osgi.baseadaptor.BaseData
+import org.eclipse.osgi.framework.internal.core.AbstractBundle
 import org.osgi.framework.Bundle
+
 /**
  * This launch delegate extends the normal JavaLaunchDelegate with functionality to work for the interpreter.
  */
@@ -19,70 +24,69 @@ class InterpreterLaunchConfigurationDelegate extends AbstractJavaLaunchConfigura
     val mon : IProgressMonitor = if(monitor == null) new NullProgressMonitor() else monitor
     //Helper method to actually perform the launch inside a try-catch block.
     def doTheLaunch() {        
-	    val mainClass = "scala.tools.nsc.MainInterpreter"
-	    
-	    mon.beginTask(configuration.getName(), 3); //$NON-NLS-1$
-	    //We need lots of early outs if possible...
-	    if(mon.isCanceled) {
-	      return;
-	    }
-	    
-	    val vmRunner = getVMRunner(configuration, mode)
-	    //TODO - Check for null/existence of working directory... (should this always be defined?)
-	    val workingDir = { 
-	      val dir = getWorkingDirectory(configuration)
-	      if(dir != null) {
-	        dir.getAbsolutePath
-	      } else {
-	        null
-	      }
-	    }
-	    val envp = getEnvironment(configuration)
-	    val interpArgs = getProgramArguments(configuration)
-	    val vmArgs = getVMArguments(configuration)
-	    val execArgs = new ExecutionArguments(vmArgs, interpArgs)    
-	    val vmAttrs = getVMSpecificAttributesMap(configuration)
-	    
-	    val classpath = getClasspath(configuration) ++ interpreterExtraClassPath
-	
-        Console.println("interpreter classpath = " + classpath.mkString(" - "))
-     
-	    val runConfig = new VMRunnerConfiguration(mainClass, classpath);
-	    runConfig.setProgramArguments(execArgs.getProgramArgumentsArray());
-		runConfig.setEnvironment(envp);
-		runConfig.setVMArguments(execArgs.getVMArgumentsArray());
-		runConfig.setWorkingDirectory(workingDir);
-		runConfig.setVMSpecificAttributesMap(vmAttrs);
-		
-		// Bootpath - TODO - Add scala library/compiler here
-		runConfig.setBootClassPath(getBootpath(configuration));
-	    
-	    // check for cancellation (again)
-		if (mon.isCanceled()) {
-			return;
-		}	
-	    // done the verification phase
-		mon.worked(1);
-	 
-		// set the default source locator if required
-		setDefaultSourceLocator(launch, configuration);
-		mon.worked(1);		
-		
-		// Launch the configuration
-		vmRunner.run(runConfig, launch, monitor);
-		
-        //send extra commands to the configuraiton
-        if(mon.isCanceled) {
-          return;
+      val mainClass = "scala.tools.nsc.MainInterpreter"
+      
+      mon.beginTask(configuration.getName(), 3); //$NON-NLS-1$
+      //We need lots of early outs if possible...
+      if(mon.isCanceled) {
+        return;
+      }
+      
+      val vmRunner = getVMRunner(configuration, mode)
+      //TODO - Check for null/existence of working directory... (should this always be defined?)
+      val workingDir = { 
+        val dir = getWorkingDirectory(configuration)
+        if(dir != null) {
+          dir.getAbsolutePath
+        } else {
+          null
         }
-        runSeedscripts()
+      }
+
+      val envp = getEnvironment(configuration)
+      
+      val vmAttrs = getVMSpecificAttributesMap(configuration)
+      val execArgs = new ExecutionArguments(getVMArguments(configuration), getProgramArguments(configuration))    
+      val vmArgs = execArgs.getVMArgumentsArray()
+      val classpath = (toolClassPath ++ getClasspath(configuration)).mkString(File.pathSeparator)
+      val programArgs = Array("-Xnojline", "-classpath", classpath) ++ execArgs.getProgramArgumentsArray() 
+      
+      val runConfig = new VMRunnerConfiguration(mainClass, toolClassPath.toArray);
+      runConfig.setWorkingDirectory(workingDir);
+      runConfig.setEnvironment(envp);
+      runConfig.setVMArguments(vmArgs);
+      runConfig.setVMSpecificAttributesMap(vmAttrs);
+      runConfig.setProgramArguments(programArgs);
+    
+      // Bootpath - TODO - Add scala library/compiler here
+      runConfig.setBootClassPath(getBootpath(configuration));
+      
+      // check for cancellation (again)
+      if (mon.isCanceled()) {
+        return;
+      }  
+      // done the verification phase
+      mon.worked(1);
+   
+      // set the default source locator if required
+      setDefaultSourceLocator(launch, configuration);
+      mon.worked(1);    
+    
+      // Launch the configuration
+      vmRunner.run(runConfig, launch, monitor);
+    
+      //send extra commands to the configuration
+      if(mon.isCanceled) {
+        return;
+      }
+      runSeedscripts()
         
-		// check for cancellation
-		if (mon.isCanceled()) {
-			return;
-		}		 
+      // check for cancellation
+      if (mon.isCanceled()) {
+        return;
+      }     
     }
-    /** Helpe5r method to deal with launch configuration */
+    /** Helper method to deal with launch configuration */
     implicit def pimpConfiguration(configuration : ILaunchConfiguration) = new {
       def getAttributeOption(name : String) : Option[String] = {
         configuration.getAttribute(name, "") match {
@@ -125,26 +129,32 @@ class InterpreterLaunchConfigurationDelegate extends AbstractJavaLaunchConfigura
     }
   }
   
-   
-  
-  
   /** Retreives the extra classpath needed for the interpreter*/
-  def interpreterExtraClassPath : Seq[String] = {
-    val bundle = Platform.getBundle("scala.tools.nsc")
-    def getBundleEntry(path : String) = {
-      val entryURL = bundle.getEntry(path)
-      if(entryURL == null)
-        "" //TODO - Error
-      else {
-        val bundlePath = FileLocator.resolve(entryURL).getPath
-        if(bundlePath.endsWith("!/"))
-          bundlePath.subSequence(0, bundlePath.length-2).toString
-        else
-          bundlePath
-      }
+  def toolClassPath : Seq[String] = {
+    // Equinox-specific hack which allows us to refer to the unpacked
+    // scala-library.jar for use on the out-of-process interpreters
+    // classpath
+    val libraryJarFile =
+      Platform.getBundle("scala.library").asInstanceOf[AbstractBundle].
+      getBundleData.asInstanceOf[BaseData].
+      getBundleFile.getFile("/lib/scala-library.jar", false)
+    
+    val compilerBundle = Platform.getBundle("scala.tools.nsc")
+    val compilerBundleFile = FileLocator.getBundleFile(compilerBundle)
+
+    // Compiler classpath corresponds to either the installed .jar
+    // or a compiler projectc in a development time workspace
+    if (compilerBundleFile.getName.endsWith(".jar"))
+      libraryJarFile.getPath ::
+      compilerBundleFile.getPath ::
+      Nil
+    else {
+      def mkEntry(entry : String) = new File(compilerBundleFile, entry).getPath
+      libraryJarFile.getPath ::
+      mkEntry("/bin") ::
+      mkEntry("/lib/fjbg.jar") ::
+      mkEntry("/lib/msil.jar") ::
+      Nil
     }
-    // The compiler if scala.tools.nsc is unexploded
-    // The compiler if scala.tools.nsc is exploded in an outer workspace
-    getBundleEntry("/") :: getBundleEntry("/bin"):: getBundleEntry("/lib/fjbg.jar") :: Nil    
   }
 }
