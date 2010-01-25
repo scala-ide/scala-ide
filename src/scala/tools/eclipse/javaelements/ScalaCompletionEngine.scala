181aa411e9c9f21ecb146f38c9674718ec3e6806
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2010-01-25 23:16:25 +0000

    Fixed #2954. Review by community.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin/trunk@20666 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/scala/tools/eclipse/javaelements/ScalaCompletionEngine.scala
+++ src/scala/tools/eclipse/javaelements/ScalaCompletionEngine.scala
@@ -51,7 +51,7 @@ class ScalaCompletionEngine {
       
       val completed = new SyncVar[Either[List[compiler.Member], Throwable]]
       val (start, end) = t0 match {
-        case Some(s@compiler.Select(qualifier, name)) =>
+        case Some(s@compiler.Select(qualifier, name)) if qualifier.pos.isDefined =>
           val cpos0 = qualifier.pos.endOrPoint 
           val cpos = compiler.rangePos(sourceFile, cpos0, cpos0, cpos0)
           compiler.askTypeCompletion(cpos, completed)
