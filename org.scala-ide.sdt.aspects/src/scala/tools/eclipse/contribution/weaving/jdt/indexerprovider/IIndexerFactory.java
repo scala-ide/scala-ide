b79db504d2052788134e37cfd15049ed39cee760
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2010-02-06 21:45:56 +0000

    Deferred symbols should be mapped to abstract; suppressed a spurious warning.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin-aspects/trunk@20813 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- org.scala-ide.sdt.aspects/src/scala/tools/eclipse/contribution/weaving/jdt/indexerprovider/IIndexerFactory.java
+++ org.scala-ide.sdt.aspects/src/scala/tools/eclipse/contribution/weaving/jdt/indexerprovider/IIndexerFactory.java
@@ -8,6 +8,7 @@ package scala.tools.eclipse.contribution.weaving.jdt.indexerprovider;
 import org.eclipse.jdt.core.search.SearchDocument;
 import org.eclipse.jdt.internal.core.search.indexing.AbstractIndexer;
 
+@SuppressWarnings("restriction")
 public interface IIndexerFactory {
   public AbstractIndexer createIndexer(SearchDocument document);
 }
