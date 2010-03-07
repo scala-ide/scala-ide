8a01f4a3f11cb807be7388cf63b41c92754911de
milessabin <milessabin@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2010-03-07 20:29:38 +0000

    Much improved New Scala Project Wizard derived from the JDTs New Java Project Wizard. Fixes 2519, 2520 and 2703.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-plugin/trunk@21090 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ src/scala/tools/eclipse/wizards/ScalaProjectWizard.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2005-2010 LAMP/EPFL
+ */
+// $Id$
+
+package scala.tools.eclipse.wizards
+
+import scala.collection.mutable.ArrayBuffer
+
+import org.eclipse.core.runtime.{ IProgressMonitor, NullProgressMonitor, OperationCanceledException, Path, SubProgressMonitor }
+import org.eclipse.jdt.core.{ IClasspathEntry, JavaCore }
+import org.eclipse.jdt.internal.ui.wizards.{ JavaProjectWizard, NewWizardMessages }
+import org.eclipse.jdt.internal.ui.wizards.buildpaths.BuildPathsBlock
+import org.eclipse.jdt.ui.PreferenceConstants
+import org.eclipse.jdt.ui.wizards.{ JavaCapabilityConfigurationPage, NewJavaProjectWizardPageOne, NewJavaProjectWizardPageTwo }
+
+import scala.tools.eclipse.ScalaPlugin
+import scala.tools.eclipse.util.ReflectionUtils
+
+class ScalaProjectWizard extends {
+    val pageOne = new NewScalaProjectWizardPageOne
+    val pageTwo = new NewScalaProjectWizardPageTwo(pageOne)
+  }
+  with JavaProjectWizard(pageOne, pageTwo) {
+  setWindowTitle("New Scala Project")
+  pageOne.setDescription("Create a new Scala project")
+  pageOne.setTitle("New Scala Project")
+}
+  
+class NewScalaProjectWizardPageOne extends NewJavaProjectWizardPageOne {
+  override def getDefaultClasspathEntries() : Array[IClasspathEntry] =
+    (JavaCore.newContainerEntry(Path.fromPortableString(ScalaPlugin.plugin.scalaLibId)) +=: ArrayBuffer(super.getDefaultClasspathEntries : _*)).toArray
+}
+
+class NewScalaProjectWizardPageTwo(pageOne : NewJavaProjectWizardPageOne) extends NewJavaProjectWizardPageTwo(pageOne) {
+  import NewScalaProjectWizardPageTwoUtils._
+  override def configureJavaProject(newProjectCompliance : String, monitor0 : IProgressMonitor) {
+    val monitor = if (monitor0 != null) monitor0 else new NullProgressMonitor
+    val nSteps = 6
+    monitor.beginTask(NewWizardMessages.JavaCapabilityConfigurationPage_op_desc_java, nSteps)
+
+    try {
+      addScalaNatures(new SubProgressMonitor(monitor, 1))
+      getBuildPathsBlock(this).configureJavaProject(newProjectCompliance, new SubProgressMonitor(monitor, 5))
+    } catch {
+      case ex : OperationCanceledException => throw new InterruptedException
+    } finally {
+      monitor.done
+    }
+  }
+
+  def addScalaNatures(monitor : IProgressMonitor) {
+    if (monitor != null && monitor.isCanceled)
+      throw new OperationCanceledException
+    val project = getJavaProject.getProject
+    if (!project.hasNature(JavaCore.NATURE_ID)) {
+      val desc = project.getDescription
+      val natures = ArrayBuffer(desc.getNatureIds : _*) + ScalaPlugin.plugin.natureId + JavaCore.NATURE_ID
+      desc.setNatureIds(natures.toArray)
+      project.setDescription(desc, monitor)
+    } else {
+      if (monitor != null) {
+        monitor.worked(1)
+      }
+    }
+  }
+}
+
+object NewScalaProjectWizardPageTwoUtils extends ReflectionUtils {
+  val jccpClazz = classOf[JavaCapabilityConfigurationPage]
+  val getBuildPathsBlockMethod = getDeclaredMethod(jccpClazz, "getBuildPathsBlock")
+  
+  def getBuildPathsBlock(jccp : JavaCapabilityConfigurationPage) = getBuildPathsBlockMethod.invoke(jccp).asInstanceOf[BuildPathsBlock]
+}
