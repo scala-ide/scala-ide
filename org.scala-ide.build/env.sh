100d675c67338d581df50fd9a6c8fd968663320b
David Bernard <david.bernard.31@gmail.com>
2010-06-22 16:53:32 +0200

    change build script to use regular maven local repository ( with several version of some plugin like maven-resources-plugin)
    use tycho.version property to specify tycho version
    
    remove hardcoded path to maven3 executable

---
+++ org.scala-ide.build/env.sh
@@ -0,0 +1,14 @@
+# MAVEN needs to point to a MAVEN3 installation:
+if which mvn >/dev/null; then
+  mvn -version | grep "Maven 3" > /dev/null
+  if [ $? -eq 0 ]; then
+    MAVEN="mvn"
+  fi
+fi
+
+if [ "X$MAVEN" = "X" ] ; then
+  echo "Missing environment variable \"MAVEN\". This has to point to a maven 3.0 installation, "
+  echo "e.g. add the following line to your .bashrc (and make sure the path is correct):"
+  echo "export MAVEN=/opt/apache-maven-3.0-beta-1/bin/mvn"
+  exit
+fi
--- org.scala-ide.build/update-ide-versions.sh
+++ org.scala-ide.build/update-ide-versions.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-export MAVEN=/opt/apache-maven-3.0-beta-1/bin/mvn
-export MAVEN_REPO_LOCAL=./m2repo
+source $(dirname $0)/env.sh
 
 ${MAVEN} \
--Dmaven.repo.local=${MAVEN_REPO_LOCAL} \
 -N versions:update-child-modules
