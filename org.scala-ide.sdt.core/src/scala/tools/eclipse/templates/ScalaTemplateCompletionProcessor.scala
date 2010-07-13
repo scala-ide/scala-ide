2abb0d69cec62d234c1a2f9f49593c42b858d4df
David Bernard <david.bernard.31@gmail.com>
2010-07-13 16:27:30 +0200

    templates : adjust some parameters

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/templates/ScalaTemplateCompletionProcessor.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/templates/ScalaTemplateCompletionProcessor.scala
@@ -69,7 +69,7 @@ class ScalaTemplateCompletionProcessor(val tm : ScalaTemplateManager) extends Te
   }
 
   protected override def getRelevance(template : Template, prefix : String) : Int = {
-    (prefix == null | prefix.trim().length == 0) match {
+    (prefix == null || prefix.trim().length == 0) match {
       case true => 0
       case false => super.getRelevance(template, prefix)
     }
