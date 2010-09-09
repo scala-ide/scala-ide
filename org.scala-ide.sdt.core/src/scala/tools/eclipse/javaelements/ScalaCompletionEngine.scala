23c3bb47c229b4e3e51ce25e43d621a1e05e3591
eugene.vigdorchik <ven@HERMIT.(none)>
2010-09-09 22:16:18 +0400

    Change completion to include more variants, e.g. classes are included

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/javaelements/ScalaCompletionEngine.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/javaelements/ScalaCompletionEngine.scala
@@ -16,6 +16,7 @@ import org.eclipse.jdt.core.compiler.CharOperation
 import org.eclipse.jdt.internal.codeassist.{ InternalCompletionContext, InternalCompletionProposal }
 import org.eclipse.jdt.internal.core.JavaProject
 import org.eclipse.jdt.ui.text.java.CompletionProposalCollector
+import ch.epfl.lamp.fjbg.JType
 
 import scala.tools.nsc.symtab.Flags
 
@@ -23,6 +24,8 @@ import scala.tools.eclipse.ScalaWordFinder
 import scala.tools.eclipse.util.ReflectionUtils
 
 class ScalaCompletionEngine {
+  private var debug = true
+	
   def complete
     (cu : env.ICompilationUnit, unitToSkip : env.ICompilationUnit,
      position : Int,  requestor : CompletionRequestor, owner : WorkingCopyOwner, typeRoot : ITypeRoot,
@@ -81,7 +84,7 @@ class ScalaCompletionEngine {
           }
       }
       
-      val prefix = scu.getBuffer.getText(start, end-start).trim.toArray
+      val prefix = (if (position <= start) "" else scu.getBuffer.getText(start, position-start).trim).toArray
   
       def createContext = {
         new CompletionContext {
@@ -119,7 +122,7 @@ class ScalaCompletionEngine {
           proposal.setTokenRange(start, end)
           proposal.setRelevance(relevance)
           requestor.accept(proposal)
-        } else if (sym.isMethod && !sym.isConstructor && sym.name != nme.asInstanceOf_ && sym.name != nme.isInstanceOf_) {
+        } else if (sym.isMethod && sym.name != nme.asInstanceOf_ && sym.name != nme.isInstanceOf_) {
           val proposal =  new ScalaCompletionProposal(CompletionProposal.METHOD_REF, position-1)
           val paramNames = tpe.paramss.flatMap(_.map(_.name))
           val paramTypes = tpe.paramss.flatMap(_.map(_.tpe))
@@ -142,7 +145,9 @@ class ScalaCompletionEngine {
           val sig0 = javaType(tpe).getSignature.replace('/', '.')
           val sig = if (sig0.startsWith("(")) sig0 else "()"+sig0
           
-          proposal.setDeclarationSignature(javaType(sym.owner.tpe).getSignature.replace('/', '.').toArray)
+          val jtOwner = javaType(sym.owner.tpe)
+          if (jtOwner != JType.UNKNOWN)
+    	    proposal.setDeclarationSignature(jtOwner.getSignature.replace('/', '.').toArray)
           proposal.setSignature(sig.toArray)
           setDeclarationPackageName(proposal, sym.enclosingPackage.fullName.toArray)
           setDeclarationTypeName(proposal, mapTypeName(sym.owner).toArray)
@@ -163,6 +168,21 @@ class ScalaCompletionEngine {
           val proposal =  new ScalaCompletionProposal(CompletionProposal.LOCAL_VARIABLE_REF, position-1)
           val transformedName = NameTransformer.decode(sym.name.toString) 
           val relevance = 30
+          if (!sym.isModule)
+            proposal.setSignature(javaType(tpe).getSignature.replace('/', '.').toArray)
+          setPackageName(proposal, tpe.typeSymbol.enclosingPackage.fullName.toArray)
+          setTypeName(proposal, mapTypeName(tpe.typeSymbol).toArray)
+          proposal.setName(transformedName.toArray)
+          proposal.setCompletion(transformedName.toArray)
+          proposal.setFlags(mapModifiers(sym))
+          proposal.setReplaceRange(start, end)
+          proposal.setTokenRange(start, end)
+          proposal.setRelevance(relevance)
+          requestor.accept(proposal)
+        } else if (sym.isClass) {
+          val proposal =  new ScalaCompletionProposal(CompletionProposal.TYPE_REF, position-1)
+          val transformedName = NameTransformer.decode(sym.name.toString) 
+          val relevance = 30
           proposal.setSignature(javaType(tpe).getSignature.replace('/', '.').toArray)
           setPackageName(proposal, tpe.typeSymbol.enclosingPackage.fullName.toArray)
           setTypeName(proposal, mapTypeName(tpe.typeSymbol).toArray)
@@ -177,29 +197,22 @@ class ScalaCompletionEngine {
       }
       
       def nameMatches(sym : compiler.Symbol) = {
-        val name = sym.name.toString.toArray 
+        val name = sym.rawname.toString.toArray
         CharOperation.prefixEquals(prefix, name, false) ||
         CharOperation.camelCaseMatch(prefix, name)
       }
       
       def validType(sym : compiler.Symbol, tpe : compiler.Type) = {
-        try {
-          if (sym.isEmptyPackage || tpe == compiler.NoType || tpe == compiler.ErrorType)
-            false
-          else { 
-            val tpe0 = compiler.uncurry.transformInfo(sym, tpe)
-            javaType(sym.owner.tpe).getSignature
-            javaType(tpe0).getSignature
-            true
-          }
-        } catch {
-          case _ => false
-        }
+        !sym.isEmptyPackage && !sym.isRootPackage && !sym.isConstructor && 
+        tpe != compiler.ErrorType
       }
       
       completed.get.left.toOption match {
         case Some(completions) =>
           requestor.acceptContext(createContext)
+          if (debug) {
+        	  println("Completions: " + completions.mkString("\n"))
+          }
           for(completion <- completions) {
             completion match {
               case compiler.TypeMember(sym, tpe, accessible, inherited, viaView) if nameMatches(sym) && validType(sym, tpe) =>
