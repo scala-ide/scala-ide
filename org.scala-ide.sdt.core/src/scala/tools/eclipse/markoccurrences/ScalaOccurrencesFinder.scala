350444ca9ecff4499d24766de321ef28f01c5bdc
eugene.vigdorchik <eugene.vigdorchik@gmail.com>
2010-10-11 00:16:20 +0400

    Race: by the time the post selection handler is called the file might have been closed and no longer by the compiler

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/markoccurrences/ScalaOccurrencesFinder.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/markoccurrences/ScalaOccurrencesFinder.scala
@@ -12,28 +12,27 @@ case class Occurrences(name: String, locations: List[Region])
 class ScalaOccurrencesFinder(file: ScalaSourceFile, offset: Int, length: Int) {
 
   def findOccurrences(): Option[Occurrences] = {
-    val (from, to) = (offset, offset + length) match {
-      case (f, t) if f == t => (f - 1, t + 1) // pretend that we have a selection
-      case x => x
-    }
+    val (from, to) = (offset, offset + length)
     file.withSourceFile { (sourceFile, compiler) =>
       val analysis = new Selections with GlobalIndexes {
         val global = compiler
         val index = GlobalIndex(Nil)
       }
       import analysis._
-      compiler.ask { () => 
-        val selection = new FileSelection(sourceFile.file, from, to)
-        selection.selectedSymbolTree flatMap { selectedLocal =>
-          val symbol = selectedLocal.symbol
-          if (symbol.name.isOperatorName)
-            None
-          else {
-            val index = GlobalIndex(global.unitOf(sourceFile).body)
-            val positions = index occurences symbol map (_.namePosition) filter (_ != global.NoPosition) map (p => (p.start, p.end - p.start))
-            val symbolName = symbol.nameString
-            val locations = positions collect { case (offset, length) if length == symbolName.length => new Region(offset, length) }
-            Some(Occurrences(symbolName, locations.toList))
+      compiler.ask { () =>
+        if (!compiler.unitOfFile.contains(sourceFile.file)) None else {
+          val selection = new FileSelection(sourceFile.file, from, to)
+          selection.selectedSymbolTree flatMap { selectedLocal =>
+            val symbol = selectedLocal.symbol
+            if (symbol.name.isOperatorName)
+              None
+            else {
+              val index = GlobalIndex(global.unitOf(sourceFile).body)
+              val positions = index occurences symbol map (_.namePosition) filter (_ != global.NoPosition) map (p => (p.start, p.end - p.start))
+              val symbolName = symbol.nameString
+              val locations = positions collect { case (offset, length) if length == symbolName.length => new Region(offset, length) }
+              Some(Occurrences(symbolName, locations.toList))
+            }
           }
         }
       }
