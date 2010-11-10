bb1da3c5622492e0a13496173d0384e74206c98a
Miles Sabin <miles@milessabin.com>
2010-11-10 15:49:53 +0000

    Added support for building relative to 2.8.1.final.

--- org.scala-ide.build/build-ide-local-2.8.x.sh
+++ org.scala-ide.build/build-ide-local-2.8.x.sh
@@ -2,10 +2,10 @@
 
 . $(dirname $0)/env.sh
 
-SCALA_VERSION=2.8.1-SNAPSHOT
+SCALA_VERSION=2.8.2-SNAPSHOT
 
 ${MAVEN} \
   -U \
-  -P local-scala-2.8.1,!scala-2.8.1 \
+  -P local-scala-2.8.2,!scala-2.8.x \
   -Dscala.version=${SCALA_VERSION} \
   clean install $*
