c895d1c7821fa5bd1f37597be6b447327606edfb
David Bernard <david.bernard.31@gmail.com>
2010-12-06 16:56:18 +0100

    avoid NPE in ScalaDebugHover

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaDebugHover.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaDebugHover.scala
@@ -11,16 +11,29 @@ import org.eclipse.jface.text.{ ITextViewer, IRegion }
 import scala.tools.eclipse.javaelements.ScalaCompilationUnit
 
 class ScalaDebugHover extends JavaSourceHover {
+  
+  private val _noHoverInfo = "" // could return null but prefere to return empty (see API of ITextHover)
 
+  // override deprecated getHoverInfo instead of getHoverInfo2 like JavaSourceHover    
   override def getHoverInfo(textViewer : ITextViewer, hoverRegion :  IRegion) = {
-    val scu = getCodeAssist.asInstanceOf[ScalaCompilationUnit]
-    
-    val start = hoverRegion.getOffset
-    val length = hoverRegion.getLength
-    val end = start+length
-    
-    scu.withSourceFile({ (sourceFile, compiler) =>
-      compiler.debugInfo(sourceFile, start, length)
-    })
+    getCodeAssist match {
+      case null => _noHoverInfo // getCodeAssist can return null
+      case scu : ScalaCompilationUnit => {
+        val start = hoverRegion.getOffset
+        val length = hoverRegion.getLength
+        val end = start+length
+        scu.withSourceFile({ (sourceFile, compiler) =>
+          compiler.debugInfo(sourceFile, start, length)
+        })
+      }
+      case _ => super.getHoverInfo(textViewer, hoverRegion)
+    }
   }
+  
+  /*
+   * @see org.eclipse.jface.text.ITextHoverExtension2#getHoverInfo2(org.eclipse.jface.text.ITextViewer, org.eclipse.jface.text.IRegion)
+   * @since 3.4
+   */
+  // duplicate code from AbstractJavaEditorText to avoid loosing feature if super code change.
+  override def getHoverInfo2(textViewer : ITextViewer , hoverRegion : IRegion) : Object =  getHoverInfo(textViewer, hoverRegion)
 }
