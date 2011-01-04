1fd3e46d73a4ac8c5a1557d694ed4fe490c0eee4
David Bernard <david.bernard.31@gmail.com>
2011-01-04 14:35:36 +0100

    Cached : replace use of synchronized+wait+notify by ReentrantReadWriteLock (try to avoid a freeze I've got on openning Eclipse : wait never notified)

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/util/Cached.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/util/Cached.scala
@@ -6,51 +6,109 @@
 package scala.tools.eclipse.util
 
 trait Cached[T] {
-  private var inProgress = false
-  private var elem : Option[T] = None
+  import java.util.concurrent.locks.ReentrantReadWriteLock
   
-  def apply[U](op : T => U) : U = {
-    val e = synchronized {
+//  private var inProgress = false
+  @volatile
+  private var elem : Option[T] = None
+  private val _rwl = new ReentrantReadWriteLock()
+  
+  /**
+   * create only if it's doesn't already exists
+   */
+//  private def provide() : T = {
+//    synchronized {
+//      elem match {
+//        case Some(t) => t
+//        case None => {
+//          while (!elem.isDefined) {
+//            try {
+//              while (inProgress)
+//                wait
+//            } catch {
+//              case ex : InterruptedException =>
+//                if (!elem.isDefined) {
+//                  throw ex
+//                }
+//            }
+//        
+//            if (!elem.isDefined) {
+//              inProgress = true
+//              try {
+//                val t = create
+//                elem = Some(t)
+//              } finally {
+//                inProgress = false
+//                notifyAll
+//              }
+//            }
+//          }
+//          elem.get
+//        }
+//      }
+//    }
+//  }
+//  def invalidate() {
+//    val oldElem = synchronized {
+//      val elem0 = elem
+//      elem = None
+//      elem0
+//    }
+//    
+//    oldElem match {
+//      case Some(t) => destroy(t)
+//      case _ =>
+//    }
+//  }
+
+  // implementation based on http://download.oracle.com/javase/6/docs/api/java/util/concurrent/locks/ReentrantReadWriteLock.html
+  private def provide() : T = {
+    _rwl.readLock().lock()
+    try {
       elem match {
         case Some(t) => t
         case None => {
-          while (!elem.isDefined) {
-            try {
-              while (inProgress)
-                wait
-            } catch {
-              case ex : InterruptedException =>
-                if (!elem.isDefined) {
-                  throw ex
-                }
-            }
-        
-            if (!elem.isDefined) {
-              inProgress = true
-              try {
+          // Must release read lock before acquiring write lock
+          _rwl.readLock().unlock()
+          _rwl.writeLock().lock()
+          try {
+            // Recheck state because another thread might have acquired
+            //   write lock and changed state before we did.
+            elem match {
+              case Some(t) => t
+              case None => {
                 val t = create
                 elem = Some(t)
-              } finally {
-                inProgress = false
-                notifyAll
+                t
               }
             }
+          } finally {
+            // Downgrade by acquiring read lock before releasing write lock
+            _rwl.readLock().lock();
+            _rwl.writeLock().unlock(); // Unlock write, still hold read
           }
-          elem.get
-	}
+        }
       }
+    } finally {
+      _rwl.readLock().unlock()  
     }
-    
+  }
+  
+  def apply[U](op : T => U) : U = {
+    val e = provide()
+    //TODO should the execution of op be part of the readLock ??
     op(e)
   }
   
   def invalidate() {
-    val oldElem = synchronized {
-      val elem0 = elem
+    var oldElem : Option[T] = None  
+    _rwl.writeLock().lock()
+    try {
+      oldElem = elem
       elem = None
-      elem0
+    } finally {
+      _rwl.writeLock().unlock()
     }
-    
     oldElem match {
       case Some(t) => destroy(t)
       case _ =>
