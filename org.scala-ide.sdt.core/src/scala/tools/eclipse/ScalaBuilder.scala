65cea8e687a01dc5ebf8f5767d045e89934a929a
David Bernard <david.bernard.31@gmail.com>
2011-01-19 11:13:56 +0100

    ScalaBuilder : fix bug , clean itself and dependency project ScalaBuilder : rebuild itself if dependency project changed without error

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaBuilder.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaBuilder.scala
@@ -6,19 +6,17 @@
 package scala.tools.eclipse
 
 import scala.collection.mutable.HashSet
-
 import java.{ lang => jl, util => ju }
-
 import org.eclipse.core.resources.{ IFile, IncrementalProjectBuilder, IProject, IResource, IResourceDelta, IResourceDeltaVisitor, IResourceVisitor }
 import org.eclipse.core.runtime.{ IProgressMonitor, IPath }
 import org.eclipse.jdt.internal.core.JavaModelManager
 import org.eclipse.jdt.internal.core.builder.{ JavaBuilder, NameEnvironment, State }
-
 import scala.tools.eclipse.contribution.weaving.jdt.builderoptions.ScalaJavaBuilder
 import scala.tools.eclipse.javaelements.JDTUtils
 import scala.tools.eclipse.util.{ FileUtils, ReflectionUtils }
 import scala.tools.eclipse.internal.logging.Tracer
 import scala.tools.eclipse.util.IDESettings
+import org.eclipse.core.resources.IMarker
 
 class ScalaBuilder extends IncrementalProjectBuilder {
   def plugin = ScalaPlugin.plugin
@@ -36,97 +34,115 @@ class ScalaBuilder extends IncrementalProjectBuilder {
     JDTUtils.refreshPackageExplorer
   }
   
+  /**
+   * @param kind the kind of build being requested. Valid values are
+   * <ul>
+   * <li>{@link #FULL_BUILD} - indicates a full build.</li>
+   * <li>{@link #INCREMENTAL_BUILD}- indicates an incremental build.</li>
+   * <li>{@link #AUTO_BUILD} - indicates an automatically triggered
+   * incremental build (autobuilding on).</li>
+   * </ul>
+   * @param args a table of builder-specific arguments keyed by argument name
+   * (key type: <code>String</code>, value type: <code>String</code>);
+   * <code>null</code> is equivalent to an empty map
+   * @param monitor a progress monitor, or <code>null</code> if progress
+   * reporting and cancellation are not desired
+   * @return the list of projects for which this builder would like deltas the
+   * next time it is run or <code>null</code> if none
+   */
   override def build(kind : Int, ignored : ju.Map[_, _], monitor : IProgressMonitor) : Array[IProject] = Tracer.timeOf("ScalaBuilder build :" + kindToString(kind)){
     import IncrementalProjectBuilder._
 
     val project = plugin.getScalaProject(getProject)
     
-    val allSourceFiles = project.allSourceFiles()
-    val dependeeProjectChanged =
-      project.externalDepends.exists(
-        x => { val delta = getDelta(x); delta == null || delta.getKind != IResourceDelta.NO_CHANGE})
-    val cleanBuildForced = IDESettings.alwaysCleanBuild.value || dependeeProjectChanged
-    val (addedOrUpdated, removed) = cleanBuildForced match {
+    val depends = project.externalDepends
+    val cleanBuildForced = IDESettings.alwaysCleanBuild.value || dependsChangedWithNoError(depends)
+    val newKind = cleanBuildForced match {
       case true => {
         Tracer.println("clean+build forced")
         clean(monitor)
-        (allSourceFiles, Set.empty[IFile])
+        FULL_BUILD
       }
-      case false => kind match {
-        case INCREMENTAL_BUILD | AUTO_BUILD => {
-          val addedOrUpdated0 = new HashSet[IFile] ++ allSourceFiles.filter(FileUtils.hasBuildErrors(_))
-          val removed0 = new HashSet[IFile]
-          val sourceFolders = project.sourceFolders.map{ _.getLocation }
-          getDelta(project.underlying).accept(new IResourceDeltaVisitor {
-            def visit(delta : IResourceDelta) = {
-              delta.getResource match {
-                case file : IFile if plugin.isBuildable(file) && sourceFolders.exists(_.isPrefixOf(file.getLocation)) =>
-                  delta.getKind match {
-                    case IResourceDelta.ADDED | IResourceDelta.CHANGED =>
-                      addedOrUpdated0 += file
-                    case IResourceDelta.REMOVED =>
-                      removed0 += file
-                    case _ =>
-                  }
-                case _ =>
-              }
-              true
+      case false => kind
+    }
+    
+    val allSourceFiles = project.allSourceFiles()
+    val (addedOrUpdated, removed) = newKind match {
+      case INCREMENTAL_BUILD | AUTO_BUILD => {
+        val addedOrUpdated0 = new HashSet[IFile] ++ allSourceFiles.filter(FileUtils.hasBuildErrors(_))
+        val removed0 = new HashSet[IFile]
+        val sourceFolders = project.sourceFolders.map{ _.getLocation }
+        getDelta(project.underlying).accept(new IResourceDeltaVisitor {
+          def visit(delta : IResourceDelta) = {
+            delta.getResource match {
+              case file : IFile if plugin.isBuildable(file) && sourceFolders.exists(_.isPrefixOf(file.getLocation)) =>
+                delta.getKind match {
+                  case IResourceDelta.ADDED | IResourceDelta.CHANGED =>
+                    addedOrUpdated0 += file
+                  case IResourceDelta.REMOVED =>
+                    removed0 += file
+                  case _ =>
+                }
+              case _ =>
             }
-          })
-          //TODO made a less brutal update of PresentationCompiler (+TopLevelMap) / RefinedBuilder when some files are removed/renamed
-          removed0.isEmpty match {
-            case false => {
-              Tracer.println("clean+build forced due to files removed/renamed")
-              clean(monitor)
-              (allSourceFiles, Set.empty[IFile])
-            }
-            case true => (addedOrUpdated0.toSet, removed0.toSet) 
+            true
           }
+        })
+        //TODO made a less brutal update of PresentationCompiler (+TopLevelMap) / RefinedBuilder when some files are removed/renamed
+        removed0.isEmpty match {
+          case false => {
+            Tracer.println("clean+build forced due to files removed/renamed")
+            clean(monitor)
+            (allSourceFiles, Set.empty[IFile])
+          }
+          case true => (addedOrUpdated0.toSet, removed0.toSet) 
         }
-        case CLEAN_BUILD | FULL_BUILD => {
-          (allSourceFiles, Set.empty[IFile])
-        }
+      }
+      case CLEAN_BUILD | FULL_BUILD => {
+        (allSourceFiles, Set.empty[IFile])
       }
     }
     
-    project.build(addedOrUpdated, removed, monitor)
-
-    //trigger rebuild of depends only if no error in current build
-    val back = allSourceFiles.filter(FileUtils.hasBuildErrors(_)).isEmpty match {
-      case false => Array[IProject]()
-      case true =>{
-        val depends = project.externalDepends
-        val projects = ((allSourceFiles.exists(_.getFileExtension == "java")) match {
-          case false => {
-            ensureProject
-            val javaDepends = scalaJavaBuilder.build(kind, ignored, monitor)
-            val modelManager = JavaModelManager.getJavaModelManager
-            val state = modelManager.getLastBuiltState(getProject, null).asInstanceOf[State]
-            val newState = if (state ne null) state
-              else {
-                ScalaJavaBuilderUtils.initializeBuilder(scalaJavaBuilder, 0, false)
-                StateUtils.newState(scalaJavaBuilder)
-              }
-            StateUtils.tagAsStructurallyChanged(newState)
-            StateUtils.resetStructurallyChangedTypes(newState)
-            modelManager.setLastBuiltState(getProject, newState)
-            JDTUtils.refreshPackageExplorer
-            (depends ++ javaDepends).toArray
-          }
-          case true => depends
-        }).distinct
-        
-        // reset classpath of depend project to force reload of newly generated class in the current thread (using events can raised event too late)
-        for( p <- projects if plugin.isScalaProject(p)) {
-          plugin.getScalaProject(p).resetCompilers(monitor)
-        }
-        projects
+    (addedOrUpdated.isEmpty && removed.isEmpty) match {
+      case true => Tracer.println("no scala/java changes")
+      case false => {
+        project.build(addedOrUpdated, removed, monitor)
+        callJavaBuilder(newKind, ignored, monitor) // ignore returned value, else next time, this should take care in depends definition 
+        JDTUtils.refreshPackageExplorer
       }
     }
-    back
+    depends
   }
   
-  def ensureProject = {
+  private def dependsChangedWithNoError(depends : Array[IProject]) = {
+    depends.exists{ x => 
+      val delta = getDelta(x)
+      (
+        x.getProject.isOpen
+        && (delta == null || delta.getKind != IResourceDelta.NO_CHANGE)
+        //&& !FileUtils.hasBuildErrors(x.getProject)
+        && !(x.getProject.findMarkers(null, true, IResource.DEPTH_INFINITE).exists(_.getAttribute(IMarker.SEVERITY) == IMarker.SEVERITY_ERROR))
+      )
+    }
+  }
+
+  private def callJavaBuilder(kind : Int, ignored : ju.Map[_, _], monitor : IProgressMonitor) : Array[IProject] = {
+    ensureProject
+    val javaDepends = scalaJavaBuilder.build(kind, ignored, monitor)
+    val modelManager = JavaModelManager.getJavaModelManager
+    val state = modelManager.getLastBuiltState(getProject, null).asInstanceOf[State]
+    val newState = if (state ne null) state
+      else {
+        ScalaJavaBuilderUtils.initializeBuilder(scalaJavaBuilder, 0, false)
+        StateUtils.newState(scalaJavaBuilder)
+      }
+    StateUtils.tagAsStructurallyChanged(newState)
+    StateUtils.resetStructurallyChangedTypes(newState)
+    modelManager.setLastBuiltState(getProject, newState)
+    javaDepends
+  }
+  
+  private def ensureProject = {
     if (scalaJavaBuilder.getProject == null)
       scalaJavaBuilder.setProject0(getProject)
   }
