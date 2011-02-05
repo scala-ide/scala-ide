35750611d158a355fedf6b174ba26933b3d5a964
eugene.vigdorchik <eugene.vigdorchik@gmail.com>
2011-02-05 15:24:35 +0300

    Indent proper.

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/util/IDESettings.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/util/IDESettings.scala
@@ -7,36 +7,36 @@ package scala.tools.eclipse.util
 
 import scala.tools.nsc.Settings
 
-
 object IDESettings {
-	case class Box(name: String, userSettings: List[Settings#Setting])
+  case class Box(name: String, userSettings: List[Settings#Setting])
+  
   def shownSettings(s : Settings) : List[Box] = {
     import s._
 
     List(
-    		Box("Standard",
-    				List(deprecation, g, optimise, target, unchecked,
-                 pluginOptions, nospecialization, verbose)),
-    		Box("Advanced",
-    				List(checkInit, Xchecknull, elidebelow,
-    						 Xexperimental, future, XlogImplicits,
-    						 Xmigration28, noassertions, nouescape, plugin, disable,
-    						 require, pluginsDir, Xwarnfatal, Xwarninit)),
-    		Box("Private",
-    				List(Xcloselim, Xdce, inline, Xlinearizer, Ynogenericsig, noimports,
-    						 selfInAnnots, Yrecursion, refinementMethodDispatch,
-    						 Ywarndeadcode, Ybuildmanagerdebug)),
-        Box("Presentation Compiler",
-            List(YpresentationDebug, YpresentationVerbose, YpresentationLog, YpresentationReplay)))
+      Box("Standard",
+        List(deprecation, g, optimise, target, unchecked,
+             pluginOptions, nospecialization, verbose)),
+      Box("Advanced",
+    	List(checkInit, Xchecknull, elidebelow,
+             Xexperimental, future, XlogImplicits,
+             Xmigration28, noassertions, nouescape, plugin, disable,
+             require, pluginsDir, Xwarnfatal, Xwarninit)),
+      Box("Private",
+        List(Xcloselim, Xdce, inline, Xlinearizer, Ynogenericsig, noimports,
+             selfInAnnots, Yrecursion, refinementMethodDispatch,
+             Ywarndeadcode, Ybuildmanagerdebug)),
+      Box("Presentation Compiler",
+        List(YpresentationDebug, YpresentationVerbose, YpresentationLog, YpresentationReplay)))
   }
   
   def pluginSettings: List[Box] = {
-	  import ScalaPluginSettings._
+   import ScalaPluginSettings._
     val Yplugininfo = BooleanSetting("-plugininfo", "Print Scala plugin debugging info")
-	  List(Box("Scala Plugin Debugging", List(Yplugininfo)))    
+    List(Box("Scala Plugin Debugging", List(Yplugininfo)))    
   }
 }
 
 object ScalaPluginSettings extends Settings {
-	val YPlugininfo = BooleanSetting("-plugininfo", "Enable logging of the Scala Plugin info")
+  val YPlugininfo = BooleanSetting("-plugininfo", "Enable logging of the Scala Plugin info")
 }
