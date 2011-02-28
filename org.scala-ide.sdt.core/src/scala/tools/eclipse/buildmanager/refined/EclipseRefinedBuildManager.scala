6a9a16d2ab9039433ff3a6e2bbb32ee4ab5242ea
David Bernard <david.bernard.31@gmail.com>
2011-02-28 23:00:27 +0100

    EclipseRefinedBuildManager : restore change introduce to previous EclipseBuildManager : doesn't clear pendingSources after build but before to be more ThreadSafe and to update it more correctly if there is error

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/buildmanager/refined/EclipseRefinedBuildManager.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/buildmanager/refined/EclipseRefinedBuildManager.scala
@@ -10,12 +10,12 @@ import scala.tools.nsc.interactive.{BuildManager, RefinedBuildManager}
 import scala.tools.nsc.io.AbstractFile
 import scala.tools.nsc.reporters.Reporter
 import org.eclipse.core.runtime.NullProgressMonitor
-import scala.tools.eclipse.util.{ EclipseResource, FileUtils, Defensive}
+import scala.tools.eclipse.util.{ EclipseResource, FileUtils, Defensive, Tracer}
 
 class EclipseRefinedBuildManager(val project : ScalaProject, settings0: Settings) extends RefinedBuildManager(settings0) with EclipseBuildManager {
   var depFile:IFile = project.underlying.getFile(".scala_dependencies")
   private var monitor : IProgressMonitor = new NullProgressMonitor()
-  val pendingSources = new HashSet[IFile]
+  private lazy val pendingSources = new HashSet[IFile] ++ project.allSourceFiles() // first run should build every sources
   
   class EclipseBuildCompiler(settings : Settings, reporter : Reporter) extends BuilderGlobal(settings, reporter) {
     
@@ -44,7 +44,7 @@ class EclipseRefinedBuildManager(val project : ScalaProject, settings0: Settings
               FileUtils.clearTasks(i, monitor)
             case _ => 
           }
-          if (Defensive.notNull(file, "try to compile file == null"))
+          if (Defensive.notNull(file, "try to compile file == null")) {
             super.compileLate(file)
           }
         }
@@ -56,19 +56,30 @@ class EclipseRefinedBuildManager(val project : ScalaProject, settings0: Settings
     monitor = if (pm == null) new NullProgressMonitor() else pm
     
     pendingSources ++= addedOrUpdated
+
+    val pendingSources0 = pendingSources ++ addedOrUpdated
+    pendingSources.clear
     val removedFiles = removed.map(EclipseResource(_) : AbstractFile)
-    val toBuild = pendingSources.map(EclipseResource(_)) ++ unbuilt -- removedFiles
-    hasErrors = false
+    val unbuilt0 = unbuilt
+    val toBuild = pendingSources0.map(EclipseResource(_)) ++ unbuilt0 -- removedFiles
+    Tracer.printlnItems("ask to build (addedOrUpdated)  ", addedOrUpdated)
+    Tracer.printlnItems("ask to build (pendingSources)  ", pendingSources)
+    Tracer.printlnItems("ask to build (unbuilt)         ", unbuilt0)
+    Tracer.printlnItems("ask to build (removed)         ", removedFiles)
+    var hasErrors = false
+    clearBuildErrors(monitor)
     try {
       super.update(toBuild, removedFiles)
+      hasErrors = compiler.reporter.ERROR.count > 0      
     } catch {
       case e =>
         hasErrors = true
         buildError(IMarker.SEVERITY_ERROR, "Error in Scala compiler: " + e.getMessage)
         ScalaPlugin.plugin.logError("Error in Scala compiler", e)
     }
-    if (!hasErrors)
-      pendingSources.clear
+    Tracer.println("build ends with %d error(s), %d warnings".format(compiler.reporter.ERROR.count, compiler.reporter.WARNING.count))
+    if (hasErrors)
+      pendingSources ++= pendingSources0
       
     saveTo(EclipseResource(depFile), _.toString)
     depFile.setDerived(true)
@@ -82,7 +93,7 @@ class EclipseRefinedBuildManager(val project : ScalaProject, settings0: Settings
       if (targets(src).exists(!_.exists))
         missing += src
     
-    Set.empty ++ missing
+    missing.toSet
   }
   
   override def newCompiler(settings: Settings) = new EclipseBuildCompiler(settings,
