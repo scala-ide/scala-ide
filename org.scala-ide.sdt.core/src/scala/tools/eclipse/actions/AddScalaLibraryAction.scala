146d58496cb89f2d6f9c81e6ee8903b4c1ed6fe3
Donna Malayeri <lindydonna@gmail.com>
2011-03-16 11:34:23 +0100

    Add popup menu action to add scala library to classpath.

---
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/actions/AddScalaLibraryAction.scala
@@ -0,0 +1,12 @@
+package scala.tools.eclipse
+package actions
+
+import org.eclipse.core.resources.IProject
+import org.eclipse.ui.IWorkbenchPart
+import org.eclipse.jface.action.IAction
+
+class AddScalaLibraryAction extends AbstractPopupAction {
+  def performAction(project: IProject) {  
+    Nature.addScalaLibAndSave(project)
+  }
+}
