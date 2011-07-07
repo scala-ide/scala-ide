4fb8f402aec6c343c2171f8eedb6e6199548ad64
Iulian Dragos <jaguarul@gmail.com>
2011-07-07 13:06:13 +0200

    Forgot test file.

---
+++ org.scala-ide.sdt.core.tests/test-workspace/simple-structure-builder/src/annots/ScalaTestSuite.scala
@@ -0,0 +1,16 @@
+package annots
+import org.junit._
+
+class ScalaTestSuite {
+  
+  @Before def prepare() {
+  }
+  
+  @org.junit.Test
+  def someTestMethod() {
+  }
+  
+  @Test def anotherTestMethod() {
+    
+  }
+}
