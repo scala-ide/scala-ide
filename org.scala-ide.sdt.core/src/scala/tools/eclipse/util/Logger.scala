443e5f46aee8c850694b684ebb0ec3291a485ac6
Iulian Dragos <jaguarul@gmail.com>
2011-07-08 17:34:22 +0200

    Formatting.

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/util/Logger.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/util/Logger.scala
@@ -2,12 +2,12 @@ package scala.tools.eclipse.util
 
 trait Logger {
   object Category extends Enumeration {
-	val INFO, ERROR = Value
+    val INFO, ERROR = Value
   }
-  
+
   import Category._
-  def log(message : String, cat : Value = INFO) = {
-	val printer = if (cat eq INFO) System.out else System.err
-	printer.format("[%s] %s%n", getClass.getSimpleName, message)
+  def log(message: String, cat: Value = INFO) = {
+    val printer = if (cat eq INFO) System.out else System.err
+    printer.format("[%s] %s%n", getClass.getSimpleName, message)
   }
 }
