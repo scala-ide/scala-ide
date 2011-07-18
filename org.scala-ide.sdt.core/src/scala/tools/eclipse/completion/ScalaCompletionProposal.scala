1a54b4914201031471dfa09926eb1df6637b3a53
Iulian Dragos <jaguarul@gmail.com>
2011-07-18 18:10:26 +0200

    Place the cursor between parenthesis, if the completion contains parenthesis.

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/completion/ScalaCompletionProposal.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/completion/ScalaCompletionProposal.scala
@@ -6,12 +6,13 @@ import org.eclipse.swt.graphics.Image
 import org.eclipse.jface.text.IDocument
 import org.eclipse.jface.viewers.ISelectionProvider
 import org.eclipse.jface.text.TextSelection
+import org.eclipse.jface.text.ITextViewer
 
 private[completion] class ScalaCompletionProposal(startPos: Int,
-  completion: String,
-  display: String,
-  contextName: String,
-  container: String,
+  completion: String,        // the string to be inserted in the document
+  display: String,           // the display string in the completion list
+  tooltip: String,           // tooltop info showed after a completion has been selected
+  additionalInfo: String,    // info displayed on the right of the current completion selection
   relevance: Int,
   image: Image,
   selectionProvider: ISelectionProvider)
@@ -21,21 +22,28 @@ private[completion] class ScalaCompletionProposal(startPos: Int,
   def getRelevance() = relevance
   def getImage() = image
   def getContextInformation(): IContextInformation =
-    if (contextName.size > 0)
-      new ScalaContextInformation(display, contextName, image)
+    if (tooltip.size > 0)
+      new ScalaContextInformation(display, tooltip, image)
     else null
 
   def getDisplayString() = display
-  def getAdditionalProposalInfo() = container
+  def getAdditionalProposalInfo() = additionalInfo
   def getSelection(d: IDocument) = null
   def apply(d: IDocument) { throw new IllegalStateException("Shouldn't be called") }
 
   def apply(d: IDocument, trigger: Char, offset: Int) {
     d.replace(startPos, offset - startPos, completion)
     selectionProvider.setSelection(new TextSelection(startPos + completion.length, 0))
+    selectionProvider match {
+      case viewer: ITextViewer if completion.endsWith("()") =>
+        viewer.getTextWidget().setCaretOffset(startPos + completion.length - 1)
+      case _ => () 
+    }
   }
   def getTriggerCharacters = null
   def getContextInformationPosition = 0
-  def isValidFor(d: IDocument, pos: Int) = prefixMatches(completion.toArray, d.get.substring(startPos, pos).toArray)
+  def isValidFor(d: IDocument, pos: Int) = 
+    prefixMatches(completion.toArray, 
+        d.get.substring(startPos, pos).toArray)
 } 
 
