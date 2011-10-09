289ed02d64a4b0446fe7a391311e34ccf60b7cd0
Mirco Dotta <mirco.dotta@typesafe.com>
2011-10-09 23:10:39 +0200

    Simplified test case for reproducing the error reported in ticket Re #1000658.

--- org.scala-ide.sdt.core.tests/test-workspace/pc/src/t1000658/ThreadPoolConfig.scala
+++ org.scala-ide.sdt.core.tests/test-workspace/pc/src/t1000658/ThreadPoolConfig.scala
@@ -1,15 +1,7 @@
 package t1000658
 
-import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy
-import java.util.concurrent.RejectedExecutionHandler
-
 object ThreadPoolConfig {
-  type Bounds = Int
-  type FlowHandler = Either[RejectedExecutionHandler, Bounds]
-  
-  def defaultFlowHandler: FlowHandler = flowHandler(new CallerRunsPolicy)
-  
-  def flowHandler(rejectionHandler: RejectedExecutionHandler): FlowHandler = Left(rejectionHandler)
+  type FlowHandler = String
 }
 
-case class ThreadPoolConfig(flowHandler: ThreadPoolConfig.FlowHandler = ThreadPoolConfig.defaultFlowHandler)
+case class ThreadPoolConfig(flowHandler: ThreadPoolConfig.FlowHandler = "")
