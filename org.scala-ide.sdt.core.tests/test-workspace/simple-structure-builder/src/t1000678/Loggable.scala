6c609ca513ed918bba508276dde4f4af6fac82c8
Mirco Dotta <mirco.dotta@typesafe.com>
2011-10-24 17:42:55 +0200

    Updated ScalaStructureBuilder, as it is responsible of correctly exposing module's inner classes to JDT. The issue was that module's inner classes were exposed as member of the $ module class, which is wrong, since the scala compiler expose them as inner classes of the module's companion class.
    I moved the code that was responsible of traversing the Scala AST Tree into a new class 'TreeTraverser', that takes a new field 'module2innerClassDefs' used to keep track of inner classes that are defined in a module. This solution has the advantage of avoiding possible memory leaks that could result from not correctly cleaning the 'module2innerClassDefs' map.
    
    Fixed ticket #1000678.

---
+++ org.scala-ide.sdt.core.tests/test-workspace/simple-structure-builder/src/t1000678/Loggable.scala
@@ -0,0 +1,5 @@
+package t1000678
+
+object Loggable {
+  case class RunUnit(val id : Integer) 
+}
---
+++ org.scala-ide.sdt.core.tests/test-workspace/simple-structure-builder/src/t1000678_1/JavaTest.java
@@ -0,0 +1,7 @@
+package t1000678_1;
+
+public class JavaTest {
+	public String usingScalaStatic() {
+		return Test.scalaStatic();
+	}
+}
