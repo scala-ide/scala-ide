fb7e41f946206609c819b3ce7164914612a108bb
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2011-10-27 22:06:34 +0200

    Build plugin with new sbt version 0.11.3. This should fix a couple of builder bugs

--- org.scala-ide.build-toolchain/build-toolchain-2.8.x.sh
+++ org.scala-ide.build-toolchain/build-toolchain-2.8.x.sh
@@ -3,19 +3,19 @@
 . $(dirname $0)/env.sh
 
 # Scala compiler version to be used as a dependency
-SCALA_VERSION=2.8.2-SNAPSHOT
+SCALA_VERSION=2.8.3-SNAPSHOT
 
 #THIS NEEDS TO BE UPDATED ONCE WE HAVE SNAPSHOTS
 
 # The precompiled compiler-interface.jar that SBT uses to communicate with scalac
 # This is usually the same as Scala version, and it is not enough to be just binary 
 # compatible (compiler interface depends on scalac, not just std library)
-SCALA_PRECOMPILED=2_8_2-SNAPSHOT
+SCALA_PRECOMPILED=2_8_3-SNAPSHOT
 
 # The scala version that was used to compile SBT dependencies (it is part of the artifact name)
 # It is usually enough to have a binary compatible version of SCALA_VERSION
-# For instance, here we can use 2.9.0, for a 2.9.1 compiler
-SBT_SCALA_VERSION=2.8.1
+# For instance, here we can use 2.8.0, for a 2.8.1 compiler
+SBT_SCALA_VERSION=2.8.3-SNAPSHOT
 
 SBINARY_VERSION=0.4.0
 
