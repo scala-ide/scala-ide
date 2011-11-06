5f1eda90d6b2f48b71a3c4df63bad725c38a5a9d
Iulian Dragos <dragos@iuli-mbp.local>
2011-11-06 19:25:27 +0100

    Decoupled the Scala library version from the Scala compiler version when building the toolchain. Build the toolchain by using the stable 2.9.1 scala library, and the 2.9.2-SNAPSHOT compiler.
    Cleaned up build profiles and removed obsolete scripts.(cherry picked from commit 8c8419f5e9a6187503681bf92c050accfa978f1c)

--- org.scala-ide.build/build-ide-local-2.9.x.sh
+++ org.scala-ide.build/build-ide-local-2.9.x.sh
@@ -2,7 +2,9 @@
 
 . $(dirname $0)/env.sh
 
-SCALA_VERSION=2.9.0-SNAPSHOT
-PROFILE_NAME="-P local-scala-2.9.x,!scala-trunk"
+SCALA_VERSION=2.9.2-SNAPSHOT
+SCALA_LIBRARY_VERSION=2.9.1
+
+PROFILE_NAME="-P local-scala-2.9.x,!scala-trunk,!scala-2.9.2-SNAPSHOT"
 
 build $*
