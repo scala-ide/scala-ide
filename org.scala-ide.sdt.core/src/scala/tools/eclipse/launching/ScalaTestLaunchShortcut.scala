741aa1e1b724efbf8c5c80274e1059f7d389d4ab
cheeseng <cheeseng@amaseng.com>
2012-02-08 21:24:49 +0800

    Added code to transform AST in eclipse to AST used by ScalaTest's finder.

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/launching/ScalaTestLaunchShortcut.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/launching/ScalaTestLaunchShortcut.scala
@@ -25,8 +25,88 @@ import scala.annotation.tailrec
 import scala.tools.nsc.util.Position
 import scala.tools.nsc.util.Position$
 //import org.scalatest.tools.Runner
+import org.scalatest.spi.location.AstNode
 
 class ScalaTestLaunchShortcut extends ILaunchShortcut {
+  
+  trait TreeSupport {
+  
+    type Tree = Trees#Tree
+  
+    def getParent(className: String, root: Tree, node: Tree): AstNode = {
+      val parentTreeOpt = getParentTree(root, node)
+      parentTreeOpt match {
+        case Some(parentTree) =>
+          transformAst(className, parentTree, root).getOrElse(null)
+        case None => 
+          null
+      }
+    }
+  
+    def getChildren(className: String, root: Tree, node: Tree): Array[AstNode] = {
+      val children: List[Tree] = node match {
+        case apply: Trees#Apply =>
+          if (apply.children.length > 0)
+            apply.children.last.children
+          else
+            List.empty
+        case _ =>
+          node.children
+      }
+      children.map(mapAst(className, _, root)).filter(_.isDefined).map(_.get).toArray
+    }
+  }
+  
+  class ConstructorBlock(pClassName: String, rootTree: Trees#Tree, nodeTree: Trees#Tree) 
+    extends org.scalatest.spi.location.ConstructorBlock(pClassName, Array.empty) with TreeSupport {
+    override lazy val children = {
+      val rawChildren = getChildren(pClassName, rootTree, nodeTree).toList
+      // Remove the primary constructor method definition.
+      rawChildren match {
+        case primary :: rest => 
+          if (primary.isInstanceOf[MethodDefinition] && primary.name == "this")
+            rest.toArray
+          else
+            rawChildren.toArray
+        case _ =>
+          rawChildren.toArray
+      }
+    }
+  }
+
+  class MethodDefinition(
+    pClassName: String,
+    rootTree: Trees#Tree,
+    nodeTree: Trees#Tree,
+    pName: String, 
+    pParamTypes: String*)
+    extends org.scalatest.spi.location.MethodDefinition(pClassName, null, Array.empty, pName, pParamTypes.toList: _*) with TreeSupport {
+    override lazy val parent = getParent(pClassName, rootTree, nodeTree)
+    override lazy val children = getChildren(pClassName, rootTree, nodeTree)
+  }
+  
+  class MethodInvocation(
+    pClassName: String,
+    pTarget: AstNode, 
+    rootTree: Trees#Tree,
+    nodeTree: Trees#Tree,
+    pName: String, 
+    pArgs: AstNode*)
+    extends org.scalatest.spi.location.MethodInvocation(pClassName, pTarget, null, Array.empty, pName, pArgs.toList: _*) with TreeSupport {
+    override lazy val parent = getParent(pClassName, rootTree, nodeTree)
+    override lazy val children = getChildren(pClassName, rootTree, nodeTree)
+  }
+  
+  class StringLiteral(pClassName: String, rootTree: Trees#Tree, nodeTree: Trees#Tree, pValue: String)
+    extends org.scalatest.spi.location.StringLiteral(pClassName, null, pValue) with TreeSupport {
+    override lazy val parent = getParent(pClassName, rootTree, nodeTree)
+  }
+  
+  class ToStringTarget(pClassName: String, rootTree: Trees#Tree, nodeTree: Trees#Tree, target: AnyRef) 
+    extends org.scalatest.spi.location.ToStringTarget(pClassName, null, Array.empty, target) with TreeSupport {
+    override lazy val parent = getParent(pClassName, rootTree, nodeTree)
+    override lazy val children = getChildren(pClassName, rootTree, nodeTree)
+  }
   
   type Tree = Trees#Tree
   
@@ -37,20 +117,14 @@ class ScalaTestLaunchShortcut extends ILaunchShortcut {
   
   def launch(editorPart:IEditorPart, mode:String) {
     // This get called when user right-clicked within the opened file editor and choose 'Run As' -> ScalaTest
-    //val element: ScalaElement = resolveSelectedElement(editorPart, JavaUI.getEditorInputTypeRoot(editorPart.getEditorInput()))
     val element = resolveSelectedAst(editorPart, JavaUI.getEditorInputTypeRoot(editorPart.getEditorInput()))
     if(element == null)
       println("#####Launch all suites within the source file")
     else {
-      //val elementParent = element.getParent()
-      //println("#####Launch selected element: " + element)
-      println("#####Launch selected element: " + element.getClass.getName + ", children: " + element.children.length)
+      val children = element.children
+      println("#####Launch selected element: " + element.getClass.getName + ", children: " + children.map(_.getClass.getName).mkString(", "))
     }
-    
-    //scProject.withPresentationCompiler[Unit] { compiler => 
-      //compiler.locateTree(pos)
-    //}
-    //Runner.run(Array());
+    //Runner.run(Array("-o"));
   }
   
   @tailrec
@@ -67,8 +141,7 @@ class ScalaTestLaunchShortcut extends ILaunchShortcut {
   }
   
   @tailrec
-  private def getParent(candidate: Tree, node: Tree): Option[Tree] = {
-    import scala.collection.mutable.ListBuffer
+  final def getParentTree(candidate: Tree, node: Tree): Option[Tree] = {
     val foundOpt = candidate.children.find(c => c == node)
     foundOpt match {
       case Some(a) =>
@@ -83,67 +156,78 @@ class ScalaTestLaunchShortcut extends ILaunchShortcut {
         }
         nextCandidateOpt match {
           case Some(nextCandidate) => 
-            getParent(nextCandidate, node)
+            getParentTree(nextCandidate, node)
           case None => 
             None
         }
     }
   }
-  
-  private def getTarget(apply: Trees#Apply): String = {
+   
+  private def getTarget(className: String, nodeTree: Tree, rootTree: Tree, apply: Trees#Apply): AstNode = {
     apply.fun match {
       case select: Trees#Select => 
         val q = select.qualifier
         select.qualifier match {
           case lit: Trees#Literal =>
-            lit.value.stringValue
+            new ToStringTarget(className, rootTree, nodeTree, lit.value.stringValue)
           case impl: scala.tools.nsc.ast.Trees$ApplyImplicitView => 
             val implFirstArg: Tree = impl.args(0)
             implFirstArg match {
               case litArg: Trees#Literal =>
-                litArg.value.stringValue
+                new ToStringTarget(className, rootTree, nodeTree, litArg.value.stringValue)
               case _ => 
-                implFirstArg.toString
+                new ToStringTarget(className, rootTree, nodeTree, implFirstArg.toString)
             }
           case _ =>
-            select.qualifier.toString
+            new ToStringTarget(className, rootTree, nodeTree, select.qualifier.toString)
         }
       case _ =>
-        apply.fun.toString
+        new ToStringTarget(className, rootTree, nodeTree, apply.fun.toString)
     }
   }
   
-  private def transformAst(selectedTree: Tree, root: Tree): Unit = {
-    println("#####selectedTree: " + selectedTree.getClass.getName)
+  private def mapAst(className: String, selectedTree: Tree, rootTree: Tree): Option[AstNode] = {    
     selectedTree match {
       case defDef: Trees#DefDef =>
         val defDefSym = defDef.symbol
-        println("#####name: " + defDefSym.decodedName)
         //This does not work?  Error: value paramsType is not a member of Symbols.this.Type
         //println("#####param types: " + defDefSym.info.paramTypes)
-        println("#####param types: " + defDef.vparamss.flatten.toList.map(valDef => valDef.tpt.symbol.fullName))
+        Some(new MethodDefinition(className, rootTree, selectedTree, defDefSym.decodedName, defDef.vparamss.flatten.toList.map(valDef => valDef.tpt.symbol.fullName): _*))
       case apply: Trees#Apply =>
-        println("#####target: " + getTarget(apply))
-        println("#####name: " + apply.symbol.decodedName)
-        println("#####params: " + apply.args.map(arg => arg match {
+        val target = getTarget(className, selectedTree, rootTree, apply)
+        val name = apply.symbol.decodedName
+        val args = apply.args.map(arg => arg match {
           case lit: Trees#Literal =>
-            lit.value.stringValue
+            new StringLiteral(className, rootTree, selectedTree, lit.value.stringValue)
           case _ =>
-            arg.toString
-        }))
+            new ToStringTarget(className, rootTree, selectedTree, arg.toString)
+        })
+        Some(new MethodInvocation(className, target, rootTree, selectedTree, name, args: _*))
       case template: Trees#Template =>
-        println("#####children count: " + template.children.length)
+        Some(new ConstructorBlock(className, rootTree, selectedTree))
       case _ =>
-        val parentOpt = getParent(root, selectedTree)
+        None
+    }
+  }
+  
+  private def transformAst(className: String, selectedTree: Tree, rootTree: Tree): Option[AstNode] = {
+    val astNodeOpt = mapAst(className, selectedTree, rootTree)
+    println("#####selectedTree: " + selectedTree.getClass.getName + ", astNodeOpt: " + astNodeOpt)
+    astNodeOpt match {
+      case Some(astNode) => astNodeOpt
+      case None => 
+        val parentOpt = getParentTree(rootTree, selectedTree)
         parentOpt match {
           case Some(parent) =>
-            transformAst(parent, root)
+            println("#####parent is: " + parent.getClass.getName)
+            transformAst(className, parent, rootTree)
           case None =>
+            None
         }
     }
   }
   
-  def resolveSelectedAst(editorPart: IEditorPart, typeRoot: ITypeRoot): Tree = {
+  def resolveSelectedAst(editorPart: IEditorPart, typeRoot: ITypeRoot): AstNode = {
     val selectionProvider:ISelectionProvider = editorPart.getSite().getSelectionProvider()
     if(selectionProvider == null)
       return null
@@ -169,13 +253,7 @@ class ScalaTestLaunchShortcut extends ILaunchShortcut {
             
             val position = new OffsetPosition(scu.createSourceFile, textSelection.getOffset)
             val selectedTree = compiler.locateTree(position)
-            transformAst(selectedTree, rootTree)
-            val parentOpt = getParent(rootTree, selectedTree)
-            parentOpt match {
-              case Some(parent) => println("#####Found!!, parent is: " + parent.getClass.getName + ", toString: " + parent)
-              case None => println("#####Parent Not Found")
-            }
-            compiler.locateTree(position)
+            transformAst(classElement.getFullyQualifiedName, selectedTree, rootTree).getOrElse(null)
           } (null)
         case _ =>
           return null
