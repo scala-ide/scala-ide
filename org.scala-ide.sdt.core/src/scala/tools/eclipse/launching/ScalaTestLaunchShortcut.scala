d2d8e60eac76560a5a8663dfa52877df64ac97d1
cheeseng <cheeseng@amaseng.com>
2012-03-01 12:00:44 +0800

    Added shortcut support to launch selected file.

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/launching/ScalaTestLaunchShortcut.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/launching/ScalaTestLaunchShortcut.scala
@@ -35,6 +35,42 @@ import org.eclipse.debug.ui.DebugUITools
 import org.eclipse.jface.dialogs.MessageDialog
 import ScalaTestLaunchConstants._
 
+class ScalaTestFileLaunchShortcut extends ILaunchShortcut {
+  
+  def launch(selection:ISelection, mode:String) {
+    // This get called when user right-clicked .scala file on package navigator and choose 'Run As' -> ScalaTest
+    // Should just run all suites within the selected file.
+  }
+  
+  def launch(editorPart:IEditorPart, mode:String) {
+    val typeRoot = JavaUI.getEditorInputTypeRoot(editorPart.getEditorInput)
+    typeRoot match {
+      case scSrcFile: ScalaSourceFile => 
+        val configType = getLaunchManager.getLaunchConfigurationType("scala.scalatest")
+          val existingConfigs = getLaunchManager.getLaunchConfigurations(configType)
+          val simpleName = scSrcFile.getElementName
+          val existingConfigOpt = existingConfigs.find(config => config.getName == simpleName)
+          val config = existingConfigOpt match {
+                         case Some(existingConfig) => existingConfig
+                         case None => 
+                           val wc = configType.newInstance(null, getLaunchManager.generateUniqueLaunchConfigurationNameFrom(simpleName))
+                           val project = editorPart.getEditorInput.asInstanceOf[IFileEditorInput].getFile.getProject
+                           val scProject = ScalaPlugin.plugin.getScalaProject(project)
+                           wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, scSrcFile.getPath.toPortableString)
+                           wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, project.getName)
+                           wc.setAttribute(SCALATEST_LAUNCH_TYPE_NAME, TYPE_FILE)
+                           wc.setAttribute(SCALATEST_LAUNCH_INCLUDE_NESTED_NAME, INCLUDE_NESTED_FALSE)
+                           wc.doSave
+                         }
+          DebugUITools.launch(config, mode)
+      case _ => 
+        MessageDialog.openError(null, "Error", "Please select a Scala source file.")
+    }
+  }
+  
+  private def getLaunchManager = DebugPlugin.getDefault.getLaunchManager
+}
+
 class ScalaTestSuiteLaunchShortcut extends ILaunchShortcut {
   
   def launch(selection:ISelection, mode:String) {
@@ -79,8 +115,6 @@ class ScalaTestSuiteLaunchShortcut extends ILaunchShortcut {
   }
   
   private def getLaunchManager = DebugPlugin.getDefault.getLaunchManager
-  
-  
 }
 
 class ScalaTestTestLaunchShortcut extends ILaunchShortcut {
