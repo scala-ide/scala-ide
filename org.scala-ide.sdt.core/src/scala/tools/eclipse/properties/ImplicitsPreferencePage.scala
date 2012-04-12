2e2dea6ff04dafd34f20136033ab58b6ec32afe8
Iulian Dragos <jaguarul@gmail.com>
2012-04-12 12:05:52 +0200

    Better preferences and defaults for implicit highlighting.
    - switched to dark green and underlined for highlighting annotations
    - added hyperlinks from the Implicits preferences to the Annotations page.

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/properties/ImplicitsPreferencePage.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/properties/ImplicitsPreferencePage.scala
@@ -4,20 +4,36 @@ package properties
 import org.eclipse.jface.preference._
 import org.eclipse.ui.IWorkbenchPreferencePage
 import org.eclipse.ui.IWorkbench
-
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer
 import org.eclipse.jface.preference.IPreferenceStore
-
 import scala.tools.eclipse.ScalaPlugin
+import org.eclipse.swt.widgets.Link
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.swt.widgets.Control
+import org.eclipse.jdt.internal.ui.preferences.PreferencesMessages
+import org.eclipse.swt.SWT
+import org.eclipse.swt.events.SelectionEvent
+import org.eclipse.ui.dialogs.PreferencesUtil
 
 class ImplicitsPreferencePage extends FieldEditorPreferencePage with IWorkbenchPreferencePage {
   import ImplicitsPreferencePage._
+  import util.SWTUtils._
 
   setPreferenceStore(ScalaPlugin.plugin.getPreferenceStore)
   setDescription("""
-	    Set the highlighting for implicit conversions and implicit parameters.
-	    See also General > Editors > Text Editors > Annotations (Scala Implicit)
+Set the highlighting for implicit conversions and implicit parameters.
   """)
+
+  override def createContents(parent: Composite): Control = {
+    val control = super.createContents(parent).asInstanceOf[Composite]
+    val link = new Link(control, SWT.NONE)
+    link.setText("""More options for highlighting for implicit conversions on the <a href="org.eclipse.ui.preferencePages.GeneralTextEditor">Text Editors</a>/<a href="org.eclipse.ui.editors.preferencePages.Annotations">Annotations</a> preference page.""")
+    link.addSelectionListener { e: SelectionEvent =>
+      PreferencesUtil.createPreferenceDialogOn(parent.getShell, e.text, null, null)
+    }
+
+    control
+  }
 
   override def createFieldEditors() {
     addField(new BooleanFieldEditor(P_ACTIVE, "Active", getFieldEditorParent))
@@ -46,5 +62,4 @@ class ImplicitsPagePreferenceInitializer extends AbstractPreferenceInitializer {
     store.setDefault(P_BOLD, false)
     store.setDefault(P_ITALIC, false)
   }
-
 }
