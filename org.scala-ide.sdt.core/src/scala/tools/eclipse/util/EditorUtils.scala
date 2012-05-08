1aa2c020d1c95ce9307b13d63980d0aa73fcd42b
Mirco Dotta <mirco.dotta@typesafe.com>
2012-05-08 14:10:42 +0200

    Added FIXME note on ``EditorUtils``
    ``scala.tools.eclipse.refactoring.EditorHelpers`` and
    ``scala.tools.eclipse.util.EditorUtils`` should be merged.

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/util/EditorUtils.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/util/EditorUtils.scala
@@ -1,16 +1,17 @@
 package scala.tools.eclipse.util
 
+import scala.collection.JavaConverters.asScalaIteratorConverter
+
 import org.eclipse.jdt.internal.core.JavaElement
-import org.eclipse.jface.text.Position
-import org.eclipse.jface.text.source.Annotation
-import org.eclipse.ui.IEditorPart
-import scala.collection.JavaConverters._
-import org.eclipse.jface.text.source.IAnnotationModelExtension2
-import scala.tools.eclipse.ScalaWordFinder
 import org.eclipse.jface.text.IRegion
 import org.eclipse.jface.text.ITextSelection
+import org.eclipse.jface.text.Position
 import org.eclipse.jface.text.Region
+import org.eclipse.jface.text.source.Annotation
+import org.eclipse.jface.text.source.IAnnotationModelExtension2
+import org.eclipse.ui.IEditorPart
 
+// FIXME: This should be merged with merged with {{{scala.tools.eclipse.refactoring.EditorHelpers}}}
 object EditorUtils {
 
   def openEditorAndApply[T](element: JavaElement)(editor: IEditorPart => T): T =
