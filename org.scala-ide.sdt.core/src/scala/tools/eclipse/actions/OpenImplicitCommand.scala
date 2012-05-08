26d2e2229db11475ca4cefa268338ee112c8f7fb
Mirco Dotta <mirco.dotta@typesafe.com>
2012-05-08 14:09:33 +0200

    Implemented enable/disable of "Open Implicit" menu item

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/actions/OpenImplicitCommand.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/actions/OpenImplicitCommand.scala
@@ -1,12 +1,13 @@
 package scala.tools.eclipse.actions
 
 import scala.tools.eclipse.ScalaSourceFileEditor
+import scala.tools.eclipse.hyperlink.text.detector.ImplicitHyperlinkDetector
+import scala.tools.eclipse.refactoring.EditorHelpers
+import scala.tools.eclipse.ui.actions.HyperlinkOpenAction
+
 import org.eclipse.core.commands.AbstractHandler
 import org.eclipse.core.commands.ExecutionEvent
 import org.eclipse.ui.handlers.HandlerUtil
-import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility
-import scala.tools.eclipse.ui.actions.HyperlinkOpenAction
-import scala.tools.eclipse.hyperlink.text.detector.ImplicitHyperlinkDetector
 
 class OpenImplicitCommand extends AbstractHandler {
   override def execute(event: ExecutionEvent): Object = {
@@ -17,4 +18,8 @@ class OpenImplicitCommand extends AbstractHandler {
     }
     null
   }
+  
+  override def isEnabled: Boolean = EditorHelpers.withCurrentEditor { editor =>
+    Some(new HyperlinkOpenAction(ImplicitHyperlinkDetector(), editor).isEnabled)
+  }.getOrElse(false)
 }
