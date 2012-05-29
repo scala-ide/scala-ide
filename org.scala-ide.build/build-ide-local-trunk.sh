bfc88fd04d80d68760e43dbe60b638c314280f0d
Luc Bourlier <luc.bourlier@typesafe.com>
2012-05-29 17:44:54 +0200

    Major refactoring of the maven build.
    No bash script needed anymore. Everything is managed through maven.
    The new pom at in root folder contains version information and general configuration.
    org.scala-ide.build-toolchain is the root of the toolchain part
    org.scala-ide.sbt.build is the root of the sdt part
    The version numbers of some plugins has been upgraded
    
    The downloaded libs have been moved to the target/lib folders, to simplify the clean up process.
    
    The first time, run './build-all.sh (or './build-all.sh install -P scala-2.10.x' for 2.10) from the root to generate an initial build and install the top pom locally. Then use maven from any project or subproject.
    
    Use the scala-2.10.x profile ('-P scala-2.10.x') to compile the 2.10 version.
    
    Re #1001056

--- org.scala-ide.build/build-ide-local-trunk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-. $(dirname $0)/env.sh
-
-SCALA_VERSION=2.10.0-SNAPSHOT
-SCALA_LIBRARY_VERSION=2.10.0-SNAPSHOT
-PROFILE="-P local-scala-trunk,!scala-trunk"
-
-build $*
--- org.scala-ide.build/build-ide-trunk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-. $(dirname $0)/env.sh
-
-SCALA_VERSION=2.10.0-SNAPSHOT
-
-PROFILE="-P scala-trunk"
-
-build $*
