6bea246f78356cc64726f70e2bdd289e472bb2a4
Mirko Stocker <me@misto.ch>
2012-06-22 17:25:59 +0200

    Pass the user's formatting options to the refactoring library codegen.
    At the moment, only for Organize Imports, but more will be added in
    the future.
    
    Fixes #1001073

---
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/refactoring/UserPreferencesFormatting.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2012 LAMP/EPFL
+ */
+
+package scala.tools.eclipse
+package refactoring
+
+import scala.tools.eclipse.formatter.FormatterPreferences
+import scala.tools.refactoring.Refactoring
+
+import scalariform.formatter.preferences.SpaceInsideParentheses
+
+/**
+ * Enables passing the user's source formatting preferences to the refactoring library's
+ * source code generation.
+ */
+trait UserPreferencesFormatting {
+  this: ScalaIdeRefactoring =>
+  
+  /**
+   * Refactoring actions should mix in this trait when creating a refactoring instance to
+   * automatically pass the user's formatting preferences to the refactoring implementation.
+   */
+  trait FormattingOverrides {
+    this: Refactoring =>
+      
+    override val spacingAroundMultipleImports: String = {
+      for {
+        javaProject <- Option(file.getJavaProject)
+        val prefs = FormatterPreferences.getPreferences(javaProject)
+        if  prefs(SpaceInsideParentheses)
+      } yield " "
+    } getOrElse ""
+    
+    // TODO: Create more overrides here and in the refactoring library.
+  }
+}
