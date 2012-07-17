35a64597e4e9a0484509452e176d92f66e442f5c
Mirco Dotta <mirco.dotta@typesafe.com>
2012-07-17 22:11:19 +0200

    Documented thread safety guarantees of ``EclipseLogger`` and also described its synchronization policy for mantainers

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/logging/EclipseLogger.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/logging/EclipseLogger.scala
@@ -10,10 +10,21 @@ import java.util.concurrent.atomic.AtomicReference
 /** Use the `EclipseLogger` when you want to communicate with the user. Messages logged
  *  through the `EclipseLogger` are persisted in the Error Log.
  *
- *  This class is thread-safe.
+ *  This class is meant to be thread-safe, but it isn't because of a possible race condition existing 
+ *  in the Eclipse Logger. Indeed, the call to `ScalaPlugin.plugin.getLog()` isn't thread-safe, but 
+ *  there is really nothing we can do about it. The issue is that the `logs` map accessed in 
+ *  [[org.eclipse.core.internal.runtime.InternalPlatform.getLog(bundle)]] is not synchronized.
+ *  
+ *  Mantainers should evolve this class by keeping in mind that the `EclipseLogger` will be 
+ *  accessed by different threads at the same time, so it is important to keep this class lock-free, 
+ *  and thread-safe (or maybe a better wording would be: as much thread-safe as it can be).
+ *  And that is actually the main motivation for using an [[java.util.concurrent.atomic.AtomicReference]] 
+ *  for `lastCrash`. Also, note that declaring `lastCrash` as volatile (instead of using a 
+ *  [[java.util.concurrent.atomic.AtomicReference]]) would have made this class less correct (because 
+ *  volatile fields don't offer atomic operations such as `getAndSet`).
  */
 private[logging] object EclipseLogger extends Logger {
-  private final val pluginLogger: ILog = ScalaPlugin.plugin.getLog
+  private val pluginLogger: ILog = ScalaPlugin.plugin.getLog()
 
   private val lastCrash: AtomicReference[Throwable] = new AtomicReference
 
