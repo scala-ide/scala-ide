7835bc0f46d4ec42ced76726edfabf6a8d115ac5
Mirco Dotta <mirco.dotta@typesafe.com>
2012-07-17 09:14:44 +0200

    Optimization to log an exception at most once if the same exception is being (re-)thrown several times
    This used to be an ad-hoc optimization available only in
    ``ScalaCompilationUnit``.  However, I believe this can be useful in general,
    and the optimization belongs to ``EclipseLoggger``.
    
    The former implementation was thread-safe because ``lastCrash`` was always
    accessed within the ``PresentationCompilerThread`` (thread-safety was achieved
    via thread-confinement). However, it was a weak form of thread-safety, as it
    was potentially very easy to break if you didn't understand the thread policy
    of the class (which is not documented).  Implementing this optimization in
    ``EclipseLogger`` forced us to have a stronger form of thread-safety, because
    ``lastCrash`` can be accessed from any thread.

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/logging/EclipseLogger.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/logging/EclipseLogger.scala
@@ -5,9 +5,17 @@ import scala.tools.eclipse.ScalaPlugin
 import scala.tools.eclipse.util.SWTUtils
 import org.eclipse.core.runtime.{ ILog, IStatus }
 import scala.util.control.ControlThrowable
+import java.util.concurrent.atomic.AtomicReference
 
+/** Use the `EclipseLogger` when you want to communicate with the user. Messages logged
+ *  through the `EclipseLogger` are persisted in the Error Log.
+ *
+ *  This class is thread-safe.
+ */
 private[logging] object EclipseLogger extends Logger {
   private final val pluginLogger: ILog = ScalaPlugin.plugin.getLog
+
+  private val lastCrash: AtomicReference[Throwable] = new AtomicReference
 
   def debug(message: => Any) {
     info(message)
@@ -48,23 +56,28 @@ private[logging] object EclipseLogger extends Logger {
   def fatal(message: => Any, t: Throwable) {
     error(message, t)
   }
-  
+
   private def log(severity: Int, message: => Any, t: Throwable = null) {
-    // Because of a potential deadlock in the Eclipse internals (look at #1000914), the log action need to be executed in the UI thread.
-    def log(status: Status) = 
-      if (ScalaPlugin.plugin.headlessMode) pluginLogger.log(status)
-      else SWTUtils.asyncExec { pluginLogger.log(status) }
-    
-    log(createStatus(severity, message, t))
-    t match {
-      // `ControlThrowable` should never (ever!) be caught by user code. If that happens, generate extra noise. 
-      case ce: ControlThrowable =>
-        log(createStatus(IStatus.ERROR, "Incorrectly logged ControlThrowable: " + ce.getClass.getSimpleName + "(" + ce.getMessage + ")", t))
-      case _ => () // do nothing
+    if (t == null) logInUiThread(severity, message, t)
+    else {
+      // this is an optimization to log the exception at most once if the same exception is being re-thrown several times.
+      val oldValue = lastCrash.getAndSet(t)
+      if (oldValue != t) {
+        logInUiThread(severity, message, t)
+        t match {
+          // `ControlThrowable` should never (ever!) be caught by user code. If that happens, generate extra noise. 
+          case ce: ControlThrowable =>
+            logInUiThread(IStatus.ERROR, "Incorrectly logged ControlThrowable: " + ce.getClass.getSimpleName + "(" + ce.getMessage + ")", t)
+          case _ => () // do nothing
+        }
+      }
     }
   }
 
-  private def createStatus(severity: Int, message: Any, exception: Throwable): Status = {
-    new Status(severity, ScalaPlugin.plugin.getBundle.getSymbolicName, message.toString, exception)
+  // Because of a potential deadlock in the Eclipse internals (look at #1000914), the log action need to be executed in the UI thread.
+  private def logInUiThread(severity: Int, message: Any, exception: Throwable): Unit = {
+    val status = new Status(severity, ScalaPlugin.plugin.getBundle.getSymbolicName, message.toString, exception)
+    if (ScalaPlugin.plugin.headlessMode) pluginLogger.log(status)
+    else SWTUtils.asyncExec { pluginLogger.log(status) }
   }
 }
