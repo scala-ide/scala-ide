8562d0de48421dab4227cf0ccf62c31a42b79308
Mirco Dotta <mirco.dotta@typesafe.com>
2012-08-14 19:09:19 +0200

    Created extension point for SourceFileProvider so that the Hyperlinking logic can be easily used by other plugins (e.g., worksheet)
    The extension mechanism is such that other plugins will be able to register new source file provider and benefit of the existing
    declaration hyperlink detection logic.
    
    The fix was motivated by https://github.com/dragos/scala-worksheet/issues/23, and hopefully it will be useful for others.
    
    Fix #1001208

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/hyperlink/text/Hyperlink.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/hyperlink/text/Hyperlink.scala
@@ -1,10 +1,12 @@
 package scala.tools.eclipse.hyperlink.text
 
-import org.eclipse.jdt.internal.core.Openable
+import org.eclipse.core.resources.IFile
 import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility
-import org.eclipse.jface.text.hyperlink.IHyperlink
 import org.eclipse.jface.text.IRegion
+import org.eclipse.jface.text.hyperlink.IHyperlink
 import org.eclipse.ui.texteditor.ITextEditor
+import org.eclipse.jdt.internal.core.Openable
+import scala.tools.eclipse.InteractiveCompilationUnit
 
 /** A creator of resolved hyperlinks.
  *  
@@ -13,17 +15,23 @@ import org.eclipse.ui.texteditor.ITextEditor
  */
 object Hyperlink {
  
-  type Factory = (Openable, Int, Int, String, IRegion) => IHyperlink
+  type Factory = (AnyRef, Int, Int, String, IRegion) => IHyperlink
 
-  def withText(name: String)(file: Openable, pos: Int, len: Int, label: String, wordRegion: IRegion): IHyperlink = 
-    new ScalaHyperlink(file, pos, len, label, text = name, wordRegion)
+  def withText(name: String)(openableOrUnit: AnyRef, pos: Int, len: Int, label: String, wordRegion: IRegion): IHyperlink = 
+    new ScalaHyperlink(openableOrUnit, pos, len, label, text = name, wordRegion)
   
-  private class ScalaHyperlink(file: Openable, pos: Int, len: Int, label: String, text: String, wordRegion: IRegion) extends IHyperlink {
+  private class ScalaHyperlink(openableOrUnit: AnyRef, pos: Int, len: Int, label: String, text: String, wordRegion: IRegion) extends IHyperlink {
     def getHyperlinkRegion = wordRegion
     def getTypeLabel = label
     def getHyperlinkText = text
     def open = {
-      EditorUtility.openInEditor(file, true) match {
+      /* This is a bad hack, but is currently needed to correctly navigate to sources attached to a binary file. */ 
+      val part = openableOrUnit match {
+        case editorInput: Openable => EditorUtility.openInEditor(editorInput, true)
+        case unit: InteractiveCompilationUnit => EditorUtility.openInEditor(unit.workspaceFile, true)
+        case _ => null
+      }
+      part match {
         case editor: ITextEditor => editor.selectAndReveal(pos, len)
         case _ =>
       }
