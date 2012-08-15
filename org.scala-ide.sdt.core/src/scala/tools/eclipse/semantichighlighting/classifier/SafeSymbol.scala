88132af2126baa1a2f32971258a542a5f05de965
Simon Schaefer <mail@antoras.de>
2012-08-15 22:34:19 +0200

    Add semantic highlighting for object members in type params
    Fix #1001209

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/semantichighlighting/classifier/SafeSymbol.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/semantichighlighting/classifier/SafeSymbol.scala
@@ -104,6 +104,9 @@ trait SafeSymbol extends CompilerAccess with PimpedTrees {
     case ExistentialTypeTree(tpt, whereClauses) =>
       (tpt :: whereClauses).flatMap(safeSymbol)
 
+    case tpe @ Select(qualifier, _) =>
+      global.askOption(() => tpe.symbol -> tpe.namePosition).toList ::: safeSymbol(qualifier)
+
     case _ =>
       // the local variable backing a lazy value is called 'originalName$lzy'. We swap it here for its
       // accessor, otherwise this symbol would fail the test in `getNameRegion`
