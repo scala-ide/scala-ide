scala-ide/scala-ide

commit 88132af2126baa1a2f32971258a542a5f05de965

Add semantic highlighting for object members in type params
Fix #1001209

org.scala-ide.sdt.core/src/scala/tools/eclipse/semantichighlighting/classifier/SafeSymbol.scala
@@ -104,6 +104,9 @@ trait SafeSymbol extends CompilerAccess with PimpedTrees {
     case ExistentialTypeTree(tpt, whereClauses) =>
       (tpt :: whereClauses).flatMap(safeSymbol)
 
+    case tpe @ Select(qualifier, _) =>
+      global.askOption(() => tpe.symbol -> tpe.namePosition).toList ::: safeSymbol(qualifier)
+
     case _ =>
       // the local variable backing a lazy value is called 'originalName$lzy'. We swap it here for its
       // accessor, otherwise this symbol would fail the test in `getNameRegion`

org.scala-ide.sdt.core.tests/src/scala/tools/eclipse/semantichighlighting/classifier/ObjectTest.scala
@@ -58,4 +58,16 @@ class ObjectTest extends AbstractSymbolClassifierTest {
         """,
       Map("OBJ" -> Object, "PKG" -> Package))
   }
+
+  @Test
+  def object_member_within_type_param() {
+    checkSymbolClassification("""
+      object TypeA { class TypeB }
+      trait Trait extends Seq[TypeA.TypeB]
+      """, """
+      object TypeA { class TypeB }
+      trait Trait extends $T$[$OBJ$.$CLS$]
+      """,
+      Map("CLS" -> Class, "OBJ" -> Object, "T" -> Type))
+  }
 }

commit 1a29189cf9d165b5ae9b72603f4414eb486857c3

Fix 1001222
This bug is introduced with commit 88132af2126baa1a2f32971258a542a5f05de965

org.scala-ide.sdt.core/src/scala/tools/eclipse/semantichighlighting/classifier/SafeSymbol.scala
@@ -105,7 +105,9 @@ trait SafeSymbol extends CompilerAccess with PimpedTrees {
       (tpt :: whereClauses).flatMap(safeSymbol)
 
     case tpe @ Select(qualifier, _) =>
-      global.askOption(() => tpe.symbol -> tpe.namePosition).toList ::: safeSymbol(qualifier)
+      val tpeSym = if (hasSourceCodeRepresentation(tpe)) global.askOption(() => tpe.symbol -> tpe.namePosition).toList else Nil
+      val qualiSym = if(hasSourceCodeRepresentation(qualifier)) safeSymbol(qualifier) else Nil
+      tpeSym ::: qualiSym
 
     case _ =>
       // the local variable backing a lazy value is called 'originalName$lzy'. We swap it here for its