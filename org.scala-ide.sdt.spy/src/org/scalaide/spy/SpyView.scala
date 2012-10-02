a9fd2726b6ece237373522cec0299932a9d61c47
Iulian Dragos <jaguarul@gmail.com>
2012-10-02 17:10:16 +0200

    Added Scala Plugin Spy.
    This adds a view that can show the cursor offset in the current document,
    and additional Scala compiler information:
    
    * tree.pos (range positions)
    * tree.tpe
    * tree.symbol
    * tree.tpe

---
+++ org.scala-ide.sdt.spy/src/org/scalaide/spy/SpyView.scala
@@ -0,0 +1,75 @@
+package org.scalaide.spy
+
+import org.eclipse.jface.text.ITextSelection
+import org.eclipse.jface.viewers.ISelection
+import org.eclipse.swt.SWT
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.swt.widgets.Text
+import org.eclipse.ui.ISelectionListener
+import org.eclipse.ui.IWorkbenchPart
+import org.eclipse.ui.part.ViewPart
+import scala.tools.eclipse.InteractiveCompilationUnit
+import scala.tools.eclipse.ui.InteractiveCompilationUnitEditor
+import org.eclipse.swt.graphics.Font
+import org.eclipse.jface.resource.JFaceResources
+
+class SpyView extends ViewPart {
+  private var textArea: Text = _
+
+  def setFocus() {
+    textArea.setFocus()
+  }
+
+  def createPartControl(parent: Composite) {
+    textArea = new Text(parent, SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL)
+    textArea.setFont(JFaceResources.getTextFont()) // fixed width font
+
+    getSite.getWorkbenchWindow().getSelectionService().addPostSelectionListener(listener)
+  }
+
+  override def dispose() {
+    super.dispose()
+    getSite.getWorkbenchWindow().getSelectionService().removePostSelectionListener(listener)
+  }
+
+  private def updateView(offset: Int, length: Int, part: IWorkbenchPart) {
+    textArea.setText("Offset: \t%s".format(offset.toString))
+    textArea.append("\nLength: \t%s".format(length.toString))
+
+    part match {
+      case icuEditor: InteractiveCompilationUnitEditor =>
+        for (cu <- icuEditor.getInteractiveCompilationUnit) {
+          cu.doWithSourceFile { (source, compiler) =>
+            import compiler._
+
+            val response = new Response[Tree]
+            compiler.askTypeAt(rangePos(source, offset, offset, offset + length), response)
+            response.get match {
+              case Left(tree) =>
+                textArea.append("\n\n============\n\nTree: \t\t" + tree.productPrefix)
+                textArea.append("\ntree.pos: \t%s".format(tree.pos))
+                textArea.append("\ntree.tpe: \t%s".format(tree.tpe))
+                textArea.append("\n\nsymbol: \t\t%s".format(tree.symbol))
+                for (sym <- Option(tree.symbol) if sym ne NoSymbol)
+                  textArea.append("\nsymbol.info: \t%s".format(tree.symbol.info))
+
+              case _ =>
+
+            }
+
+          }
+        }
+    }
+    textArea.setSelection(0, 0)
+  }
+
+  object listener extends ISelectionListener {
+    override def selectionChanged(sourcePart: IWorkbenchPart, selection: ISelection) {
+      selection match {
+        case textSelection: ITextSelection =>
+          updateView(textSelection.getOffset(), textSelection.getLength(), sourcePart)
+        case _ =>
+      }
+    }
+  }
+}
