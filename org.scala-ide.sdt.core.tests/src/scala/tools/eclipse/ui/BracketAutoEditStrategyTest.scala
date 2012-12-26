b5afb7448f5cf1bbff6c39ee7bea1f27d5076e17
Simon Schaefer <mail@antoras.de>
2012-12-26 17:58:28 +0100

    Fix #1001309
    Because it turned out that the new behavior led to some problems it must
    explicitly activated by turning off a new option, located in:
    
      Preferences -> Scala -> Editor
    
    If the option is disabled, a brace isn't automatically closed if it is
    located before some other source code in the same line. In the following
    examples the caret is denoted by a ^ sign:
    
         `List(1) map^ (_+1)`
      => `List(1) map{^ (_+1)`
    
    The former is transformed to the latter because the opening brace
    doesn't occur at the end of the line. If a line contains only trailing
    white spaces after the caret, this behavior is not applied:
    
         `List(1) map ^  `
      => `List(1) map {^}  `
    
    It turned out that this behavior is annoying when an opening brace is
    placed between an already existing pair of braces:
    
         `List(List(1)) map { _ map ^ }`
      => `List(List(1)) map { _ map {^} }`
    
    Here, a closing brace is always inserted even though the option in the
    preferences is disabled and the opening brace is not the last non white
    space sign in the line. There is one situation left now where the
    behavior can be tricky:
    
         `List(List(1)) map { _ map^ (_+1) }`
      => `List(List(1)) map { _ map{^} (_+1) }`
    
    Actually, the caret is positioned before the argument of the function.
    But it also occurs between a pair of braces, hence there is conflicting
    behavior. Since it was not possible to exactly determine which behavior
    should be chosen without doing a full (and slow) parsing of the source
    code, the behavior inserting a closing bracket is chosen (as shown in
    the example).
    Another reason to choose this behavior is that the current indentation
    engine also can not handle this "inline bracket case" - by hitting enter
    it inserts a closing brace after the whole content, placed after the
    caret, and not after the closing parentheses.

--- org.scala-ide.sdt.core.tests/src/scala/tools/eclipse/ui/BracketAutoEditStrategyTest.scala
+++ org.scala-ide.sdt.core.tests/src/scala/tools/eclipse/ui/BracketAutoEditStrategyTest.scala
@@ -1,8 +1,27 @@
 package scala.tools.eclipse.ui
 
-import org.junit.Test
+import scala.tools.eclipse.properties.EditorPreferencePage
 
-class BracketAutoEditStrategyTest extends AutoEditStrategyTests(new BracketAutoEditStrategy) {
+import org.eclipse.jface.preference.IPreferenceStore
+import org.junit.{ Before, Test }
+import org.mockito.Mockito._
+
+object BracketAutoEditStrategyTest {
+
+  val prefStore = mock(classOf[IPreferenceStore])
+
+  def enableAutoClosing(enable: Boolean) {
+    when(prefStore.getBoolean(EditorPreferencePage.P_ENABLE_AUTO_CLOSING_BRACES)).thenReturn(enable)
+  }
+}
+
+class BracketAutoEditStrategyTest extends AutoEditStrategyTests(
+    new BracketAutoEditStrategy(BracketAutoEditStrategyTest.prefStore)) {
+
+  @Before
+  def startUp() {
+    BracketAutoEditStrategyTest.enableAutoClosing(true)
+  }
 
   @Test
   def auto_closing_opening_brace() {
@@ -32,5 +51,52 @@ class BracketAutoEditStrategyTest extends AutoEditStrategyTests(new BracketAutoE
   @Test
   def remove_opening_brace() {
     test(input = "{^", expectedOutput = "^", operation = Remove("{"))
+  }
+
+  @Test
+  def auto_closing_nested_opening_brace() {
+    BracketAutoEditStrategyTest.enableAutoClosing(false)
+    test(input = "{^}", expectedOutput = "{{^}}", operation = Add("{"))
+  }
+
+  @Test
+  def prevent_auto_closing_brace_when_editing_an_existing_line() {
+    BracketAutoEditStrategyTest.enableAutoClosing(false)
+    test(
+        input = "List(1) map^ (_+1)",
+        expectedOutput = "List(1) map{^ (_+1)",
+        operation = Add("{"))
+  }
+
+  @Test
+  def not_prevent_auto_closing_brace_when_editing_an_existing_line() {
+    test(
+        input = "List(1) map^ (_+1)",
+        expectedOutput = "List(1) map{^} (_+1)",
+        operation = Add("{"))
+  }
+
+  @Test
+  def auto_closing_brace_in_function_literal() {
+    BracketAutoEditStrategyTest.enableAutoClosing(false)
+    test(
+        input = "List(List(1)) map { _ map ^ }",
+        expectedOutput = "List(List(1)) map { _ map {^} }",
+        operation = Add("{"))
+  }
+
+  @Test
+  def auto_closing_brace_in_and_before_another_function_literal() {
+    BracketAutoEditStrategyTest.enableAutoClosing(false)
+    test(
+        input = "List(List(1)) map { _ map^ (_+1) }",
+        expectedOutput = "List(List(1)) map { _ map{^} (_+1) }",
+        operation = Add("{"))
+  }
+
+  @Test
+  def auto_closing_brace_before_trailing_whitespaces() {
+    BracketAutoEditStrategyTest.enableAutoClosing(false)
+    test(input = "^   ", expectedOutput = "{^}   ", operation = Add("{"))
   }
 }
--- org.scala-ide.sdt.core/src/scala/tools/eclipse/ui/BracketAutoEditStrategy.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/ui/BracketAutoEditStrategy.scala
@@ -1,18 +1,32 @@
 package scala.tools.eclipse.ui
 
-import org.eclipse.jface.text.DocumentCommand
-import org.eclipse.jface.text.IAutoEditStrategy
-import org.eclipse.jface.text.IDocument
+import org.eclipse.jface.text.{ DocumentCommand, IAutoEditStrategy, IDocument }
+import scala.tools.eclipse.ScalaPlugin
+import scala.tools.eclipse.properties.EditorPreferencePage
+import org.eclipse.jface.preference.IPreferenceStore
 
 /**
  * Automatically applies several auto edit actions when a user enters or removes
  * an opening or closing bracket.
  */
-class BracketAutoEditStrategy extends IAutoEditStrategy {
+class BracketAutoEditStrategy(prefStore: IPreferenceStore) extends IAutoEditStrategy {
     def customizeDocumentCommand(document: IDocument, command: DocumentCommand) {
+    def isLineEndEmpty = {
+      val lineInfo = document.getLineInformationOfOffset(command.offset)
+      val str = document.get(command.offset, lineInfo.getLength() + lineInfo.getOffset() - command.offset)
+      val (open, close) = (str.count(_ == '{'), str.count(_ == '}'))
+      val hasUnmatchedClosingBracket = (open > 0 || close > 0) && open < close
+      hasUnmatchedClosingBracket || str.trim.length == 0
+    }
+
     command.text match {
       case "{" => // add a closing brace
-        command.text = "{}"
+        val isAutoClosingEnabled = prefStore.getBoolean(
+            EditorPreferencePage.P_ENABLE_AUTO_CLOSING_BRACES)
+
+        if (isAutoClosingEnabled || isLineEndEmpty) {
+          command.text = "{}"
+        }
         command.caretOffset = command.offset + 1
         command.shiftsCaret = false
       case "}" => // jump over closing brace
