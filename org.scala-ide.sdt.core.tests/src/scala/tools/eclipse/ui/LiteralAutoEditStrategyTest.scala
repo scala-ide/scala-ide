bbd40dca8da0c8b3ce7e4843c077b15eb1ecee25
Simon Schaefer <mail@antoras.de>
2012-12-29 00:00:28 +0100

    Add character literal completion
    As for string literals, \ signs are automatically escaped to prevent
    compiler errors.
    
    Furthermore it turned out that the current partitioning strategy doesn't
    work perfectly together with this component. Two consecutive apostrophes
    aren't handled as their own partition since they are not valid Scala
    syntax. Thus, the behavior that should only applied to existing
    character literals is placed in the same component and must be moved if
    the partitioning strategy for character literals ever changes.
    
    Fix #1001403

--- org.scala-ide.sdt.core.tests/src/scala/tools/eclipse/ui/LiteralAutoEditStrategyTest.scala
+++ org.scala-ide.sdt.core.tests/src/scala/tools/eclipse/ui/LiteralAutoEditStrategyTest.scala
@@ -23,4 +23,62 @@ class LiteralAutoEditStrategyTest extends AutoEditStrategyTests(new LiteralAutoE
   def no_auto_close_on_existing_string_literal_after_caret() {
     test(input = "^\"", expectedOutput = "\"^\"", operation = Add("\""))
   }
+
+  @Test
+  def auto_close_character_literal() {
+    test(input = "^", expectedOutput = "'^'", operation = Add("'"))
+  }
+
+  @Test
+  def remove_character_pair() {
+    test(input = """ '^' """, expectedOutput = """ ^ """, operation = Remove("'"))
+  }
+
+  @Test
+  def no_auto_close_on_existing_char_literal_before_caret() {
+    test(input = "'^", expectedOutput = "''^", operation = Add("'"))
+  }
+
+  @Test
+  def no_auto_close_on_existing_char_literal_after_caret() {
+    test(input = "^'", expectedOutput = "'^'", operation = Add("'"))
+  }
+
+  // the following tests belong to existing character literals,
+  // i.e. two apostrophes '' are already created
+
+  @Test
+  def add_apostrophe_to_character_literal() {
+    test(input = """ '^' """, expectedOutput = """ ''^' """, operation = Add("'"))
+  }
+
+  @Test
+  def jump_not_over_closing_character_literal_if_it_is_filled() {
+    test(input = """ ' ^' """, expectedOutput = """ ' '^' """, operation = Add("'"))
+  }
+
+  @Test
+  def do_not_auto_escape_character_literal() {
+    test(input = """ '^' """, expectedOutput = """ ''^' """, operation = Add("'"))
+  }
+
+  @Test
+  def remove_escaped_character_literal() {
+    test(input = """ '\'^' """, expectedOutput = """ '^' """, operation = Remove("'"))
+  }
+
+  @Test
+  def auto_escape_backslash() {
+    test(input = """ '^' """, expectedOutput = """ '\\^' """, operation = Add("\\"))
+  }
+
+  @Test
+  def no_auto_escape_backslash_on_preceding_backslash() {
+    test(input = """ '\^' """, expectedOutput = """ '\\^' """, operation = Add("\\"))
+  }
+
+  @Test
+  def remove_escape_backslash() {
+    test(input = """ '\\^' """, expectedOutput = """ '^' """, operation = Remove("\\"))
+  }
 }
--- org.scala-ide.sdt.core/src/scala/tools/eclipse/ui/LiteralAutoEditStrategy.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/ui/LiteralAutoEditStrategy.scala
@@ -3,7 +3,7 @@ package scala.tools.eclipse.ui
 import org.eclipse.jface.text.{ DocumentCommand, IAutoEditStrategy, IDocument }
 
 /**
- * Applies several auto edit actions to string literals.
+ * Applies several auto edit actions to string and character literals.
  */
 class LiteralAutoEditStrategy extends IAutoEditStrategy {
 
@@ -14,7 +14,7 @@ class LiteralAutoEditStrategy extends IAutoEditStrategy {
     }
 
     def addClosingLiteral() {
-      if (ch(-1, '"') || ch(0, '"')) {
+      if (ch(-1, '"') || ch(-1, ''') || ch(0, '"') || ch(0, ''')) {
         return
       }
       command.caretOffset = command.offset + 1
@@ -22,16 +22,84 @@ class LiteralAutoEditStrategy extends IAutoEditStrategy {
       command.shiftsCaret = false
     }
 
-    def removeClosingLiteral() {
-      if (ch(0, '"') && ch(1, '"')) {
+    def removeLiteral() {
+      if ((ch(0, '"') && ch(1, '"')) || (ch(0, ''') && ch(1, '''))) {
         command.length = 2
       }
     }
 
-    command.text match {
-      case "\"" => addClosingLiteral()
-      case ""   => removeClosingLiteral()
-      case _    =>
+    def removeEscapedSign() {
+      if (ch(-1, '\\') && (ch(0, ''') || ch(0, '\\'))) {
+        command.length = 2
+        command.offset -= 1
+      }
     }
+
+    def jumpOverClosingLiteral() {
+      command.text = ""
+      command.caretOffset = command.offset + 1
+    }
+
+    def handleClosingLiteral() {
+      val isCharFilled = if (ch(-1, ''')) ch(-2, '\\') else !ch(-1, '\\')
+
+      if (ch(0, ''') && isCharFilled)
+        jumpOverClosingLiteral()
+      else if (!ch(-1, '\\') && ch(-2, '''))
+        command.text = "\\'"
+    }
+
+    def handleEscapeSign() {
+      if (!ch(-1, '\\')) {
+        command.text = "\\\\"
+      }
+    }
+
+    def customizeLiteral() {
+      if (command.length > 1) {
+        /*
+         * The current auto edit strategy is not able to handle changes that added or
+         * removed more than a single sign, thus further calculations are aborted to
+         * avoid wrong behavior.
+         * This is only a temporary solution - in the future the needed behavior
+         * should be implemented.
+         */
+        return
+      }
+
+      command.text match {
+        case "\\" => handleEscapeSign()
+        case "'"  => handleClosingLiteral()
+        case ""   => removeEscapedSign()
+        case _    =>
+      }
+    }
+
+    def customizeChar() {
+      command.text match {
+        case "\"" | "'" => addClosingLiteral()
+        case ""         => removeLiteral()
+        case _          =>
+      }
+    }
+
+    val isCharacterLiteral = (ch(-1, ''') || ch(-1, '\\')) && (ch(0, ''') || ch(0, '\\'))
+
+    /*
+     * Normally, character literals should be handled by its own component. But
+     * because two consecutive apostrophes '' are not valid Scala syntax, the
+     * partitioner doesn't move them to an own partition, hence the same component
+     * is called when a sign is added to such a character literal.
+     *
+     * If in future the partitioning strategy for character literals changes
+     * in this case, the code called my `cutomizeLiteral` should be moved in its
+     * own component (and this new component also should be configured in
+     * `ScalaSourceViewerConfiguration`) otherwise the corresponding behavior is
+     * not called
+     */
+    if (isCharacterLiteral)
+      customizeLiteral()
+    else
+      customizeChar()
   }
 }
