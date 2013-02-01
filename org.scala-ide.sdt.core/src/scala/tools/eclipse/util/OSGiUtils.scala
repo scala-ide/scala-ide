85809fcac9a504ba78fcb3e6a0393bc707076de6
Iulian Dragos <jaguarul@gmail.com>
2013-02-01 15:49:06 +0100

    Use plain Scala OSGi bundles to build the IDE.
    * toolchain is used only to build sbt
    * all Scala-related dependencies are resolved through Maven
    * bundles not needed for execution (scala-swing, continuations, various source attachments) are added to sdt.core
    * features need an extra step, because in 2.11 we need to ship two extra bundles (xml and parser combinators). This is done through the new -Pset-features profile in build-all.sh
    
    As a bonus, all Scala bundles have correct source attachments now, so plugin
    development should be much easier.
    
    Fixed #1001889.

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/util/OSGiUtils.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/util/OSGiUtils.scala
@@ -5,18 +5,26 @@
 
 package scala.tools.eclipse.util
 
+import java.io.IOException
 import java.net.URL
+
+import scala.tools.eclipse.logging.HasLogger
+
 import org.eclipse.core.runtime.FileLocator
 import org.eclipse.core.runtime.IPath
 import org.eclipse.core.runtime.Path
 import org.osgi.framework.Bundle
 
-object OSGiUtils {
+object OSGiUtils extends HasLogger {
   private def urlToPath(url: URL): IPath = Path.fromOSString(FileLocator.toFileURL(url).getPath)
 
-  def pathInBundle(bundle: Bundle, path: String) : Option[IPath] = {
+  def pathInBundle(bundle: Bundle, path: String): Option[IPath] = {
     val url = FileLocator.find(bundle, Path.fromPortableString(path), null)
     Option(url) map urlToPath
+  }
+
+  def getBundlePath(bundle: Bundle): Option[IPath] = util.control.Exception.failing(classOf[IOException]) {
+    Option(FileLocator.getBundleFile(bundle)).map(f => Path.fromOSString(f.getAbsolutePath()))
   }
 
   def allPathsInBundle(bundle: Bundle, path: String, filePattern: String): Iterator[IPath] = {
