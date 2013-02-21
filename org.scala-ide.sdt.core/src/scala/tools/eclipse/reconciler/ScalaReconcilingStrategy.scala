43647dd512dc56fea45461f68b0a84d7866fda60
François Garillot <francois@garillot.net>
2013-02-21 20:18:53 +0100

    Plugs in IJavaReconcilingListeners
    Better interfaces
    Essential tweaks in Problem annotation safety
    Narrows down invalidated text region
    Fix stale / ill-formatted code.
    
    Fixes #1001454
    
    De-activated test requiring ui

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/reconciler/ScalaReconcilingStrategy.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/reconciler/ScalaReconcilingStrategy.scala
@@ -1,21 +1,35 @@
 package scala.tools.eclipse.reconciler
 
 import org.eclipse.core.runtime.IProgressMonitor
+import org.eclipse.core.runtime.NullProgressMonitor;
 import scala.tools.eclipse.logging.HasLogger
 import org.eclipse.jface.text._
-import org.eclipse.jface.text.reconciler.{IReconcilingStrategy, IReconcilingStrategyExtension, DirtyRegion}
+import org.eclipse.jface.text.reconciler.IReconcilingStrategy
+import org.eclipse.jface.text.reconciler.IReconcilingStrategyExtension
+import org.eclipse.jface.text.reconciler.DirtyRegion
 import org.eclipse.jface.text.source._
+import org.eclipse.jdt.core.dom.CompilationUnit
+import org.eclipse.jdt.internal.ui.text.java.IJavaReconcilingListener
+import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor
 import org.eclipse.ui.texteditor._
-import scala.tools.eclipse.ScalaEditor
-import scala.tools.eclipse.ScalaSourceFileEditor
 import scala.tools.eclipse.InteractiveCompilationUnit
-import scala.tools.eclipse.javaelements.ScalaCompilationUnit
+import scala.tools.eclipse.ui.InteractiveCompilationUnitEditor
 
-class ScalaReconcilingStrategy(scEditor: ScalaEditor) extends IReconcilingStrategy with IReconcilingStrategyExtension with HasLogger {
+class ScalaReconcilingStrategy(icuEditor: InteractiveCompilationUnitEditor) extends IReconcilingStrategy with IReconcilingStrategyExtension with HasLogger {
 
   private var progressMonitor : IProgressMonitor = _
   private var document: IDocument = _
-  private lazy val scUnit = scEditor.getInteractiveCompilationUnit().asInstanceOf[ScalaCompilationUnit]
+
+  /**
+   * The underlying compilation unit, in general implemented by a ScalaSourceFile.
+   *
+   * @note This member is a def, not a lazy val, to avoid doc/reconciler
+   * desynchronizations if the underlying document is swapped.
+   *  (see https://github.com/scala-ide/scala-ide/pull/309#discussion_r3048592)
+   */
+  private def icUnit = icuEditor.getInteractiveCompilationUnit()
+
+  private val listeningEditor = icuEditor.asInstanceOf[IJavaReconcilingListener]
 
   override def setDocument(doc: IDocument) {
     document = doc
@@ -30,15 +44,20 @@ class ScalaReconcilingStrategy(scEditor: ScalaEditor) extends IReconcilingStrate
   }
 
   override def reconcile(partition: IRegion) {
-    scUnit.scalaProject.doWithPresentationCompiler(_.flushScheduledReloads())
-    val errors = scUnit.reconcile(document.get)
+    listeningEditor.aboutToBeReconciled()
+    icUnit.scalaProject.doWithPresentationCompiler(_.flushScheduledReloads())
+    val errors = icUnit.reconcile(document.get)
     // we only update the edited compilation unit
-    scEditor.updateErrorAnnotations(errors)
+    icuEditor.updateErrorAnnotations(errors)
+    // reconciled expects a jdt.core.dom.CompilationUnitEditor as first argument,
+    // which ScalaSourceFileEditor and other ICU Editors aren't
+    // it is possible we starve Java-Side IReconcilingListeners here
+    listeningEditor.reconciled(null, false, new NullProgressMonitor())
   }
 
   override def initialReconcile() {
     // an askReload there adds the scUnit to the list of managed CUs
-    scUnit.scheduleReconcile()
+    icUnit.scheduleReconcile()
   }
 
 }
--- org.scala-ide.sdt.core/src/scala/tools/eclipse/ui/DecoratedInteractiveEditor.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/ui/DecoratedInteractiveEditor.scala
@@ -1,43 +1,52 @@
 package scala.tools.eclipse.ui
 
+import org.eclipse.jface.text.source.Annotation
 import org.eclipse.jface.text.source.IAnnotationModel
 import org.eclipse.jface.text.source.IAnnotationModelExtension
 import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitDocumentProvider.ProblemAnnotation
+import scala.collection.breakOut
 import org.eclipse.jdt.core.compiler.IProblem
-import scala.collection.JavaConverters
 import scala.tools.eclipse.util.SWTUtils
 import org.eclipse.jface.text.Position
+import org.eclipse.jface.text.ITextViewerExtension2
 import scala.tools.eclipse.ISourceViewerEditor
+import scala.tools.eclipse.util.RichAnnotationModel._
 
 trait DecoratedInteractiveEditor extends ISourceViewerEditor {
 
-  type IAnnotationModelExtended = IAnnotationModel with IAnnotationModelExtension
-
   /** Return the annotation model associated with the current document. */
-  private def annotationModel: IAnnotationModelExtended = getDocumentProvider.getAnnotationModel(getEditorInput).asInstanceOf[IAnnotationModelExtended]
+  private def annotationModel = getDocumentProvider.getAnnotationModel(getEditorInput).asInstanceOf[IAnnotationModel]
 
-  @volatile
-  private var previousAnnotations = List[ProblemAnnotation]()
+  private var previousAnnotations = List[Annotation]()
 
   /**
    * Update annotations on the editor from a list of IProblems
    */
 
   def updateErrorAnnotations(errors: List[IProblem]) {
-    val newAnnotations = for (e <- errors) yield {
+    val newAnnotations: Map[Annotation, Position] = (for (e <- errors) yield {
       val annotation = new ProblemAnnotation(e, null) // no compilation unit
       val position = new Position(e.getSourceStart, e.getSourceEnd - e.getSourceStart + 1)
       (annotation, position)
-    }
+    })(breakOut)
 
-    import JavaConverters._
-    val newMap = newAnnotations.toMap.asJava
-    annotationModel.replaceAnnotations(previousAnnotations.toArray, newMap)
-    previousAnnotations = newAnnotations.unzip._1
+    annotationModel.replaceAnnotations(previousAnnotations, newAnnotations)
+    previousAnnotations = newAnnotations.keys.toList
 
     // This shouldn't be necessary in @dragos' opinion. But see #84 and
     // http://stackoverflow.com/questions/12507620/race-conditions-in-annotationmodel-error-annotations-lost-in-reconciler
-    SWTUtils.asyncExec { getViewer.invalidateTextPresentation() }
+    val presViewer = getViewer
+    if (presViewer.isInstanceOf[ITextViewerExtension2]) {
+        // TODO: This should be replaced by a better modularization of semantic highlighting PositionsChange
+        val newPositions = newAnnotations.values
+                def end (x:Position) = x.offset + x.length - 1
+                val taintedBounds : (Int, Int) = ((Int.MaxValue, 0) /: newPositions) {(acc, p1) => (Math.min(acc._1, p1.offset), Math.max(acc._2, end(p1)))}
+        val taintedLength = (taintedBounds._2 - taintedBounds._1 +1)
+
+        SWTUtils.asyncExec { presViewer.asInstanceOf[ITextViewerExtension2].invalidateTextPresentation(taintedBounds._1, taintedLength) }
+    } else {
+        SWTUtils.asyncExec { getViewer.invalidateTextPresentation() }
+    }
   }
 
 }
