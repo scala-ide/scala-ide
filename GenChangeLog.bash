020ae1818960ffebc55178308ee59574681273a8
François Garillot <francois@garillot.net>
2013-02-22 14:46:56 +0100

    Modified the script to pick up commits that were NOT introduced through a merge. (since we do many backports this way)
    Cherry-picks are attributed to the picker, though, NOT to the original author.

--- GenChangeLog.bash
+++ GenChangeLog.bash
@@ -13,8 +13,19 @@
 
 DEBUG=""
 
-function usage(){
+function usage() {
     echo "Usage : $0 <since> <until>"
+}
+
+function is_git_merge() {
+    # merges have 2 parents, so 3 outputs to rev-list
+    # non-merges have only one, so 1 output
+    nmerges=$(git rev-list -n 1 --parents $1 | awk '{print NF}')
+    if [ $nmerges -gt 2 ]; then echo 1; fi
+}
+
+function grep_for_fixes {
+  echo $(echo $@ | grep -oie "fix[[:alpha:]]*[[:space:]]*\?\#\?100[[:digit:]]\{4\}"|awk '{print $NF}')
 }
 
 if [ $# -lt 2 ]; then
@@ -31,34 +42,40 @@ then
     echo "I'm going to end at $end"
 fi
 
-merges=$(git rev-list --merges $end ^$start)
+commits=$(git rev-list --first-parent $end ^$start)
 
-for m in $merges
+for m in $commits
 do
     echo ""
     if [ $DEBUG ]; then
         echo $m
     fi
-    # we want the author of the merge branch, not the one of the merge
-    # commit
-    realAuthorName=$(git log -1 --format='%an' $m^2)
-    mergeDate=$(git log -1 --format='%aD' $m)
-    echo -e "$mergeDate - $realAuthorName\n"
-    echo "$(git log -1 --format='  * %b' $m)"
-
-    # $(git rev-list --parents -n 1 $m) is the set of direct parents of
-    # the merge commits (plus the merge commit itself)
-    PARENTSET=$(git rev-list --parents -n 1 $m)
+    if [ $(is_git_merge "$m" ) ]; then
+        # we want the author of the merge branch, not the one of the merge
+        # commit
+        realAuthorName=$(git log -1 --format='%an' $m^2)
+        mergeDate=$(git log -1 --format='%aD' $m)
+        echo -e "$mergeDate - $realAuthorName\n"
+        echo "$(git log -1 --format='  * %b' $m)"
     
-    # $(git merge-base --octopus $PARENTSET) is the root of the merge
-    # --octopus here just in case
-    # FIXME: how does this behave if the PR is NOT rebased before merge ?
-    BRANCH_ORIGIN=$(git merge-base --octopus $PARENTSET)
-
-    # $m^2 is the head of the merged branch, right before merge
-    FINAL_COMMIT="$m^2"
-    # $(git log <opts> $BRANCH_ORIGIN..$FINAL_COMMIT) is the log for the branch
-    fixes=$(git log --format=%B $BRANCH_ORIGIN..$FINAL_COMMIT|grep -oie "fix[[:alpha:]]*[[:space:]]*\?\#\?100[[:digit:]]\{4\}"|awk '{print $NF}')
+        # $(git rev-list --parents -n 1 $m) is the set of direct parents of
+        # the merge commits (plus the merge commit itself)
+        PARENTSET=$(git rev-list --parents -n 1 $m)
+        
+        # $(git merge-base --octopus $PARENTSET) is the root of the merge
+        # --octopus here just in case
+        # FIXME: how does this behave if the PR is NOT rebased before merge ?
+        BRANCH_ORIGIN=$(git merge-base --octopus $PARENTSET)
+    
+        # $m^2 is the head of the merged branch, right before merge
+        FINAL_COMMIT="$m^2"
+        # $(git log <opts> $BRANCH_ORIGIN..$FINAL_COMMIT) is the log for the branch
+        logtext=$(git log --format='%B' $BRANCH_ORIGIN..$FINAL_COMMIT)
+    else
+        echo "$(git log -1 --format='%aD - %an%n  * %s' $m)"
+        logtext=$(git log -1 --format=%B $m)
+    fi
+    fixes=$(grep_for_fixes $logtext)
 
     # convert this to a list (there is shorter, but less portable)
     fixesList=$(echo $fixes| sed ':a;{N;s/\n/\, /};ba')
