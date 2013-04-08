5e65629299eb85c518d9316f4a8fc9ddecfea910
François Garillot <francois@garillot.net>
2013-04-08 16:56:08 +0200

    Removed remaining @serializable (deprecated in 2.8->2.9)

--- org.scala-ide.sdt.core.tests/test-workspace/hyperlinks-sub/src/util/Box.scala
+++ org.scala-ide.sdt.core.tests/test-workspace/hyperlinks-sub/src/util/Box.scala
@@ -4,5 +4,4 @@ sealed abstract class Box[+A] {
   type myInt = Int
 }
 
-@serializable
-final case class Full[A](value: A) extends Box[A]
+final case class Full[A](value: A) extends Box[A] with Serializable
