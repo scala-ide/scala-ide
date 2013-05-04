b018628d30c662e110a4bba99e5fbc71b700f56a
Simon Schaefer <mail@antoras.de>
2013-05-05 00:43:58 +0200

    Fixed trailing whitespaces not automatically detected

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/quickfix/ExtractLocalProposal.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/quickfix/ExtractLocalProposal.scala
@@ -7,6 +7,6 @@ import org.eclipse.swt.graphics.Image
 import org.eclipse.swt.graphics.Point
 import scala.tools.eclipse.refactoring.ExtractLocalAction
 
-object ExtractLocalProposal 
+object ExtractLocalProposal
   extends ProposalRefactoringActionAdapter(
       new ExtractLocalAction, "Extract to local value")
