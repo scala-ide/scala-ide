cb0b9e9b41305d9207d0701e1c0b80193ea767bb
Donny Nadolny <donny.nadolny@gmail.com>
2013-05-05 18:59:02 -0400

    This needs to be in the object because JUnit creates a new instance of the class for each test case

--- org.scala-ide.sdt.core.tests/src/scala/tools/eclipse/quickfix/createmethod/MissingMemberInfoTests.scala
+++ org.scala-ide.sdt.core.tests/src/scala/tools/eclipse/quickfix/createmethod/MissingMemberInfoTests.scala
@@ -9,15 +9,15 @@ import org.junit.Test
 
 import MissingMemberInfoTests.compilationUnit
 
-object MissingMemberInfoTests extends TestProjectSetup("quickfix")
+object MissingMemberInfoTests extends TestProjectSetup("quickfix") {
+  private lazy val unit = compilationUnit("createmethod/CreateMethod.scala").asInstanceOf[ScalaCompilationUnit]
+  private lazy val source = new String(unit.getContents)
+  private lazy val ast = ScalariformParser.safeParse(source).get._1
+}
 
 class MissingMemberInfoTests {
   import MissingMemberInfoTests._
   
-  private lazy val unit = compilationUnit("createmethod/CreateMethod.scala").asInstanceOf[ScalaCompilationUnit]
-  private lazy val source = new String(unit.getContents)
-  private lazy val ast = ScalariformParser.safeParse(source).get._1
-
   @Test def function2BecomesFunction1() {
     val (parameterList, returnType) = infer("other.higher4")
     assertEquals(List(List(("arg", "String => Double"))), parameterList)
