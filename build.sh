48262737358c274736126ffe37602d835254a8d6
Mirco Dotta <mirco.dotta@typesafe.com>
2013-06-20 13:45:20 +0200

    Renamed & simplified build script
    * Building the Scala IDE does no longer require to first build the toolchain
    (see previous commit). Updated the build script to reflect this.
    
    * Renamed the script as it is no longer building the toolchain (hence, the all
    suffix does no longer make sense)

--- build.sh
+++ build.sh
@@ -17,29 +17,6 @@ fi
 
 echo "Running with: mvn ${ARGS}"
 
-# the parent project
-echo "Building parent project in $ROOT_DIR"
-cd ${ROOT_DIR}
-mvn ${ARGS}
-
-echo "Building the toolchain"
-# the toolchain
-cd ${ROOT_DIR}/org.scala-ide.build-toolchain
-mvn ${ARGS}
-
-
-# set the versions if required
-cd ${ROOT_DIR}
-if [ -n "${SET_VERSIONS}" ]
-then
-  echo "setting versions"
-  mvn -Pset-versions exec:java
-else
-  echo "Not running UpdateScalaIDEManifests."
-fi
-
-# the plugins
-echo "Building plugins"
 cd ${ROOT_DIR}/org.scala-ide.sdt.build
 mvn ${ARGS}
 
