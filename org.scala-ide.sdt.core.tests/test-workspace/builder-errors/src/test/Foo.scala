7d85b91408e5e74c79371b67717e380ef6bb6b92
Adriaan Moors <adriaan.moors@typesafe.com>
2013-07-09 00:06:41 -0700

    Remove unnecessary xml dependency from test
    (cherry picked from commit 14240b65bac29a3dd4663b2c9c653e6dcb6a062b)
    
    Conflicts:
    	org.scala-ide.sdt.core.tests/test-workspace/builder-errors/src/test/Foo.scala

--- org.scala-ide.sdt.core.tests/test-workspace/builder-errors/src/test/Foo.scala
+++ org.scala-ide.sdt.core.tests/test-workspace/builder-errors/src/test/Foo.scala
@@ -1,15 +1,13 @@
 package test
 
-import scala.xml.NodeSeq
-
-class Foo { 
-  def competitors() { 
-    List("foo", "bar") match { 
-      case Nil => Nil 
+class Foo {
+  def competitors() {
+    List("foo", "bar") match {
+      case Nil => Nil
       case competitors =>
-        def bindCompetitors(): scala.xml.NodeSeq = 
-          competitors.flatMap { competitor => 
-            val l: Int = competitor.length 
+        def bindCompetitors(): List[Nothing] =
+          competitors.flatMap { competitor =>
+            val l: Int = competitor.length
             l.foo.toString
           } 
         Nil
