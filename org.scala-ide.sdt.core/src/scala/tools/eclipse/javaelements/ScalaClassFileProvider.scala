e02f826519c3b8e13fd949489a1eb2ae02a35e60
Iulian Dragos <jaguarul@gmail.com>
2013-11-26 14:22:58 +0000

    Cache info about jar files that might contain Scala classifies.
    Deciding wether a class file is produced by Scala is expensive (it entails
    parsing the binary). Instead, we cache the information per package fragment,
    assuming that a jar contains Scala-only or Java-only classifies. For
    mixed Java/Scala jars, behaviour depends on the first classfile that is
    accessed. It might lead to using the Java icon/classfile editor for
    Scala classfiles.
    
    A more fine-grained strategy could be used (caching per package, instead
    of jar file), but this patch has been running at a customer and solved
    the issue, so we go for the tried-solution.
    
    Fixes #1001999
    (cherry picked from commit 492a546df849aa75d316bb8c95ee9c8b981f998d)
    
    Conflicts:
    	org.scala-ide.sdt.core/src/scala/tools/eclipse/javaelements/ScalaClassFileProvider.scala

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/javaelements/ScalaClassFileProvider.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/javaelements/ScalaClassFileProvider.scala
@@ -7,31 +7,63 @@ package scala.tools.eclipse
 package javaelements
 
 import java.io.ByteArrayInputStream
-import org.eclipse.jdt.internal.core.{ ClassFile, PackageFragment }
-import scala.tools.eclipse.contribution.weaving.jdt.cfprovider.IClassFileProvider
+
+import scala.collection.mutable.WeakHashMap
 import scala.tools.eclipse.ScalaClassFileDescriber
-import org.eclipse.jdt.core.IClassFile
+import scala.tools.eclipse.ScalaPlugin
+import scala.tools.eclipse.contribution.weaving.jdt.cfprovider.IClassFileProvider
 import scala.tools.eclipse.logging.HasLogger
 
+import org.eclipse.jdt.core.IClassFile
+import org.eclipse.jdt.core.IJavaElement
+import org.eclipse.jdt.core.IPackageFragmentRoot
+import org.eclipse.jdt.internal.core.ClassFile
+import org.eclipse.jdt.internal.core.PackageFragment
+
 class ScalaClassFileProvider extends IClassFileProvider with HasLogger {
-  override def create(contents : Array[Byte], parent : PackageFragment, name : String) : ClassFile =
-    ScalaClassFileDescriber.isScala(new ByteArrayInputStream(contents)) match {
-      case Some(sourceFile) =>
-        val scf = new ScalaClassFile(parent, name, sourceFile)
-        val sourceMapper = parent.getSourceMapper
-        if (sourceMapper == null)
-          null
-        else {
-          val source = sourceMapper.findSource(scf.getType, sourceFile)
-          if (source != null) scf else null
+
+  /** @return a ScalaClassFile implementation if bytes represent a Scala classfile, or `null`
+   *          if the default JDT implementation should be used.
+   */
+  override def create(contents: Array[Byte], parent: PackageFragment, name: String): ClassFile = {
+    def updateCache(isScalaClassfile: Boolean) {
+      val pfr = parent.getPackageFragmentRoot()
+      if (pfr ne null)
+        scalaPackageFragments.synchronized {
+          if (!scalaPackageFragments.isDefinedAt(pfr)) {
+            logger.debug(s"Setting ${pfr.getElementName} (because of class $name) to be ${if (isScalaClassfile) "Scala" else "Java"}")
+            scalaPackageFragments += pfr -> isScalaClassfile
+          }
         }
-      case _ => null
     }
-  
-  override def isInteresting(classFile: IClassFile): Boolean = {
-    val res = ScalaPlugin.plugin.isScalaProject(classFile.getJavaProject())
-    if (!res) 
-      logger.debug("Not interested in %s in project %s".format(classFile.getElementName(), classFile.getJavaProject().getElementName()))
-    res
+
+    val scalaCF = ScalaClassFileDescriber.isScala(new ByteArrayInputStream(contents)) match {
+      case Some(sourcePath) => new ScalaClassFile(parent, name, sourcePath)
+      case _                => null
+    }
+    updateCache(scalaCF ne null)
+    scalaCF
   }
+
+  /** Return `true` if the classfile could be a Scala classfile.
+   *
+   *  @note This method caches the result of the first classfile read from a package fragment (usually a jar).
+   *        This heuristic might fail if a single jar mixes Java and Scala classfiles, and if the first classfile
+   *        is comes from Java, a plain Java classfile editor and icon would be used for all classfiles in that jar.
+   */
+  override def isInteresting(classFile: IClassFile): Boolean = {
+    if (ScalaPlugin.plugin.isScalaProject(classFile.getJavaProject)) {
+      val pfr = ancestorFragmentRoot(classFile)
+      pfr.map(scalaPackageFragments.getOrElse(_, true)).getOrElse(false)
+    } else
+      false
+  }
+
+  private def ancestorFragmentRoot(classFile: IClassFile): Option[IPackageFragmentRoot] =
+    classFile.getAncestor(IJavaElement.PACKAGE_FRAGMENT_ROOT) match {
+      case pfr: IPackageFragmentRoot => Some(pfr)
+      case _                         => None
+    }
+
+  private val scalaPackageFragments: WeakHashMap[IPackageFragmentRoot, Boolean] = WeakHashMap.empty
 }
