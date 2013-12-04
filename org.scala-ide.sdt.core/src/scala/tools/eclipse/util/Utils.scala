5180272c63efe00991c1deceaccd1f529567d281
Iulian Dragos <jaguarul@gmail.com>
2013-12-04 16:51:46 +0100

    Don't create a reconciler for non-editor source configurations.
    In the Syntax Coloring preview pane we create a source viewer without an
    editor. The reconciler needs an editor to get to the compilation unit, so
    we don't attach a reconciler in this case (there won't be any editing).
    
    We might need to revisit this if we ever want to use a source viewer outside
    an editor (with reconciliation capabilities).

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/util/Utils.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/util/Utils.scala
@@ -1,7 +1,6 @@
 package scala.tools.eclipse.util
 
 import scala.tools.eclipse.logging.HasLogger
-import scala.util.control.Exception
 
 object Utils extends HasLogger {
 
@@ -46,10 +45,19 @@ object Utils extends HasLogger {
   implicit class WithAsInstanceOfOpt(obj: AnyRef) {
     import scala.reflect.runtime.universe._
 
-    def asInstanceOfOpt[B : TypeTag]: Option[B] = if (obj eq null) None else Exception.catching(classOf[Exception]) opt {
-      val m = runtimeMirror(getClass.getClassLoader)
-      val typeOfObj = m.reflect(obj).symbol.toType
-      if (typeOfObj <:< typeOf[B]) Some(obj.asInstanceOf[B]) else None
-    } flatten
+    /** Type-safe cast
+     *
+     * @return None if the cast fails or the object is null, Some[B] otherwise
+     */
+    def asInstanceOfOpt[B: TypeTag]: Option[B] =
+      if (obj eq null)
+        None
+      else try {
+        val m = runtimeMirror(getClass.getClassLoader)
+        val typeOfObj = m.reflect(obj).symbol.toType
+        if (typeOfObj <:< typeOf[B]) Some(obj.asInstanceOf[B]) else None
+      } catch {
+        case _: Exception => None
+      }
   }
 }
