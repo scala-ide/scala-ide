ab0c6a4c77c531b977a3788bba6ef2e8bf394332
Iulian Dragos <jaguarul@gmail.com>
2014-02-13 18:46:16 +0100

    Helper script to bump the version number.
    This script automates up to a point, but some updates need to be done
    manually:
    
    - <parent> tags that refer to the root pom.xml (without a relative path)
    - features.xml files that get auto-generated based on templates in resources/

---
+++ set-versions.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+function usage() {
+    cat <<EOF
+
+usage:
+    set-versions.sh <version>
+
+Delegate to the maven-tycho-plugin to set version of all subprojects. It maintains the POM and
+MANIFEST version numbers in sync. This script runs maven on each parent project.
+
+Note:
+    Use only numeric versions (do not specify -SNAPSHOT at the end, it is added by this script).
+
+Example:
+    set-versions.sh 3.0.3
+
+    This sets the pom version to 3.0.3-SNAPSHOT and the Bundle-Version to 3.0.3.qualifier
+
+EOF
+    exit 1
+}
+
+# $1 - version number to validate
+function validate() {
+
+    if [[ ! $1 =~ [0-9]+\.[0-9]+\.[0-9]+$ ]];
+    then
+        echo "Invalid version: $1. Versions should have 3 numeric components (i.e. 1.0.1) with no qualifier or suffix."
+        exit 1
+    fi
+}
+
+if [ $# -ne 1 ]; then
+    usage
+fi
+
+function setVersion() {
+    mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1-SNAPSHOT
+}
+
+validate $1
+
+echo "Setting version to $1"
+
+setVersion $1
+
+(cd org.scala-ide.build-toolchain && setVersion $1)
+(cd org.scala-ide.sdt.build && setVersion $1)
+
+cat <<EOF
+!!!!
+!!!! Don't forget to manually update versions in:
+!!!!     <parent> tag in org.scala-ide.sdt.build and build-toolchain
+!!!!     org.scala-ide.sdt.feature/resources/*.xml
+!!!!     org.scala-ide.sdt.source.feature/resources/*.xml
+!!!!
+
+EOF
