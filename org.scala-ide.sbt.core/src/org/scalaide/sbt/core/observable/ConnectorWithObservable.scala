8c787b937cc456af8fa42698c61fd50c3d061131
Luc Bourlier <luc.bourlier@typesafe.com>
2014-03-13 15:42:23 +0100

    Adds missing connection between the subscriptions

--- org.scala-ide.sbt.core/src/org/scalaide/sbt/core/observable/ConnectorWithObservable.scala
+++ org.scala-ide.sbt.core/src/org/scalaide/sbt/core/observable/ConnectorWithObservable.scala
@@ -4,18 +4,24 @@ import sbt.client.SbtConnector
 import rx.lang.scala.Observable
 import sbt.client.SbtClient
 import scala.concurrent.ExecutionContext
+import rx.lang.scala.Subscription
 
 /** Connector wrapper providing Observable based API
  */
 class ConnectorWithObservable(connector: SbtConnector) {
-  
+
   def sbtClientWatcher()(implicit context: ExecutionContext): Observable[SbtClient] = {
-    val observable = Observable[SbtClient]{ subscriber =>
-	    val subscription = connector.onConnect {
-	        sbtClient =>
-	          subscriber.onNext(sbtClient)
-	      }
+    val observable = Observable[SbtClient] { subscriber =>
+      val subscription = connector.onConnect {
+        sbtClient =>
+          subscriber.onNext(sbtClient)
       }
+      subscriber.add {
+        Subscription {
+          subscription.cancel()
+        }
+      }
+    }
     observable
   }
 
