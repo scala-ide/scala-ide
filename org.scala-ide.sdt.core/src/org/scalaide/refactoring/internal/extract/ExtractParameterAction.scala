cc1b59dfded63b1f82a94d553ea2f3fe5482618a
Luegg <wegmaluk@gmail.com>
2014-04-21 15:11:48 +0200

    integrate extract parameter refactoring
    Extract Parameter adds a new parameter to a method based on an
    arbitrary expression that evaluates to a value. The original expression
    becomes the default value of the new parameter. Hence, the according
    method calls are not modified by this refactoring.
    
    - Allows to choose the method to add the parameter when invoked in
    nested methods.
    - Inline renaming of the extracted parameter
    
    Knowns limitations:
    - Does currently not support parameter extraction in methods with no
    parameter list. Adding parameter lists would break compatibility with
    the existing method signature and require to update all existing method
    calls.

---
+++ org.scala-ide.sdt.core/src/org/scalaide/refactoring/internal/extract/ExtractParameterAction.scala
@@ -0,0 +1,21 @@
+package org.scalaide.refactoring.internal.extract
+
+import scala.tools.refactoring.common.InteractiveScalaCompiler
+import scala.tools.refactoring.implementations.extraction.ExtractParameter
+
+import org.scalaide.core.internal.jdt.model.ScalaSourceFile
+
+class ExtractParameterAction extends ExtractAction {
+  def createRefactoring(selectionStart: Int, selectionEnd: Int, file: ScalaSourceFile) =
+    new ScalaIdeExtractionRefactoring(selectionStart, selectionEnd, file) {
+      val refactoring = file.withSourceFile { (sourceFile, compiler) =>
+        new ExtractParameter with InteractiveScalaCompiler {
+          val global = compiler
+        }
+      }.get
+
+      def refactoringParameters =
+        selectedExtraction.get
+          .withAbstractionName(proposedPlaceholderName)
+    }
+}
