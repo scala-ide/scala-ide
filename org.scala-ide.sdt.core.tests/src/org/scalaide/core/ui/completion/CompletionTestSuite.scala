f1ae9ed09e3a888b5290a396ac88d9142ede3e74
Simon Schaefer <mail@antoras.de>
2014-04-21 22:51:12 +0200

    Add tests for completion overwrite feature

---
+++ org.scala-ide.sdt.core.tests/src/org/scalaide/core/ui/completion/CompletionTestSuite.scala
@@ -0,0 +1,10 @@
+package org.scalaide.core.ui.completion
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(classOf[Suite])
+@Suite.SuiteClasses(Array(
+    classOf[CompletionOverwriteTests]
+))
+class CompletionTestSuite
---
+++ org.scala-ide.sdt.core.tests/src/org/scalaide/core/ui/completion/CompletionTests.scala
@@ -0,0 +1,52 @@
+package org.scalaide.core.ui.completion
+
+import org.scalaide.core.completion.ScalaCompletions
+import org.scalaide.core.ui.CompilerSupport
+import org.scalaide.core.ui.TextEditTests
+import org.scalaide.util.internal.ScalaWordFinder
+
+/**
+ * This provides a test suite for the code completion functionality.
+ * It can not only find out which completion exists, it also checks if the source
+ * file after the insertion of a completion is inserted is correct.
+ *
+ * It can also handle Eclipse linked mode model. To depict such a model in the test
+ * simply surround the identifiers that should be considered by the linked model
+ * with [[ and ]]. The cursor is always represented by a ^.
+ */
+abstract class CompletionTests extends TextEditTests with CompilerSupport {
+
+  case class Completion(
+      completionToApply: String,
+      enableOverwrite: Boolean = false)
+        extends Operation {
+
+    def execute() = {
+      val src = createLoadedSourceFile(doc.get())
+      val r = ScalaWordFinder.findWord(doc, caretOffset)
+
+      val unit = compilationUnitOfSourceFile(src)
+      val completions = new ScalaCompletions().findCompletions(r)(caretOffset, unit)(src, compiler)
+      val completion = completions.find(_.display == completionToApply)
+
+      completion.fold(throw new IllegalArgumentException(s"the completion '$completionToApply' does not exist")) {
+        completion => completion.applyCompletionToDocument(doc, unit, caretOffset, enableOverwrite) foreach {
+          case (cursorPos, applyLinkedMode) =>
+            if (!applyLinkedMode)
+              caretOffset = cursorPos
+            else {
+              val groups = completion.linkedModeGroups.sortBy(-_._1)
+              val cursorOffset = groups.takeWhile(_._1 < cursorPos).size*4
+
+              groups foreach {
+                case (offset, length) =>
+                  doc.replace(offset+length, 0, "]]")
+                  doc.replace(offset, 0, "[[")
+              }
+              caretOffset = cursorPos + cursorOffset
+            }
+        }
+      }
+    }
+  }
+}
