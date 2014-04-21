6471a929ff3354d6f92021627b1658577b03acb6
Luegg <wegmaluk@gmail.com>
2014-04-21 14:37:35 +0200

    integrate new extract value refactoring
    A more powerful version of the existing Extract Value refactoring.
    
    Extract Value creates a new val definition that includes the extracted
    code and replaces the original expressions by a reference to the new
    value.
    
    This implementation of Extract Value offers the following features:
    - Extraction of values into arbitrary target scopes in the same CU
    - Inline renaming of the extracted value
    - Supports the extraction of multiple value definitions
    - Extraction of anonymous functions

---
+++ org.scala-ide.sdt.core/src/org/scalaide/refactoring/internal/extract/ExtractValueAction.scala
@@ -0,0 +1,21 @@
+package org.scalaide.refactoring.internal.extract
+
+import scala.tools.refactoring.common.InteractiveScalaCompiler
+import scala.tools.refactoring.implementations.extraction.ExtractValue
+
+import org.scalaide.core.internal.jdt.model.ScalaSourceFile
+
+class ExtractValueAction extends ExtractAction {
+  def createRefactoring(selectionStart: Int, selectionEnd: Int, file: ScalaSourceFile) =
+    new ScalaIdeExtractionRefactoring(selectionStart, selectionEnd, file) {
+      val refactoring = file.withSourceFile { (sourceFile, compiler) =>
+        new ExtractValue with InteractiveScalaCompiler {
+          val global = compiler
+        }
+      }.get
+
+      def refactoringParameters =
+        selectedExtraction.get
+          .withAbstractionName(proposedPlaceholderName)
+    }
+}
