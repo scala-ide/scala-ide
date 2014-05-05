cb6c2e1b6d49d5930e4f692a6ae0fe037df20e60
Luegg <wegmaluk@gmail.com>
2014-05-05 18:13:24 +0200

    add a uniform invocation method for all extraction refactorings
    Adds the “Extract… Alt + Cmd + E” to the refactoring menu which
    gives access to the following refactorings:
    
    - Extract Value
    - Extract Method
    - Extract Parameter
    - Extract Extractor
    
    After invocation “Extract…” collects all applicable extractions based
    on the currently selected code. In order to reduce the number of
    proposed extractions, “Extract…” analyses the selected code for side
    effects. If it assumes the code triggers side effects, it prefers
    method extractions over value extractions if the according target scope
    allows both.
    
    Furthermore, this patch integrates the new extraction refactorings in the quick assist menu. The additional quick assist processor uses the identical implementation as the “Extract…” command.
    
    Known limitations:
    - Detection of side effects currently uses a relatively simple
    heuristic that may lead to impracticable proposals
    
    Conflicts:
    	org.scala-ide.sdt.core/plugin.xml

--- org.scala-ide.sdt.core/src/org/scalaide/refactoring/internal/extract/ExtractionProposal.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/refactoring/internal/extract/ExtractionProposal.scala
@@ -1,11 +1,20 @@
 package org.scalaide.refactoring.internal.extract
 
+import scala.collection.mutable.ArrayBuffer
+import scala.reflect.internal.util.SourceFile
+import scala.tools.refactoring.common.InteractiveScalaCompiler
+import scala.tools.refactoring.common.TextChange
+import scala.tools.refactoring.implementations.extraction.ExtractCode
+
 import org.eclipse.core.resources.IMarker
 import org.eclipse.jface.text.DocumentEvent
 import org.eclipse.jface.text.IDocument
 import org.eclipse.jface.text.ITextViewer
 import org.eclipse.jface.text.contentassist.ICompletionProposalExtension2
 import org.eclipse.ui.IFileEditorInput
+import org.scalaide.core.compiler.ScalaPresentationCompiler
+import org.scalaide.core.completion.RelevanceValues
+import org.scalaide.core.internal.jdt.model.ScalaCompilationUnit
 import org.scalaide.core.internal.quickfix.BasicCompletionProposal
 import org.scalaide.util.internal.eclipse.EditorUtils
 
@@ -36,4 +45,45 @@ abstract class ExtractionProposal(displayString: String, hightlightFrom: Int, hi
 
   def apply(viewer: ITextViewer, trigger: Char, stateMask: Int, offset: Int) = apply(null)
   def validate(document: IDocument, offset: Int, event: DocumentEvent) = true
+}
+
+object ExtractionProposal {
+  def getQuickAssistProposals(cu: ScalaCompilationUnit, selectionStart: Int, selectionEnd: Int): Array[ExtractionProposal] = {
+    val proposals = new ArrayBuffer[ExtractionProposal]
+
+    cu.withSourceFile { (file, compiler) =>
+      val refactoring = createRefactoring(compiler, file, selectionStart, selectionEnd)
+      var relevance = RelevanceValues.ProposalRefactoringHandlerAdapter - 1
+
+      refactoring.extractions.foreach { extraction =>
+        val pos = extraction.extractionTarget.enclosing.pos
+
+        proposals += new ExtractionProposal(extraction.displayName, pos.startOrPoint, pos.endOrPoint, relevance) {
+          def apply(doc: IDocument) = {
+            refactoring.perform(extraction) match {
+              case Right((change: TextChange) :: Nil) =>
+                EditorUtils.doWithCurrentEditor { editor =>
+                  EditorUtils.applyRefactoringChangeToEditor(change, editor)
+                  LocalNameOccurrences(extraction.abstractionName).performInlineRenaming()
+                }
+            }
+          }
+        }
+
+        relevance -= 1
+      }
+    }
+
+    proposals.toArray
+  }
+
+  private def createRefactoring(compiler: ScalaPresentationCompiler, file: SourceFile, selectionStart: Int, selectionEnd: Int) =
+    new ExtractCode with InteractiveScalaCompiler {
+      val global = compiler
+
+      val selection =
+        askLoadedAndTypedTreeForFile(file).fold(new FileSelection(file.file, _, selectionStart, selectionEnd), throw _)
+
+      val extractions = prepare(selection).fold(_ => Nil, _.extractions)
+    }
 }
