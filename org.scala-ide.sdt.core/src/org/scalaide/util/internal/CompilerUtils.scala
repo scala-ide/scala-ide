c9b3c5f1b46bf45e4dc7a017c7697883d41f2840
Iulian Dragos <jaguarul@gmail.com>
2014-05-12 15:28:14 +0200

    Introduce ScalaInstallation to represent the Scala jars used to build a project.

---
+++ org.scala-ide.sdt.core/src/org/scalaide/util/internal/CompilerUtils.scala
@@ -0,0 +1,23 @@
+package org.scalaide.util.internal
+
+import scala.tools.nsc.settings.ScalaVersion
+import scala.tools.nsc.settings.SpecificScalaVersion
+
+object CompilerUtils {
+  object ShortScalaVersion {
+    def unapply(v: ScalaVersion): Option[(Int, Int)] = v match {
+      case SpecificScalaVersion(major, minor, _, _) => Some((major, minor))
+      case _                                        => None
+    }
+  }
+
+  def isBinarySame: (ScalaVersion, ScalaVersion) => Boolean = {
+    case (ShortScalaVersion(major, minor), ShortScalaVersion(thatMajor, thatMinor)) => major == thatMajor && minor == thatMinor
+    case _ => false
+  }
+
+  def isBinaryPrevious: (ScalaVersion, ScalaVersion) => Boolean = {
+    case (ShortScalaVersion(major, minor), ShortScalaVersion(thatMajor, thatMinor)) => major == thatMajor && minor == thatMinor + 1
+    case _ => false
+  }
+}
