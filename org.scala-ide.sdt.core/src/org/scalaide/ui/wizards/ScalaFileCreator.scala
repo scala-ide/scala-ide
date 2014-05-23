e87ad474998f7a27da005608df2678af645365c3
Simon Schaefer <mail@antoras.de>
2014-05-23 18:40:56 +0200

    Add validation logic to `ScalaFileCreator`
    There is one flaw left in this validation logic: It only checks if an
    identifier is a valid Java identifier and if it is no Scala keyword. For
    package paths it should also check if they are no Java identifiers and
    for file names it should check if the name is a valid Scala identifier
    (which can contain special signs).

--- org.scala-ide.sdt.core/src/org/scalaide/ui/wizards/ScalaFileCreator.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/ui/wizards/ScalaFileCreator.scala
@@ -2,11 +2,18 @@ package org.scalaide.ui.wizards
 
 import org.eclipse.core.resources.IProject
 import org.eclipse.core.resources.IResource
+import org.eclipse.core.resources.ResourcesPlugin
 import org.eclipse.core.runtime.IPath
+import org.scalaide.core.ScalaPlugin
+import org.scalaide.util.internal.Commons
 import org.scalaide.util.internal.eclipse.ProjectUtils
+
+import scalariform.lexer._
 
 trait ScalaFileCreator extends FileCreator {
   import ProjectUtils._
+
+  private[wizards] type FileExistenceCheck = IProject => Validation
 
   override def templateVariables(project: IProject, name: String): Map[String, String] = {
     ???
@@ -21,7 +28,15 @@ trait ScalaFileCreator extends FileCreator {
   }
 
   override def validateName(project: IProject, name: String): Validation = {
-    ???
+    if (!ScalaPlugin.plugin.isScalaProject(project))
+      Invalid("Not a Scala project")
+    else {
+      val srcDirs = sourceDirs(project)
+      doValidation(srcDirs.map(_.lastSegment()), name) match {
+        case Left(v) => v
+        case Right(f) => f(project)
+      }
+    }
   }
 
   override def createFileFromName(project: IProject, name: String): IPath = {
@@ -51,6 +66,80 @@ trait ScalaFileCreator extends FileCreator {
       }
 
       generatePath(if (srcDirs contains topFolder) "." else "/")
+    }
+  }
+
+
+  private[wizards] def doValidation(srcDirs: Seq[String], name: String): Either[Invalid, FileExistenceCheck] = {
+    def isValidScalaIdent(str: String) = {
+      val validIdent =
+        str.nonEmpty &&
+        Character.isJavaIdentifierStart(str.head) &&
+        str.tail.forall(Character.isJavaIdentifierPart)
+
+      def noKeyword =
+        !Tokens.KEYWORDS(ScalaLexer.tokenise(str, forgiveErrors = true).head.tokenType)
+
+      validIdent && noKeyword
+    }
+
+    val rawPath = Commons.split(name, '/')
+    val noFolder = rawPath.size == 1
+    val noPackageNotation = rawPath.size > 2
+
+    if (name.isEmpty())
+      Left(Invalid("No file path specified"))
+    else if (noFolder)
+      Right(checkFileExists(_, name))
+    else if (!srcDirs.contains(rawPath.head))
+      if (name.endsWith("/"))
+        Left(Invalid("No file name specified"))
+      else
+        Right(checkFileExists(_, name))
+    else if (noPackageNotation)
+      Left(Invalid(s"Incorrect syntax for file path. Has to be <folder>/<package>.<filename>"))
+    else {
+      val fullyQualifiedType = rawPath(1)
+      val packages = Commons.split(fullyQualifiedType, '.')
+
+      if (packages.last.isEmpty)
+        Left(Invalid("No type name specified"))
+      else
+        packages.find(!isValidScalaIdent(_)) match {
+          case Some(e) => Left(Invalid(s"'$e' is not a valid package name"))
+          case _       => Right(checkTypeExists(_, fullyQualifiedType, name.replace('.', '/')))
+        }
+    }
+  }
+
+  private[wizards] def checkFileExists(project: IProject, path: String): Validation = {
+    val root = ResourcesPlugin.getWorkspace().getRoot()
+    val fullPath = root.getRawLocation().append(project.getFullPath()).append(path)
+    val fileExists = fullPath.toFile().exists()
+    if (fileExists)
+      Invalid("File already exists")
+    else
+      Valid
+  }
+
+  private[wizards] def checkTypeExists(project: IProject, fullyQualifiedType: String, path: String): Validation = {
+    val v = checkFileExists(project, path + ".scala")
+    if (v.isInvalid)
+      v
+    else {
+      val scalaProject = ScalaPlugin.plugin.asScalaProject(project)
+      val typeExists = scalaProject flatMap { scalaProject =>
+        scalaProject.presentationCompiler { compiler =>
+          compiler.askOption { () =>
+            compiler.rootMirror.getClassIfDefined(fullyQualifiedType) != compiler.NoSymbol
+          }
+        }.flatten
+      } getOrElse false
+
+      if (typeExists)
+        Invalid("Type already exists")
+      else
+        Valid
     }
   }
 }
