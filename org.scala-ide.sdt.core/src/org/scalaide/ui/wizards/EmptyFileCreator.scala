40bf486fa88a691647bf2cdb93d13bb8b453f755
Simon Schaefer <mail@antoras.de>
2014-06-30 00:53:13 +0200

    Create `EmptyFileCreator`
    This file creator can only create empty files. It inserts an empty
    template because that is the easiest way to create an empty file without
    touching the file creator interface.

---
+++ org.scala-ide.sdt.core/src/org/scalaide/ui/wizards/EmptyFileCreator.scala
@@ -0,0 +1,24 @@
+package org.scalaide.ui.wizards
+
+import org.eclipse.core.resources.IProject
+import org.eclipse.core.resources.IResource
+
+/**
+ * File creator that can only create empty files. It is based on
+ * `ScalaFileCreator` because it defines useful initial path and validation
+ * logic.
+ */
+class EmptyFileCreator extends ScalaFileCreator {
+
+  override def validateName(project: IProject, name: String): Validation =
+    checkFileExists(project, name)
+
+  override def initialPath(res: IResource): String =
+    generateInitialPath(
+        path = res.getFullPath().segments(),
+        srcDirs = Nil,
+        isDirectory = res.getType() == IResource.FOLDER)
+
+  override def templateVariables(project: IProject, name: String): Map[String, String] =
+    Map()
+}
