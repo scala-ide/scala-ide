4d65329a33252a093a0bd2ede811574a34db22f5
Simon Schaefer <mail@antoras.de>
2014-07-14 19:21:59 +0200

    Simplify empty file creator
    This file creator doesn't provide any special behavior anymore beside
    from creating an empty file and checking if it already exists. The
    decision to inherit from `ScalaFileCreator` was also reverted.

--- org.scala-ide.sdt.core/src/org/scalaide/ui/wizards/EmptyFileCreator.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/ui/wizards/EmptyFileCreator.scala
@@ -2,21 +2,28 @@ package org.scalaide.ui.wizards
 
 import org.eclipse.core.resources.IFolder
 import org.eclipse.core.resources.IResource
+import org.eclipse.core.resources.ResourcesPlugin
+import org.eclipse.core.runtime.IPath
+import org.scalaide.util.internal.eclipse.FileUtils
 
 /**
- * File creator that can only create empty files. It is based on
- * `ScalaFileCreator` because it defines some useful validation and file
- * creation logic.
+ * File creator that can only create empty files.
  */
-class EmptyFileCreator extends ScalaFileCreator {
+class EmptyFileCreator extends FileCreator {
+
+  override def createFilePath(folder: IFolder, name: String): IPath = {
+    val root = ResourcesPlugin.getWorkspace().getRoot()
+    root.getRawLocation().append(folder.getFullPath()).append(name)
+  }
 
   override def validateName(folder: IFolder, name: String): Validation =
-    checkFileExists(folder, name)
+    if (FileUtils.existsWorkspaceFile(folder.getFullPath().append(name)))
+      Invalid("File already exists")
+    else
+      Valid
 
   override def initialPath(res: IResource): String =
-    generateInitialPath(
-        path = res.getFullPath().segments(),
-        isDirectory = res.getType() == IResource.FOLDER)
+    ""
 
   override def completionEntries(folder: IFolder, name: String): Seq[String] =
     Seq()
@@ -24,15 +31,4 @@ class EmptyFileCreator extends ScalaFileCreator {
   override def templateVariables(folder: IFolder, name: String): Map[String, String] =
     Map()
 
-  private[wizards] def generateInitialPath(path: Seq[String], isDirectory: Boolean): String = {
-    if (path.size < 3)
-      ""
-    else {
-      val Seq(_, rawSubPath @ _*) = path
-      val subPath = if (isDirectory) rawSubPath else rawSubPath.init
-      val p = subPath.mkString("/")
-
-      if (p.isEmpty()) "" else s"$p/"
-    }
-  }
 }
