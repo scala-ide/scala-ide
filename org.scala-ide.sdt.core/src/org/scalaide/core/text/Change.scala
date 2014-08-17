b7b606eeab1d8cbc1b165b30a2938aae3160ae86
Simon Schaefer <mail@antoras.de>
2014-08-17 15:19:09 +0200

    Add copy method and better apply method to `TextChange`
    The apply method now creates the best fit text change object and not
    only an `Replace` object.
    The copy method considers forwards to this apply method to keep the
    behavior.

--- org.scala-ide.sdt.core/src/org/scalaide/core/text/Change.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/core/text/Change.scala
@@ -4,7 +4,12 @@ trait Change
 
 object TextChange {
   def apply(start: Int, end: Int, text: String): TextChange =
-    Replace(start, end, text)
+    if (start == end)
+      Add(start, text)
+    else if (text.isEmpty())
+      Remove(start, end)
+    else
+      Replace(start, end, text)
 
   def unapply(change: TextChange): Option[(Int, Int, String)] =
     Some((change.start, change.end, change.text))
@@ -13,6 +18,9 @@ trait TextChange extends Change {
   def start: Int
   def end: Int
   def text: String
+
+  def copy(start: Int = this.start, end: Int = this.end, text: String = this.text): TextChange =
+    TextChange(start, end, text)
 
   override def toString(): String =
     s"""TextChange(start=$start, end=$end, text="$text")"""
