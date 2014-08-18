0420d916d485308e399e62816fdb728ea3852833
Simon Schaefer <mail@antoras.de>
2014-08-18 14:55:39 +0200

    Remove dependency to internal JDT spelling engine

--- org.scala-ide.sdt.core/src/org/scalaide/ui/internal/editor/spelling/ScalaSpellingEngine.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/ui/internal/editor/spelling/ScalaSpellingEngine.scala
@@ -3,8 +3,8 @@ package org.scalaide.ui.internal.editor.spelling
 import org.eclipse.core.runtime.AssertionFailedException
 import org.eclipse.core.runtime.IProgressMonitor
 import org.eclipse.jdt.internal.ui.text.spelling.JavaSpellingProblem
+import org.eclipse.jdt.internal.ui.text.spelling.SpellCheckEngine
 import org.eclipse.jdt.internal.ui.text.spelling.SpellCheckIterator
-import org.eclipse.jdt.internal.ui.text.spelling.SpellingEngine
 import org.eclipse.jdt.internal.ui.text.spelling.engine.ISpellChecker
 import org.eclipse.jdt.internal.ui.text.spelling.engine.ISpellEvent
 import org.eclipse.jdt.internal.ui.text.spelling.engine.ISpellEventListener
@@ -14,30 +14,41 @@ import org.eclipse.jface.text.BadLocationException
 import org.eclipse.jface.text.IDocument
 import org.eclipse.jface.text.IRegion
 import org.eclipse.jface.text.TextUtilities
+import org.eclipse.ui.texteditor.spelling.ISpellingEngine
 import org.eclipse.ui.texteditor.spelling.ISpellingProblemCollector
+import org.eclipse.ui.texteditor.spelling.SpellingContext
 import org.scalaide.core.internal.lexical.ScalaPartitions._
 
 /**
  * Checks Scala files for spelling mistakes.
  */
-class ScalaSpellingEngine extends SpellingEngine {
+class ScalaSpellingEngine extends ISpellingEngine {
 
-  def check(document: IDocument, regions: Array[IRegion], checker: ISpellChecker, collector: ISpellingProblemCollector, monitor: IProgressMonitor) {
+  override def check(document: IDocument, regions: Array[IRegion], context: SpellingContext, collector: ISpellingProblemCollector, monitor: IProgressMonitor) {
+    val engine = SpellCheckEngine.getInstance()
+    val checker = engine.getSpellChecker()
+
+    if (checker != null) {
+      try check(document, regions, checker, collector, monitor)
+      catch {
+        case _: BadLocationException | _: AssertionFailedException =>
+        // Ignore: the document has been changed in another thread and will be checked again
+      }
+    }
+  }
+
+  private def check(document: IDocument, regions: Array[IRegion], checker: ISpellChecker, collector: ISpellingProblemCollector, monitor: IProgressMonitor) {
     val spellCheckablePartitionTypes = getSpellCheckablePartitionTypes
     val listener = new SpellEventListener(collector, document)
-    try {
-      for {
-        region <- regions
-        partition <- TextUtilities.computePartitioning(document, JAVA_PARTITIONING, region.getOffset, region.getLength, false)
-        if spellCheckablePartitionTypes contains partition.getType
-      } {
-        if (monitor != null && monitor.isCanceled || listener.isProblemsThresholdReached)
-          return
-        checker.execute(listener, new SpellCheckIterator(document, partition, checker.getLocale))
-      }
-    } catch {
-      case _: BadLocationException | _: AssertionFailedException =>
-      // Ignore: the document has been changed in another thread and will be checked again
+
+    for {
+      region <- regions
+      partition <- TextUtilities.computePartitioning(document, JAVA_PARTITIONING, region.getOffset, region.getLength, false)
+      if spellCheckablePartitionTypes contains partition.getType
+    } {
+      if (monitor != null && monitor.isCanceled || listener.isProblemsThresholdReached)
+        return
+      checker.execute(listener, new SpellCheckIterator(document, partition, checker.getLocale))
     }
   }
 
@@ -51,7 +62,7 @@ class ScalaSpellingEngine extends SpellingEngine {
     val problemsThreshold = PreferenceConstants.getPreferenceStore.getInt(PreferenceConstants.SPELLING_PROBLEMS_THRESHOLD)
     var problemCount = 0
 
-    def handle(event: ISpellEvent) =
+    override def handle(event: ISpellEvent) =
       if (problemCount < problemsThreshold) {
         problemCount += 1
         collector.accept(new JavaSpellingProblem(event, document))
