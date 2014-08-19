1a899dd19f4d829ecb5659ec552c12c2d3a42622
Simon Schaefer <mail@antoras.de>
2014-08-19 17:17:05 +0200

    Interrupt Scala builder on resource change
    Whenever Eclipse is configured to automatically build, an interrupt
    event is sent to all builders whenever an resource change happens in the
    workspace.
    
    The Scala builder didn't react on such an interrupt event so far - this
    changes with this commit.
    
    Beside from aborting the current running build, the build is restarted
    after the resource change event is handled.
    
    Fixes #1002229

--- org.scala-ide.sdt.core/src/org/scalaide/core/internal/builder/ScalaBuilder.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/core/internal/builder/ScalaBuilder.scala
@@ -71,7 +71,7 @@ class ScalaBuilder extends IncrementalProjectBuilder with JDTBuilderFacade with 
           val removed0 = new HashSet[IFile]
 
           getDelta(project.underlying).accept(new IResourceDeltaVisitor {
-            def visit(delta : IResourceDelta) = {
+            override def visit(delta : IResourceDelta) = {
               delta.getResource match {
                 case file : IFile if plugin.isBuildable(file) && project.sourceFolders.exists(_.isPrefixOf(file.getLocation)) =>
                   delta.getKind match {
@@ -114,7 +114,7 @@ class ScalaBuilder extends IncrementalProjectBuilder with JDTBuilderFacade with 
       }
     }
 
-    val subMonitor = SubMonitor.convert(monitor, 100).newChild(100, SubMonitor.SUPPRESS_NONE)
+    val subMonitor = SubMonitor.convert(new BuildMonitor(monitor, this), 100).newChild(100, SubMonitor.SUPPRESS_NONE)
     subMonitor.beginTask("Running Scala Builder on " + project.underlying.getName, 100)
 
     val projectsInError = project.transitiveDependencies.filter(p => plugin.getScalaProject(p).buildManager.hasErrors)
