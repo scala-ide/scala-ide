702c5c3039fde4d69aaf22ac35918197e0a9d63b
Simon Schaefer <mail@antoras.de>
2014-08-21 18:26:44 +0200

    Add `Remove` operation to `AutoEditTests`
    This allows to write tests that check if the removal of text works
    correctly.

--- org.scala-ide.sdt.core.tests/src/org/scalaide/extensions/autoedits/AutoEditTests.scala
+++ org.scala-ide.sdt.core.tests/src/org/scalaide/extensions/autoedits/AutoEditTests.scala
@@ -3,10 +3,12 @@ package autoedits
 
 import org.eclipse.jface.text.{Document => EDocument}
 import org.scalaide.core.internal.text.TextDocument
+import org.scalaide.core.{text => impl}
 import org.scalaide.core.text.CursorUpdate
 import org.scalaide.core.text.Document
 import org.scalaide.core.text.TextChange
 import org.scalaide.core.ui.TextEditTests
+import org.scalaide.extensions.AutoEdit
 
 abstract class AutoEditTests extends TextEditTests {
 
@@ -21,9 +23,8 @@ abstract class AutoEditTests extends TextEditTests {
 
   override def source = udoc.get()
 
-  case class Add(text: String) extends Operation {
-    override def execute() = {
-      val change = TextChange(caretOffset, caretOffset, text)
+  abstract class AutoEditOperation extends Operation {
+    def applyChange(change: TextChange): Unit = {
       val appliedChange = autoEdit(new TextDocument(udoc), change).perform()
 
       appliedChange match {
@@ -39,8 +40,30 @@ abstract class AutoEditTests extends TextEditTests {
 
         case None =>
           udoc.replace(change.start, change.end-change.start, change.text)
-          caretOffset += 1
+          change match {
+            case impl.Add(_, text) =>
+              caretOffset += text.size
+            case impl.Remove(start, end) =>
+              caretOffset += start-end
+          }
       }
+    }
+  }
+
+  case class Add(text: String) extends AutoEditOperation {
+    override def execute() = {
+      val change = impl.Add(caretOffset, text)
+      applyChange(change)
+    }
+  }
+
+  case class Remove(text: String) extends AutoEditOperation {
+    override def execute() = {
+      val actual = udoc.get().substring(caretOffset-text.length(), caretOffset)
+      require(text == actual, "removeable content does not equal to the declared content")
+
+      val change = impl.Remove(caretOffset-text.length(), caretOffset)
+      applyChange(change)
     }
   }
 }
