a17893c438494bfffe2c17f242dc0a80f4734695
François Garillot <francois.garillot@typesafe.com>
2014-09-17 17:36:37 +0200

    Constrain type of CompletionProposal

--- org.scala-ide.sdt.core/src/org/scalaide/core/completion/CompletionProposal.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/core/completion/CompletionProposal.scala
@@ -9,6 +9,7 @@ import org.scalaide.util.internal.eclipse.EditorUtils
 import org.scalaide.core.compiler.InteractiveCompilationUnit
 import org.scalaide.core.internal.ScalaPlugin
 import org.scalaide.ui.internal.preferences.EditorPreferencePage
+import org.eclipse.jface.internal.text.html.BrowserInput
 
 object HasArgs extends Enumeration {
   val NoArgs, EmptyArgs, NonEmptyArgs = Value
@@ -64,7 +65,7 @@ case class CompletionProposal(
   paramTypes: List[List[String]],          // parameter types matching parameter names (excluding implicit parameter sections)
   fullyQualifiedName: String, // for Class, Trait, Type, Objects: the fully qualified name
   needImport: Boolean,        // for Class, Trait, Type, Objects: import statement has to be added
-  documentation: () => Option[Object]  // on-demand generated documentation HTML.
+  documentation: () => Option[BrowserInput]  // on-demand generated documentation HTML.
 ) {
 
   /** `getParamNames` is expensive, save this result once computed.
