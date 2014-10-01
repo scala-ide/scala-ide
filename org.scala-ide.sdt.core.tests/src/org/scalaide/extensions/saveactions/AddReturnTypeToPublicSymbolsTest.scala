ae09036778dd7c16beb9e26512721e7a97bf2748
Simon Schaefer <mail@antoras.de>
2014-10-01 16:37:57 +0200

    Add package declaration to save action tests

--- org.scala-ide.sdt.core.tests/src/org/scalaide/extensions/saveactions/AddReturnTypeToPublicSymbolsTest.scala
+++ org.scala-ide.sdt.core.tests/src/org/scalaide/extensions/saveactions/AddReturnTypeToPublicSymbolsTest.scala
@@ -19,12 +19,14 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_no_return_type_if_it_already_exists() = """^
+    package add_no_return_type_if_it_already_exists
     class X {
       def meth: java.io.File = new java.io.File("")
       val value: java.io.File = new java.io.File("")
       var variable: java.io.File = new java.io.File("")
     }
     """ becomes """^
+    package add_no_return_type_if_it_already_exists
     class X {
       def meth: java.io.File = new java.io.File("")
       val value: java.io.File = new java.io.File("")
@@ -34,10 +36,12 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_no_return_type_to_private_def() = """^
+    package add_no_return_type_to_private_def
     class X {
       private def meth = new java.io.File("")
     }
     """ becomes """^
+    package add_no_return_type_to_private_def
     class X {
       private def meth = new java.io.File("")
     }
@@ -45,10 +49,12 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_no_return_type_to_private_val() = """^
+    package add_no_return_type_to_private_val
     class X {
       private val value = new java.io.File("")
     }
     """ becomes """^
+    package add_no_return_type_to_private_val
     class X {
       private val value = new java.io.File("")
     }
@@ -56,10 +62,12 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_no_return_type_to_private_var() = """^
+    package add_no_return_type_to_private_var
     class X {
       private var variable = new java.io.File("")
     }
     """ becomes """^
+    package add_no_return_type_to_private_var
     class X {
       private var variable = new java.io.File("")
     }
@@ -67,6 +75,7 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_no_return_type_to_nested_def() = """^
+    package add_no_return_type_to_nested_def
     class X {
       def meth: java.io.File = {
         def m = new java.io.File("")
@@ -78,6 +87,7 @@ class AddReturnTypeToPublicSymbolsTest {
       }
     }
     """ becomes """^
+    package add_no_return_type_to_nested_def
     class X {
       def meth: java.io.File = {
         def m = new java.io.File("")
@@ -92,6 +102,7 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_no_return_type_to_nested_val() = """^
+    package add_no_return_type_to_nested_val
     class X {
       val value: java.io.File = {
         val m = new java.io.File("")
@@ -103,6 +114,7 @@ class AddReturnTypeToPublicSymbolsTest {
       }
     }
     """ becomes """^
+    package add_no_return_type_to_nested_val
     class X {
       val value: java.io.File = {
         val m = new java.io.File("")
@@ -117,6 +129,7 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_no_return_type_to_nested_var() = """^
+    package add_no_return_type_to_nested_var
     class X {
       var variable: java.io.File = {
         var m = new java.io.File("")
@@ -128,6 +141,7 @@ class AddReturnTypeToPublicSymbolsTest {
       }
     }
     """ becomes """^
+    package add_no_return_type_to_nested_var
     class X {
       var variable: java.io.File = {
         var m = new java.io.File("")
@@ -142,10 +156,12 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_return_type_to_public_def() = """^
+    package add_return_type_to_public_def
     class X {
       def meth = new java.io.File("")
     }
     """ becomes """^
+    package add_return_type_to_public_def
     class X {
       def meth: java.io.File = new java.io.File("")
     }
@@ -153,10 +169,12 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_return_type_to_public_val() = """^
+    package add_return_type_to_public_val
     class X {
       val value = new java.io.File("")
     }
     """ becomes """^
+    package add_return_type_to_public_val
     class X {
       val value: java.io.File = new java.io.File("")
     }
@@ -164,10 +182,12 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_return_type_to_public_var() = """^
+    package add_return_type_to_public_var
     class X {
       val variable = new java.io.File("")
     }
     """ becomes """^
+    package add_return_type_to_public_var
     class X {
       val variable: java.io.File = new java.io.File("")
     }
@@ -175,6 +195,7 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_return_type_if_def_belongs_to_inner_class() = """^
+    package add_return_type_if_def_belongs_to_inner_class
     class X {
       class Y {
         trait Z1 {
@@ -189,6 +210,7 @@ class AddReturnTypeToPublicSymbolsTest {
       }
     }
     """ becomes """^
+    package add_return_type_if_def_belongs_to_inner_class
     class X {
       class Y {
         trait Z1 {
@@ -206,6 +228,7 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_no_return_type_if_def_belongs_to_private_inner_class() = """^
+    package add_no_return_type_if_def_belongs_to_private_inner_class
     class X {
       private class Y {
         trait Z1 {
@@ -220,6 +243,7 @@ class AddReturnTypeToPublicSymbolsTest {
       }
     }
     """ becomes """^
+    package add_no_return_type_if_def_belongs_to_private_inner_class
     class X {
       private class Y {
         trait Z1 {
@@ -237,6 +261,7 @@ class AddReturnTypeToPublicSymbolsTest {
 
   @Test
   def add_no_return_type_to_refinement_types() = """^
+    package add_no_return_type_to_refinement_types
     class X {
       val t = new T {
         def meth = 0
@@ -249,6 +274,7 @@ class AddReturnTypeToPublicSymbolsTest {
       def meth: Int
     }
     """ becomes """^
+    package add_no_return_type_to_refinement_types
     class X {
       val t = new T {
         def meth = 0
