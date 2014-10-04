fbd9e737a902717840bba8715588d8af96b81097
Simon Schaefer <mail@antoras.de>
2014-10-04 23:57:17 +0200

    Allow quick assist processor to only execute a single assist
    This contains also a change of the provided default handler - no
    separate handler is provided anymore.

--- org.scala-ide.sdt.core/src/org/scalaide/core/internal/quickassist/QuickAssistHandler.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/core/internal/quickassist/QuickAssistHandler.scala
@@ -14,7 +14,6 @@ import org.eclipse.jface.text.source.Annotation
 import org.eclipse.ui.IEditorInput
 import org.scalaide.core.quickassist.BasicCompletionProposal
 import org.scalaide.core.quickassist.InvocationContext
-import org.scalaide.util.eclipse.EclipseUtils
 import org.scalaide.util.eclipse.EditorUtils
 
 /**
@@ -26,7 +25,7 @@ final class QuickAssistHandler extends AbstractHandler {
   override def execute(event: ExecutionEvent): AnyRef = {
     EditorUtils.doWithCurrentEditor { editor =>
       val input = editor.getEditorInput
-      val p = new QuickAssistProcessor(input)
+      val p = new QuickAssistProcessor(input, event.getCommand.getId)
       val a = new QuickAssistAssistant
       a.setQuickAssistProcessor(p)
       a.install(editor.getViewer)
@@ -45,7 +44,35 @@ object NoProposals extends BasicCompletionProposal(0, "No suggestions available"
   override def apply(doc: IDocument): Unit = ()
 }
 
-final class QuickAssistProcessor(input: IEditorInput) extends IQuickAssistProcessor {
+object QuickAssistProcessor {
+  /**
+   * All available quick assists.
+   */
+  val QuickAssists: Seq[QuickAssistMapping] = QuickAssistMapping.mappings
+
+  /**
+   * The ID that needs to be used when all available quick assists should be
+   * executed in the companion class.
+   */
+  final val DefaultId = "org.scalaide.core.quickassist"
+}
+
+/**
+ * A quick assist processor that executes quick assists that are registered
+ * for the Scala editor. Always returns at least one element when
+ * `computeQuickAssistProposals` is executed. This rule is ensured by returning
+ * an array that contains [[NoProposals]] when no proposals are found.
+ *
+ * @param input
+ *        The editor where the quick assist feature should be invoked on.
+ * @param id
+ *        Represents the ID that belongs to a quick assist. The corresponding
+ *        quick assist is executed by this processor. If this ID is the same as
+ *        [[QuickAssistProcessor.DefaultId]] all available quick assists are
+ *        executed. If the ID doesn't exist no quick assists are executed.
+ */
+final class QuickAssistProcessor(input: IEditorInput, id: String) extends IQuickAssistProcessor {
+  import QuickAssistProcessor._
 
   override def computeQuickAssistProposals(ctx: IQuickAssistInvocationContext): Array[ICompletionProposal] = {
     val cu = JavaUI.getWorkingCopyManager.getWorkingCopy(input)
@@ -64,15 +91,15 @@ final class QuickAssistProcessor(input: IEditorInput) extends IQuickAssistProces
         ps
     }
 
-    val ictx = InvocationContext(cu, ctx.getOffset, ctx.getLength, problems)
-    val quickAssists = EclipseUtils.withSafeRunner("Error occurred while executing ScalaQuickAssistProcessor") {
-      new ScalaQuickAssistProcessor().compute(ictx)
-    }.getOrElse(Seq())
-    val quickFixes = EclipseUtils.withSafeRunner("Error occurred while executing ScalaQuickFixProcessor") {
-      new ScalaQuickFixProcessor().compute(ictx)
-    }.getOrElse(Seq())
+    val assists =
+      if (id == DefaultId)
+        QuickAssists
+      else
+        QuickAssists.find(_.id == id).toSeq
 
-    val sorted = (quickAssists ++ quickFixes).sortBy(-_.getRelevance())
+    val ictx = InvocationContext(cu, ctx.getOffset, ctx.getLength, problems)
+    val proposals = assists flatMap (_ withInstance (_ compute ictx))
+    val sorted = proposals.flatten.sortBy(-_.getRelevance())
 
     if (sorted.isEmpty)
       Array(NoProposals)
