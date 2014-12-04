0c74b78b1024a4f9f1c5f97e71d6219597a9ae00
Michał Pociecha <michal.pociecha@gmail.com>
2014-12-04 15:38:32 +0100

    Test suite for operations on primitives
    Test includes:
     - `!`, `==`, `!=`, `<`, `>`, `<=`, `>=` on Booleans,
     - `||`, `&&`, `&`, `|`, `^` on Booleans,
     - `+`, `-`, `*`, `/`, `%` on numerics,
     - bitwise `&`, `|`, `<<`, `>>`, `>>>`, `^` on integrals,
     - unary `+`, `-` on numerics and unary `~` on integrals,
     - type conversion (`toXXX`)
     - and various other methods defined in `ScalaNumberProxy`,
       `RichXXX`, `Fractional` and `Integral`.
    
    This test suite was extracted from previous commit to ease review process.

---
+++ org.scala-ide.sdt.debug.tests/src/org/scalaide/debug/internal/expression/proxies/primitives/operations/NumericComparisonTest.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2014 Contributor. All rights reserved.
+ */
+package org.scalaide.debug.internal.expression.proxies.primitives.operations
+
+import org.junit.Test
+import org.junit.Ignore
+import org.scalaide.debug.internal.expression.Names.Java
+import org.scalaide.debug.internal.expression.BaseIntegrationTest
+import org.scalaide.debug.internal.expression.BaseIntegrationTestCompanion
+import org.scalaide.debug.internal.expression.TestValues
+
+class NumericComparisonTest extends BaseIntegrationTest(NumericComparisonTest) {
+
+  import TestValues.ValuesTestCase._
+  import TestValues.any2String
+
+  private def baseBooleanTest(operator: String, expected: Boolean): Unit =
+    eval(s"int $operator int2", expected.toString, Java.boxed.Boolean) // compare to 1
+
+  @Test
+  def integerComparisonTest(): Unit = {
+    baseBooleanTest("<", true)
+    baseBooleanTest(">", false)
+    baseBooleanTest(">=", false)
+    baseBooleanTest("<=", true)
+    baseBooleanTest("==", false)
+    baseBooleanTest("!=", true)
+  }
+
+  @Test
+  def testComparisionForDifferentTypes(): Unit = {
+    eval("int == double", int == double, Java.boxed.Boolean)
+    eval("int != double", int != double, Java.boxed.Boolean)
+    eval("1 < 5L", 1 < 5L, Java.boxed.Boolean)
+    eval("int < 5L", int < 5L, Java.boxed.Boolean)
+    eval("int < long", int < long, Java.boxed.Boolean)
+    eval("int <= long", int <= long, Java.boxed.Boolean)
+    eval("int > long", int > long, Java.boxed.Boolean)
+    eval("int >= long", int >= long, Java.boxed.Boolean)
+    eval("short >= double", short >= double, Java.boxed.Boolean)
+    eval("long >= int", long >= int, Java.boxed.Boolean)
+    eval("double >= short", double >= short, Java.boxed.Boolean)
+  }
+}
+
+object NumericComparisonTest extends BaseIntegrationTestCompanion
