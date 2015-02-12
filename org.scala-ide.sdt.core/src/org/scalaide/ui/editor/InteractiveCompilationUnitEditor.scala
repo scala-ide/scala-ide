0c9a75ded17f38bd7d11518f6a51f657b31ff1ac
Simon Schäfer <mail@antoras.de>
2015-02-12 19:35:36 +0100

    Fix NPE in semantic highlighting job
    In case the editor is closed, no interactive compilation unit can be
    retrieved and null is returned. This bug occurred while stepping through
    multiple files in the debugger.
    
    Fixes #1002386

--- org.scala-ide.sdt.core/src/org/scalaide/ui/editor/InteractiveCompilationUnitEditor.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/ui/editor/InteractiveCompilationUnitEditor.scala
@@ -3,5 +3,6 @@ package org.scalaide.ui.editor
 import org.scalaide.core.compiler.InteractiveCompilationUnit
 
 trait InteractiveCompilationUnitEditor extends DecoratedInteractiveEditor {
+  /** Returns `null` if the editor is closed. */
   def getInteractiveCompilationUnit(): InteractiveCompilationUnit
 }
