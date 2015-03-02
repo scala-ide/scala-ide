e125b8b7fce4645ea3dc49f2febfd24f98d58f4e
Simon Schäfer <mail@antoras.de>
2015-03-02 23:43:46 +0100

    Cleanup `SbtProjectSupport`

--- org.scala-ide.sbt.core/src/org/scalaide/sbt/core/SbtProjectSupport.scala
+++ org.scala-ide.sbt.core/src/org/scalaide/sbt/core/SbtProjectSupport.scala
@@ -1,30 +1,32 @@
 package org.scalaide.sbt.core
 
 import java.io.File
-import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
+
 import org.eclipse.core.resources.IProject
 import org.eclipse.core.resources.IProjectDescription
 import org.eclipse.core.resources.IResource
 import org.eclipse.core.resources.IWorkspace
 import org.eclipse.core.resources.ResourcesPlugin
+import org.eclipse.core.runtime.IPath
 import org.eclipse.core.runtime.IProgressMonitor
 import org.eclipse.core.runtime.Path
 import org.eclipse.core.runtime.SubMonitor
+import org.eclipse.jdt.core.IClasspathEntry
 import org.eclipse.jdt.core.JavaCore
 import org.eclipse.jdt.internal.core.ClasspathEntry
 import org.eclipse.jdt.ui.PreferenceConstants
-import org.eclipse.jdt.core.IClasspathEntry
-import org.scalaide.sbt.util.SourceUtils._
-import org.scalaide.sbt.util.PicklingUtils._
-import sbt.protocol.Attributed
 import org.scalaide.core.SdtConstants
+import org.scalaide.sbt.util.PicklingUtils._
+
+import sbt.protocol.Attributed
 
 object SbtProjectSupport {
 
-  def createWorkspaceProject(build: SbtBuild, projectName: String, builderName: String, monitor: IProgressMonitor): Future[IProject] = {
-
-    val progress = SubMonitor.convert(monitor, 100);
+  def createWorkspaceProject(build: SbtBuild, projectName: String, builderName: String, monitor: IProgressMonitor)(implicit ctx: ExecutionContext): Future[IProject] = {
+    val progress = SubMonitor.convert(monitor, 100)
 
     // create the project in the workspace
     progress.beginTask(s"$projectName: create project", 10)
@@ -61,7 +63,7 @@ object SbtProjectSupport {
     res
   }
 
-  private def createJavaProjectDescription(build: SbtBuild, projectName: String, builderName: String, workspace: IWorkspace): Future[IProjectDescription] = {
+  private def createJavaProjectDescription(build: SbtBuild, projectName: String, builderName: String, workspace: IWorkspace)(implicit ctx: ExecutionContext): Future[IProjectDescription] = {
     for {
       projectRoot <- build.setting[File](projectName, "baseDirectory")
     } yield {
@@ -78,15 +80,13 @@ object SbtProjectSupport {
     }
   }
 
-  private def configureClassPath(build: SbtBuild, projectName: String, project: IProject, monitor: IProgressMonitor): Future[Unit] = {
-    //val logger = build.system.log
-
+  private def configureClassPath(build: SbtBuild, projectName: String, project: IProject, monitor: IProgressMonitor)(implicit ctx: ExecutionContext): Future[Unit] = {
     val srcDirs = build.setting[Seq[File]](projectName, "sourceDirectories", Some("compile"))
     val clsDirs = build.setting[File](projectName, "classDirectory", Some("compile"))
-    val classpath = build.setting[Seq[Attributed[File]]](projectName, "externalDependencyClasspath", Some("compile")).map(_.map(_.data))
+    val classpath = build.setting[Seq[Attributed[File]]](projectName, "externalDependencyClasspath", Some("compile"))
     val testSrcDirs = build.setting[Seq[File]](projectName, "sourceDirectories", Some("test"))
     val testClsDirs = build.setting[File](projectName, "classDirectory", Some("test"))
-    val testClasspath = build.setting[Seq[Attributed[File]]](projectName, "externalDependencyClasspath", Some("test")).map(_.map(_.data))
+    val testClasspath = build.setting[Seq[Attributed[File]]](projectName, "externalDependencyClasspath", Some("test"))
 
     for {
       srcDir <- srcDirs
@@ -110,7 +110,7 @@ object SbtProjectSupport {
 
       val fullClasspath = (cp ++ testCp).distinct
 
-      val referencedJars = fullClasspath.flatMap(createEclipseClasspathEntry)
+      val referencedJars = fullClasspath.flatMap(cp ⇒ createEclipseClasspathEntry(cp.data))
 
       val classpath = sourcePaths ++ testSourcePaths ++ referencedJars ++ PreferenceConstants.getDefaultJRELibrary()
 
@@ -118,25 +118,24 @@ object SbtProjectSupport {
     }
   }
 
-  val ScalaLibraryRegex = ".*org\\.scala-lang/scala-library.*".r
-  val ScalaCompilerRegex = ".*org\\.scala-lang/scala-compiler.*".r
-  val ScalaReflectRegex = ".*org\\.scala-lang/scala-reflect.*".r
+  private val ScalaLibraryRegex = ".*org\\.scala-lang/scala-library.*".r
+  private val ScalaCompilerRegex = ".*org\\.scala-lang/scala-compiler.*".r
+  private val ScalaReflectRegex = ".*org\\.scala-lang/scala-reflect.*".r
 
-  def createEclipseClasspathEntry(file: File): Option[IClasspathEntry] = {
-    val path = file.getAbsolutePath()
-    path match {
+  private def createEclipseClasspathEntry(file: File): Option[IClasspathEntry] = {
+    file.getAbsolutePath() match {
       case ScalaLibraryRegex() =>
         Some(JavaCore.newContainerEntry(Path.fromPortableString(SdtConstants.ScalaLibContId)))
       case ScalaCompilerRegex() =>
         Some(JavaCore.newContainerEntry(Path.fromPortableString(SdtConstants.ScalaCompilerContId)))
       case ScalaReflectRegex() =>
         None
-      case _ =>
+      case path =>
         Some(JavaCore.newLibraryEntry(Path.fromOSString(path), null, null))
     }
   }
 
-  private def pathInProject(project: IProject, file: File) = {
+  private def pathInProject(project: IProject, file: File): IPath = {
     project.getFullPath().append(Path.fromOSString(file.getAbsolutePath()).makeRelativeTo(project.getLocation()))
   }
 
