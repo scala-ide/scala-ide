cadd4226933b2b8ec05d4e4c5e40adc9c8d32772
Jerzy Müller <sidus.gemini+gh@gmail.com>
2015-05-27 18:48:55 +0200

    Ignore test, not comment it out

--- org.scala-ide.sdt.debug.tests/src/org/scalaide/debug/internal/ScalaDebugTestSuite.scala
+++ org.scala-ide.sdt.debug.tests/src/org/scalaide/debug/internal/ScalaDebugTestSuite.scala
@@ -3,18 +3,19 @@
  */
 package org.scalaide.debug.internal
 
-import org.junit.runners.Suite
 import org.junit.runner.RunWith
+import org.junit.runners.Suite
 import org.scalaide.debug.internal.editor.StackFrameVariableOfTreeFinderTest
 import org.scalaide.debug.internal.hcr.HotCodeReplaceTest
 import org.scalaide.debug.internal.launching.RemoteConnectorTest
-import org.scalaide.debug.internal.model.ScalaThreadTest
-import org.scalaide.debug.internal.model.ScalaStackFrameTest
-import org.scalaide.debug.internal.model.ScalaValueTest
-import org.scalaide.debug.internal.model.ScalaDebugTargetTest
 import org.scalaide.debug.internal.model.DebugTargetTerminationTest
 import org.scalaide.debug.internal.model.MethodClassifierUnitTest
 import org.scalaide.debug.internal.model.ScalaDebugCacheTest
+import org.scalaide.debug.internal.model.ScalaDebugModelPresentationTest
+import org.scalaide.debug.internal.model.ScalaDebugTargetTest
+import org.scalaide.debug.internal.model.ScalaStackFrameTest
+import org.scalaide.debug.internal.model.ScalaThreadTest
+import org.scalaide.debug.internal.model.ScalaValueTest
 
 /**
  * Junit test suite for the Scala debugger.
@@ -26,7 +27,7 @@ import org.scalaide.debug.internal.model.ScalaDebugCacheTest
     classOf[ScalaDebugSteppingTest],
     classOf[ScalaDebugResumeTest],
     classOf[ScalaThreadTest],
-//    classOf[ScalaDebugModelPresentationTest], // TODO: find where to put this test
+    classOf[ScalaDebugModelPresentationTest],
     classOf[ScalaStackFrameTest],
     classOf[ScalaValueTest],
 //    classOf[LibraryJarInBootstrapTest], // the test is not running on command line right now
