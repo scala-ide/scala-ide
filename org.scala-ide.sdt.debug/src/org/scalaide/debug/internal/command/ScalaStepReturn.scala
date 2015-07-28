4f11f5e300b65eac37568c21f7d5d6015a68bccd
wpopielarski <wpopielarski@gmail.com>
2015-07-28 08:54:03 +0200

    Scala debug steping to future

--- org.scala-ide.sdt.debug/src/org/scalaide/debug/internal/command/ScalaStepReturn.scala
+++ org.scala-ide.sdt.debug/src/org/scalaide/debug/internal/command/ScalaStepReturn.scala
@@ -1,6 +1,5 @@
 package org.scalaide.debug.internal.command
 
-import org.scalaide.debug.internal.BaseDebuggerActor
 import org.scalaide.debug.internal.model.JdiRequestFactory
 import org.scalaide.debug.internal.model.ScalaDebugTarget
 import org.scalaide.debug.internal.model.ScalaStackFrame
@@ -8,18 +7,16 @@ import org.scalaide.debug.internal.model.ScalaThread
 import org.eclipse.debug.core.DebugEvent
 import com.sun.jdi.event.StepEvent
 import com.sun.jdi.request.StepRequest
+import java.util.concurrent.atomic.AtomicBoolean
+import org.scalaide.debug.internal.JdiEventReceiver
+import scala.concurrent.Future
+import com.sun.jdi.event.Event
 
 object ScalaStepReturn {
   def apply(scalaStackFrame: ScalaStackFrame): ScalaStep = {
     val stepReturnRequest = JdiRequestFactory.createStepRequest(StepRequest.STEP_LINE, StepRequest.STEP_OUT, scalaStackFrame.thread)
-
-    val companionActor = new ScalaStepReturnActor(scalaStackFrame.getDebugTarget, scalaStackFrame.thread, stepReturnRequest) {
-      // TODO: when implementing support without filtering, need to workaround problem reported in Eclipse bug #38744
-      override val scalaStep: ScalaStep = new ScalaStepImpl(this)
-    }
-    companionActor.start()
-
-    companionActor.scalaStep
+    val subordinate = new ScalaStepReturnSubordinate(scalaStackFrame.getDebugTarget, scalaStackFrame.thread, stepReturnRequest)
+    subordinate.scalaStep
   }
 }
 
@@ -27,59 +24,47 @@ object ScalaStepReturn {
  * Actor used to manage a Scala step return. It keeps track of the request needed to perform this step.
  * This class is thread safe. Instances are not to be created outside of the ScalaStepReturn object.
  */
-private[command] abstract class ScalaStepReturnActor(debugTarget: ScalaDebugTarget, thread: ScalaThread, stepReturnRequest: StepRequest) extends BaseDebuggerActor {
+private[command] class ScalaStepReturnSubordinate(debugTarget: ScalaDebugTarget, thread: ScalaThread, stepReturnRequest: StepRequest)
+    extends ScalaStep with JdiEventReceiver {
+  import scala.concurrent.ExecutionContext.Implicits.global
+  private val enabled = new AtomicBoolean
 
-  private var enabled = false
+  protected[command] def scalaStep: ScalaStep = this
 
-  protected[command] def scalaStep: ScalaStep
-
-  override protected def postStart(): Unit = link(thread.companionActor)
-
-  override protected def behavior = {
-    // JDI event triggered when a step has been performed
-    case stepEvent: StepEvent =>
-      reply {
+  override def handle(event: Event): Future[Boolean] = Future {
+    event match {
+      // JDI event triggered when a step has been performed
+      case stepEvent: StepEvent =>
         if (!debugTarget.cache.isTransparentLocation(stepEvent.location)) {
-          terminate()
+          disable()
           thread.suspendedFromScala(DebugEvent.STEP_RETURN)
           true
-        }
-        else false
-      }
-    case ScalaStep.Step => step()    // user step request
-    case ScalaStep.Stop => terminate() // step is terminated
+        } else false
+      case _ => false
+    }
   }
 
-  private def step(): Unit = {
+  override def step(): Unit = Future {
     enable()
     thread.resumeFromScala(scalaStep, DebugEvent.STEP_RETURN)
   }
 
-  private def terminate(): Unit = {
+  override def stop(): Unit = Future {
     disable()
-    poison()
   }
 
   private def enable(): Unit = {
-    if (!enabled) {
-      debugTarget.eventDispatcher.setActorFor(this, stepReturnRequest)
+    if (!enabled.getAndSet(true)) {
+      debugTarget.eventDispatcher.register(this, stepReturnRequest)
       stepReturnRequest.enable()
-      enabled = true
     }
   }
 
   private def disable(): Unit = {
-    if (enabled) {
-
+    if (enabled.getAndSet(false)) {
       stepReturnRequest.disable()
-      debugTarget.eventDispatcher.unsetActorFor(stepReturnRequest)
+      debugTarget.eventDispatcher.unregister(stepReturnRequest)
       debugTarget.virtualMachine.eventRequestManager.deleteEventRequest(stepReturnRequest)
-      enabled = false
     }
-  }
-
-  override protected def preExit(): Unit = {
-    unlink(thread.companionActor)
-    disable()
   }
 }
