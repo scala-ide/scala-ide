d9e23b0c9f8b2b0b39840bb9e2b4c8db1e0181ec
wpopielarski <wpopielarski@gmail.com>
2015-08-28 16:40:27 +0200

    Trial to make Break On Dead Letters more reliable

--- org.scala-ide.sdt.debug/src/org/scalaide/debug/internal/async/BreakOnDeadLetters.scala
+++ org.scala-ide.sdt.debug/src/org/scalaide/debug/internal/async/BreakOnDeadLetters.scala
@@ -1,27 +1,60 @@
 package org.scalaide.debug.internal.async
 
+import java.util.concurrent.atomic.AtomicReference
+
+import scala.collection.JavaConverters.asScalaBufferConverter
+
 import org.eclipse.debug.core.DebugEvent
 import org.eclipse.jface.util.PropertyChangeEvent
 import org.scalaide.core.internal.ScalaPlugin
-
 import org.scalaide.debug.internal.BaseDebuggerActor
 import org.scalaide.debug.internal.model.ScalaDebugTarget
 import org.scalaide.logging.HasLogger
-import org.scalaide.util.eclipse.SWTUtils._
+import org.scalaide.util.eclipse.SWTUtils.fnToPropertyChangeListener
 
 import com.sun.jdi.event.BreakpointEvent
+import com.sun.jdi.event.ClassPrepareEvent
 import com.sun.jdi.request.EventRequest
+
+import BreakOnDeadLetters.PrefChange
+import BreakOnDeadLetters.PreferenceId
+import BreakOnDeadLetters.Shutdown
 
 /**
  * Responsible for installing and removing the break on dead letters
  * functionality of the async debugger on top of `debugTarget`.
  */
 class BreakOnDeadLetters(debugTarget: ScalaDebugTarget) extends HasLogger {
-  import BreakOnDeadLetters._
+  private val asyncPoint = AsyncProgramPoint("akka.actor.DeadLetterActorRef", "$bang", 0)
+  private val eventRequestsRef: AtomicReference[Set[EventRequest]] = new AtomicReference(Set())
+  private[async] def eventRequests = eventRequestsRef.get
+
+  private[async] def createRequests() =
+    if (debugTarget.virtualMachine.classesByName(asyncPoint.className).asScala.isEmpty) {
+      debugTarget.cache.addClassPrepareEventListener(internalActor, asyncPoint.className)
+    } else {
+      disable()
+      eventRequestsRef.getAndSet(AsyncUtils.installMethodBreakpoint(debugTarget, asyncPoint, internalActor).toSet)
+      if (eventRequests.isEmpty)
+        logger.error("Could not install dead letter breakpoints")
+    }
+
+  private def disable(): Unit = {
+    val eventDispatcher = debugTarget.eventDispatcher
+    val eventRequestManager = debugTarget.virtualMachine.eventRequestManager
+    for (request <- eventRequestsRef.getAndSet(Set())) {
+      request.disable()
+      eventDispatcher.unsetActorFor(request)
+      eventRequestManager.deleteEventRequest(request)
+    }
+  }
 
   def start(): Unit = {
     internalActor.start()
-    internalActor ! Initialize
+    ScalaPlugin().getPreferenceStore.addPropertyChangeListener(propListener)
+    val enabled = ScalaPlugin().getPreferenceStore.getBoolean(PreferenceId)
+    if (enabled)
+      createRequests()
   }
 
   def dispose(): Unit = {
@@ -34,57 +67,31 @@ class BreakOnDeadLetters(debugTarget: ScalaDebugTarget) extends HasLogger {
   }
 
   object internalActor extends BaseDebuggerActor {
-    private val asyncPoint = AsyncProgramPoint("akka.actor.DeadLetterActorRef", "$bang", 0)
-    private var eventRequests: Set[EventRequest] = Set()
-
     override def behavior = {
-
-      case Initialize =>
-        ScalaPlugin().getPreferenceStore.addPropertyChangeListener(propListener)
-        val enabled = ScalaPlugin().getPreferenceStore.getBoolean(PreferenceId)
-        if (enabled)
-          createRequests()
-
       case PrefChange(event: PropertyChangeEvent) =>
         if (event.getNewValue().asInstanceOf[Boolean])
           createRequests()
         else
           disable()
-
       case Shutdown =>
         ScalaPlugin().getPreferenceStore.removePropertyChangeListener(propListener)
         disable()
         poison()
-
       case breakpointEvent: BreakpointEvent if eventRequests(breakpointEvent.request()) =>
         logger.debug(s"Suspending thread ${breakpointEvent.thread.name()}")
         // most likely the breakpoint was hit on a different thread than the one we started with, so we find it here
         debugTarget.getScalaThread(breakpointEvent.thread()).foreach(_.suspendedFromScala(DebugEvent.BREAKPOINT))
         reply(true) // suspend here!
+      case classPrepareEvent: ClassPrepareEvent =>
+        val refType = classPrepareEvent.referenceType()
+        if (asyncPoint.className == refType.name())
+          createRequests()
+        reply(false)
     }
-
-    def createRequests() = {
-      eventRequests = AsyncUtils.installMethodBreakpoint(debugTarget, asyncPoint, internalActor).toSet
-      if (eventRequests.isEmpty)
-        logger.error("Could not install dead letter breakpoints")
-    }
-
-    private def disable(): Unit = {
-      val eventDispatcher = debugTarget.eventDispatcher
-      val eventRequestManager = debugTarget.virtualMachine.eventRequestManager
-
-      for (request <- eventRequests) {
-        request.disable()
-        eventDispatcher.unsetActorFor(request)
-        eventRequestManager.deleteEventRequest(request)
-      }
-    }
-
   }
 }
 
 object BreakOnDeadLetters {
-  case object Initialize
   case object Shutdown
   case class PrefChange(event: PropertyChangeEvent)
 
