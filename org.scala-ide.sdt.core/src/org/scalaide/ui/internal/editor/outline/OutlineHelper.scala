c34c61c49a150494fd045d87d9d679bf45ab52a1
ailinykh <ailinykh@gmail.com>
2015-11-19 09:04:45 -0800

    fix a typo

--- org.scala-ide.sdt.core/src/org/scalaide/ui/internal/editor/outline/OutlineHelper.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/ui/internal/editor/outline/OutlineHelper.scala
@@ -3,8 +3,8 @@ package org.scalaide.ui.internal.editor.outline
 import org.eclipse.jface.viewers.TreeViewer
 
 object OutlineHelper {
-  def foldImportNodes(viewer: TreeViewer, inout: Any): Unit = {
-    inout match {
+  def foldImportNodes(viewer: TreeViewer, input: Any): Unit = {
+    input match {
       case n: ImportsNode => viewer.collapseToLevel(n, 1)
       case n: ContainerNode => n.children.values.foreach { x => foldImportNodes(viewer, x) }
       case _ =>
