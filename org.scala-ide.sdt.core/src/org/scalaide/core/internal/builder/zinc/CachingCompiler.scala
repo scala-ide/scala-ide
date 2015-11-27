51bd5910abc5f4b4e726df1b99693ba83e2fe6a7
wpopielarski <wpopielarski@gmail.com>
2015-11-27 16:51:31 +0100

    Better usage of IC API
    `IC` has better API to read previous analysis and setup. There is
    no need to implement homegrown solution.

--- org.scala-ide.sdt.core/src/org/scalaide/core/internal/builder/zinc/CachingCompiler.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/core/internal/builder/zinc/CachingCompiler.scala
@@ -1,32 +1,16 @@
 package org.scalaide.core.internal.builder.zinc
 
 import java.io.File
+
 import org.scalaide.util.internal.SbtUtils
-import sbt.CompileSetup
-import sbt.EmptyAnalysis
+
 import sbt.compiler.AnalyzingCompiler
 import sbt.compiler.IC
 import sbt.compiler.MixedAnalyzingCompiler
 import sbt.inc.Analysis
-import sbt.inc.AnalysisStore
 import xsbti.Logger
 import xsbti.Reporter
 import xsbti.compile.Compilers
-
-private object Cache {
-  type Result = (Analysis, Option[CompileSetup])
-
-  private[zinc] def previousResult(cacheFile: File, initialNameHashing: Boolean): Result = {
-    Cache(cacheFile).get().map {
-      case (analysis, setup) => (analysis, Some(setup))
-    } getOrElse {
-      (EmptyAnalysis(initialNameHashing), None)
-    }
-  }
-
-  private[zinc] def apply(cacheFile: File): AnalysisStore =
-    MixedAnalyzingCompiler.staticCachedStore(cacheFile)
-}
 
 class CachingCompiler private (cacheFile: File, sbtReporter: Reporter, log: Logger) {
   /**
@@ -43,7 +27,10 @@ class CachingCompiler private (cacheFile: File, sbtReporter: Reporter, log: Logg
     val options = in.options; import options.{ options => scalacOptions, _ }
     val aMap = (f: File) => SbtUtils.m2o(in.analysisMap(f))
     val defClass = (f: File) => { val dc = Locator(f); (name: String) => dc.apply(name) }
-    val (previousAnalysis, previousSetup) = Cache.previousResult(cacheFile, in.incOptions.nameHashing)
+    val (previousAnalysis, previousSetup) = SbtUtils.m2o(IC.readCache(cacheFile))
+      .map {
+        case (a, s) => (a, Option(s))
+      }.getOrElse((IC.readAnalysis(cacheFile, in.incOptions), None))
     import comps._
     cacheAndReturnLastAnalysis(IC.incrementalCompile(scalac, javac, options.sources, classpath, output, in.cache,
       SbtUtils.m2o(in.progress), scalacOptions, javacOptions, previousAnalysis, previousSetup, aMap, defClass,
@@ -52,7 +39,7 @@ class CachingCompiler private (cacheFile: File, sbtReporter: Reporter, log: Logg
 
   private def cacheAndReturnLastAnalysis(compilationResult: IC.Result): Analysis = {
     if (compilationResult.hasModified)
-      Cache(cacheFile).set(compilationResult.analysis, compilationResult.setup)
+      MixedAnalyzingCompiler.staticCachedStore(cacheFile).set(compilationResult.analysis, compilationResult.setup)
     compilationResult.analysis
   }
 }
--- org.scala-ide.sdt.core/src/org/scalaide/core/internal/builder/zinc/EmptyAnalysis.scala
+++
@@ -1,8 +0,0 @@
-import sbt.inc.Analysis
-
-package sbt {
-  /** Just to expose `Analysis.empty(nameHashing: Boolean)` to non `sbt` packages. */
-  object EmptyAnalysis {
-    def apply(nameHashing: Boolean): Analysis = Analysis.empty(nameHashing)
-  }
-}
