5c1650e8971bbbda87546d0419983be6322187d1
Simon Schäfer <mail@antoras.de>
2017-01-29 20:05:37 +0100

    Add 2.12 entry to CompilerSettings

--- org.scala-ide.sdt.core/src/org/scalaide/ui/internal/preferences/CompilerSettings.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/ui/internal/preferences/CompilerSettings.scala
@@ -435,11 +435,12 @@ class CompilerSettings extends PropertyPage with IWorkbenchPreferencePage with E
     def itemTitle = "Fixed Scala Installation"
   }
 
-  def choicesOfScalaInstallations(): Array[Array[String]] = {
-    (Array("Latest 2.11 bundle (dynamic)", "2.11") ::
-      (Array("Latest 2.10 bundle (dynamic)", "2.10") ::
-        ScalaInstallation.availableInstallations.map { si => Array(labeler.getDecoration(si), ScalaInstallationChoice(si).toString()) })).toArray
-  }
+  def choicesOfScalaInstallations(): Array[Array[String]] = (
+    Array("Latest 2.12 bundle (dynamic)", "2.12") ::
+    Array("Latest 2.11 bundle (dynamic)", "2.11") ::
+    Array("Latest 2.10 bundle (dynamic)", "2.10") ::
+    ScalaInstallation.availableInstallations.map { si => Array(labeler.getDecoration(si), ScalaInstallationChoice(si).toString()) }
+  ).toArray
 
   class DesiredInstallationWidget(parent: Composite) extends ComboFieldEditor(
     SettingConverterUtil.SCALA_DESIRED_INSTALLATION,
