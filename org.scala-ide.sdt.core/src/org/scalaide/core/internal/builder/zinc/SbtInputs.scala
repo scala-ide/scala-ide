adf57bd04673f1c58b2ad823af0d64a50b96080e
Simon Schäfer <mail@antoras.de>
2017-03-06 14:47:56 +0100

    Get rid of withNameHashing option
    It no longer exists.

--- org.scala-ide.sdt.core/src/org/scalaide/core/internal/builder/zinc/SbtInputs.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/core/internal/builder/zinc/SbtInputs.scala
@@ -70,9 +70,7 @@ class SbtInputs(installation: IScalaInstallation,
       withRelationsDebug(project.storage.getBoolean(SettingConverterUtil.convertNameToProperty(preferences.ScalaPluginSettings.relationsDebug.name))).
       withClassfileManagerType(Maybe.just(new TransactionalManagerType(tempDir, logger))).
       withApiDumpDirectory(Maybe.nothing()).
-      withRecompileOnMacroDef(Maybe.just(project.storage.getBoolean(SettingConverterUtil.convertNameToProperty(preferences.ScalaPluginSettings.recompileOnMacroDef.name)))).
-      // Turning off name hashing is not supported in class-based dependency tracking
-      withNameHashing(true)
+      withRecompileOnMacroDef(Maybe.just(project.storage.getBoolean(SettingConverterUtil.convertNameToProperty(preferences.ScalaPluginSettings.recompileOnMacroDef.name))))
   }
 
   def outputFolders = srcOutputs.map {
