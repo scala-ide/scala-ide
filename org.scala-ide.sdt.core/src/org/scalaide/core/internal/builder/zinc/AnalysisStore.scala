ef8df63daf5d34809ffbc6e43cc9079ecf329114
wpopielarski <wpopielarski@gmail.com>
2017-08-18 15:56:46 +0200

    Fixes compilation errors of core. In progress.

--- org.scala-ide.sdt.core/src/org/scalaide/core/internal/builder/zinc/AnalysisStore.scala
+++ org.scala-ide.sdt.core/src/org/scalaide/core/internal/builder/zinc/AnalysisStore.scala
@@ -4,21 +4,23 @@ import sbt.internal.inc.Analysis
 
 import xsbti.compile.CompileAnalysis
 import xsbti.compile.MiniSetup
+import java.util.Optional
+import xsbti.compile.AnalysisContents
 
 object AnalysisStore {
-  import sbt.internal.inc.{ AnalysisStore => SbtAnalysisStore }
+  import xsbti.compile.{ AnalysisStore => SbtAnalysisStore }
   def materializeLazy(backing: SbtAnalysisStore): SbtAnalysisStore = new SbtAnalysisStore {
-    private def materializeApis(analysis: CompileAnalysis, setup: MiniSetup) = {
+    private def materializeApis(analysis: CompileAnalysis, setup: MiniSetup): AnalysisContents = {
       if (setup.storeApis()) {
         val apis = analysis match { case a: Analysis => a.apis }
         apis.internal.foreach { case (_, v) => v.api }
         apis.external.foreach { case (_, v) => v.api }
       }
-      (analysis, setup)
+      AnalysisContents.create(analysis, setup)
     }
-    def set(analysis: CompileAnalysis, setup: MiniSetup): Unit = {
-      Function.tupled(backing.set _)(materializeApis(analysis, setup))
+    def set(contents: AnalysisContents): Unit = {
+      backing.set(materializeApis(contents.getAnalysis, contents.getMiniSetup))
     }
-    def get(): Option[(CompileAnalysis, MiniSetup)] = backing.get()
+    def get(): Optional[AnalysisContents] = backing.get()
   }
 }
